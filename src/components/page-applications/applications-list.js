8767c091054f34d63e507ea284a3c37f9eb4fc22
Nelson Menezes <nemzes@gmail.com>
2018-06-22 13:13:58 +0200

    Fix missing key

--- src/components/page-applications/applications-list.js
+++ src/components/page-applications/applications-list.js
@@ -1,13 +1,29 @@
 import React from 'react';
 
 export const ApplicationsList = ({ apps }) => {
-  console.log(apps);
   return (
     <ul>
       {apps.map(app => (
         <li key={app.metadata.name}>
           {app.metadata.name}{' '}
           {app.kind === 'RadixRegistration' && '(Not yet processed)'}
+          <ul>
+            {app.kind === 'RadixApplication' &&
+              app.spec.environments.map(env =>
+                app.spec.components.map(component => (
+                  <li key={env.name + component.name}>
+                    <a
+                      target="_blank"
+                      href={`https://${component.name}-${app.metadata.name}-${
+                        env.name
+                      }.dev.radix.equinor.com`}
+                    >
+                      {env.name}
+                    </a>
+                  </li>
+                ))
+              )}
+          </ul>
         </li>
       ))}
     </ul>
