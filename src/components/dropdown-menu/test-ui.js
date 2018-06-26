c2887f21f9eb91e6dfd44338cf96083234f7dda3
Nelson Menezes <nemzes@gmail.com>
2018-06-26 16:16:58 +0200

    Add dropdown toggler

---
+++ src/components/dropdown-menu/test-ui.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import DropdownMenu from '.';
+
+export default (
+  <DropdownMenu label="Menu">
+    <ul>
+      <li>Menu</li>
+      <li>Contents</li>
+    </ul>
+  </DropdownMenu>
+);
