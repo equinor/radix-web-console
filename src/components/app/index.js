b10e5f6d486cba87fdd7e5267b0f81ce7a3c63d9
Nelson Menezes <nemzes@gmail.com>
2018-06-26 09:54:31 +0200

    Initial styling of banner

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -12,28 +12,10 @@ import './style.css';
 export default () => (
   <div>
     <GlobalNav />
-    <h1 className="o-heading-page">Radix Web Console</h1>
-    <div>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to={routes.about}>About</Link>
-        </li>
-        <li>
-          <Link to={routes.apps}>Applications</Link>
-        </li>
-        <li>
-          <Link to={routes.authLogout}>Log out</Link>
-        </li>
-      </ul>
-
-      <hr />
-
+    <main className="o-layout-container">
       <Route path={routes.about} component={PageAbout} />
       <Route path={routes.apps} component={PageApplications} exact />
       <Route path={routes.appCreate} component={PageCreateApplication} />
-    </div>
+    </main>
   </div>
 );
--- src/components/global-nav/index.js
+++ src/components/global-nav/index.js
@@ -1,15 +1,34 @@
 import React from 'react';
 import { withRouter } from 'react-router';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 import routes from '../../routes';
+import banner from './banner.svg';
 import './style.css';
 
 export const GlobalNav = ({ match }) => (
   <nav className="global-nav">
-    <Link to={routes.home}>
-      Logo
-    </Link>
+    <div className="o-layout-container">
+      <div className="global-nav__container">
+        <NavLink to={routes.home} className="global-nav__banner">
+          <img
+            className="global-nav__banner-img"
+            src={banner}
+            alt="Omnia Radix web console"
+          />
+        </NavLink>
+        <ul className="global-nav__menu">
+          <li>
+            <NavLink to={routes.apps} activeClassName="active">
+              Applications
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to={routes.authLogout}>Log out</NavLink>
+          </li>
+        </ul>
+      </div>
+    </div>
   </nav>
 );
 
