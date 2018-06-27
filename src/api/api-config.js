ebabf0116614a1ccaa1a5d008677e41147fd6a73
Nelson Menezes <nemzes@gmail.com>
2018-06-27 14:38:57 +0200

    Use temp API URI

--- src/api/api-config.js
+++ src/api/api-config.js
@@ -32,12 +32,12 @@ const config = {
     radix_dev_playground_radix: {
       azureADAppId: '10dfbb3a-d180-4da4-a74b-94a29333b43c',
       baseUri:
-        'api.playground-k8s-f-radixvalues-1.dev.radix.equinor.com:443/apis/radix.equinor.com/v1/',
+        'playground-playground-k8s-f-16ede4-cfd7f838.hcp.northeurope.azmk8s.io:443/apis/radix.equinor.com/v1/',
     },
     radix_dev_playground_k8s: {
       azureADAppId: '10dfbb3a-d180-4da4-a74b-94a29333b43c',
       baseUri:
-        'api.playground-k8s-f-radixvalues-1.dev.radix.equinor.com:443/api/v1/',
+        'playground-playground-k8s-f-16ede4-cfd7f838.hcp.northeurope.azmk8s.io:443/api/v1/',
     },
   },
 };
