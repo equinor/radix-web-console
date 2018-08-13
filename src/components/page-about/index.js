9d84e223514d15d07c2574ff2fdeb786986328f4
Nelson Menezes <nemzes@gmail.com>
2018-08-13 09:10:50 +0200

    Dummy commit

--- src/components/page-about/index.js
+++ src/components/page-about/index.js
@@ -6,7 +6,7 @@ const PageAbout = () => {
       <div className="o-layout-page-head">
         <h1 className="o-heading-page">About</h1>
       </div>
-      <p>Hello, this is the about page</p>
+      <p>Hello, this is the about page.</p>
     </React.Fragment>
   );
 };
