c03c8d8af94cd71d0532bcc764e904e71c319378
Nelson Menezes <nemzes@gmail.com>
2018-09-19 11:37:40 +0200

    Fix component links to allow for truncated domain names

--- src/utils/string.js
+++ src/utils/string.js
@@ -4,5 +4,11 @@ export const routeWithParams = (route, params) =>
   route.replace(/:(\w+)/g, (match, key) => params[key]);
 
 export const linkToComponent = (componentName, appName, env) => {
-  return `https://${componentName}-${appName}-${env}.${clusterDomain}`;
+  // We are currently creating domain names that use only the last 5 characters
+  // of the cluster name, due to DNS entry length restrictions
+  const domainParts = clusterDomain.split('.');
+  domainParts[0] = domainParts[0].slice(-5);
+
+  const domain = domainParts.join('.');
+  return `https://${componentName}-${appName}-${env}.${domain}`;
 };
