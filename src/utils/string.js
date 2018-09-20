b456fd940f685214a2280d0641cfed60e1b21020
Nelson Menezes <nemzes@gmail.com>
2018-09-20 14:38:38 +0200

    Restore full component URLs

--- src/utils/string.js
+++ src/utils/string.js
@@ -4,11 +4,5 @@ export const routeWithParams = (route, params) =>
   route.replace(/:(\w+)/g, (match, key) => params[key]);
 
 export const linkToComponent = (componentName, appName, env) => {
-  // We are currently creating domain names that use only the last 5 characters
-  // of the cluster name, due to DNS entry length restrictions
-  const domainParts = clusterDomain.split('.');
-  domainParts[0] = domainParts[0].slice(-5);
-
-  const domain = domainParts.join('.');
-  return `https://${componentName}-${appName}-${env}.${domain}`;
+  return `https://${componentName}-${appName}-${env}.${clusterDomain}`;
 };
