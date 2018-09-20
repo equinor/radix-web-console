ff6724829bd4cc24770f7f6d8e5528b48ef9c90e
Nelson Menezes <nemzes@gmail.com>
2018-09-20 11:20:36 +0200

    Add API to retrive build log

--- src/api/jobs.js
+++ src/api/jobs.js
@@ -1,4 +1,15 @@
-import { subscribeBatchResource } from './api-helpers';
+import { getJson, getText, subscribeBatchResource } from './api-helpers';
 
 export const subscribeRadixJobs = () =>
   subscribeBatchResource('jobs?labelSelector=build');
+
+export async function getLog(jobName, namespace) {
+  const podName = await getJson(
+    `namespaces/${namespace}/pods?labelSelector=job-name%3D${jobName}`
+  ).items[0].metadata.name;
+
+  return await getText(
+    `namespaces/${namespace}/pods/${podName}/log`,
+    'radix_dev_playground_k8s'
+  );
+}
