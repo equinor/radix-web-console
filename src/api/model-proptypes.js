6798adecd5c28781eca7429260024549c9535ed7
Nelson Menezes <nemzes@gmail.com>
2018-10-04 13:36:02 +0200

    Add API contract testing

---
+++ src/api/model-proptypes.js
@@ -0,0 +1,10 @@
+import PropTypes from 'prop-types';
+
+// TODO: auto-generate this from the API server Swagger defs
+
+export const application = PropTypes.exact({
+  adGroups: PropTypes.arrayOf(PropTypes.string).isRequired,
+  publicKey: PropTypes.string,
+  repository: PropTypes.string.isRequired,
+  sharedSecret: PropTypes.string.isRequired,
+});
