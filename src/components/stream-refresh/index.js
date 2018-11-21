86e0db99c51e4c34b2448ad84b010c3d288d36a2
bjaskj <silentas@gmail.com>
2018-11-21 10:41:26 +0100

    Changed to use button control.

--- src/components/stream-refresh/index.js
+++ src/components/stream-refresh/index.js
@@ -1,27 +1,24 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { subscriptionsRefresh } from '../../state/subscriptions/action-creators';
-import Spinner from '../spinner';
+import Button from '../button';
 
 import './style.css';
 
 const streamingLabelRender = isRefreshing => {
-  if (isRefreshing) {
-    return <Spinner>Refreshing...</Spinner>;
-  }
-
-  return 'Refresh';
-}
+  return isRefreshing ? 'Refreshing...' : 'Refresh';
+};
 
 export const StreamRefresh = ({ status, subscriptionsRefresh }) => (
   <div className="o-layout-container stream-refresh">
-    <button
+    <Button
+      btnType="tiny"
       onClick={() => subscriptionsRefresh()}
       title="Trigger a refresh of all subscribed streams"
       disabled={status.isRefreshing}
     >
       {streamingLabelRender(status.isRefreshing)}
-    </button>
+    </Button>
   </div>
 );
 
