5b7efd17adb5176372a4abe76811592538f9a861
bjaskj <silentas@gmail.com>
2018-12-04 13:50:18 +0100

    OR-490 Updated dev data for app-list.

--- src/components/app-list/dev.js
+++ src/components/app-list/dev.js
@@ -36,16 +36,18 @@ const apps = [
     name: 'radix-web-console3',
   },
   {
-    name: 'Bjarte',
-  },
-  {
-    name: 'Bjarte1',
-  },
-  {
-    name: 'Bjarte2',
-  },
-  {
-    name: 'Bjarte3',
+    latestJob: {
+      appName: 'radix-pipeline-20181029135644-algpv-6hznh',
+      branch: 'master',
+      commitID: '4faca8595c5283a9d0f17a623b9255a0d9866a2e',
+      ended: '2006-01-02T15:04:05-0700',
+      environments: 'dev,qa',
+      name: 'radix-pipeline-20181029135644-algpv-6hznh',
+      pipeline: 'build-deploy',
+      started: '2006-01-02T15:04:05-0700',
+      status: jobStatuses.SUCCEEDED,
+    },
+    name: 'radix-api',
   },
 ];
 
