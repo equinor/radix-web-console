a0341a200ecd48b20e68b3d60372a304eb6cf79a
bjaskj <silentas@gmail.com>
2018-12-17 07:52:26 +0100

    OR-487 Added breadcrumb.

---
+++ src/components/global-breadcrumb/dev.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { GlobalBreadcrumb } from './index';
+
+const links = [
+  {
+    label: 'Applications',
+    to: '/applications',
+  },
+  {
+    label: 'radix-api',
+    to: '/applications/radix-api',
+  },
+  {
+    label: 'prod',
+    to: '/applications/radix-api/env/prod',
+  },
+  {
+    label: 'server-8fd44cc58-6zmjt',
+  },
+];
+
+const links2 = [
+  {
+    label: 'Applications',
+    to: '/applications',
+  },
+  {
+    label: 'radix-api',
+    to: '/applications/radix-api',
+  },
+  {
+    label: 'prod',
+  },
+];
+
+const links3 = [
+  {
+    label: 'Applications',
+    to: '/applications',
+  },
+  {
+    label: 'radix-api',
+  },
+];
+
+export default (
+  <div style={{ width: 999, margin: 'auto', marginTop: 50 }}>
+    <GlobalBreadcrumb links={links} />
+    <GlobalBreadcrumb links={links2} />
+    <GlobalBreadcrumb links={links3} />
+  </div>
+);
