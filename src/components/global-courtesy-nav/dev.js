4467914dacc9146a188903d8e4dd2364354c988e
bjaskj <silentas@gmail.com>
2018-12-17 09:48:51 +0100

    OR-489 Added courtesy navigation.

---
+++ src/components/global-courtesy-nav/dev.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { GlobalCourtesyNav } from './index';
+
+export default (
+  <div
+    style={{
+      width: 400,
+      margin: 'auto',
+      marginTop: 50,
+      gridGap: 10,
+      display: 'flex',
+      flexDirection: 'column',
+    }}
+  >
+    <GlobalCourtesyNav />
+  </div>
+);
---
+++ src/components/global-courtesy-nav/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import './style.css';
+
+const GlobalCourtesyNavLink = ({ label, to }) => (
+  <NavLink to={to} className="global-courtesy-nav__link">
+    {label}
+  </NavLink>
+);
+
+export const GlobalCourtesyNav = () => {
+  return (
+    <nav
+      className="global-courtesy-nav"
+      role="navigation"
+      aria-label="Courtesy navigation"
+    >
+      <GlobalCourtesyNavLink to="/documentation" label="Documentation" />
+      <GlobalCourtesyNavLink to="/community" label="Community" />
+    </nav>
+  );
+};
