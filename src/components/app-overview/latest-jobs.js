c984a529be5511c5baf84b786db42efb29efb6c5
Nelson Menezes <nemzes@gmail.com>
2019-01-10 21:12:17 +0100

    Tidy up layout; status highlighting

--- src/components/app-overview/latest-jobs.js
+++ src/components/app-overview/latest-jobs.js
@@ -12,6 +12,18 @@ import CommitHash from '../commit-hash';
 import { routeWithParams, formatDateTime } from '../../utils/string';
 import routes from '../../routes';
 
+const Duration = ({ job }) => {
+  if (!job.ended) {
+    return null;
+  }
+
+  return (
+    <span title="Job duration">
+      {distanceInWords(new Date(job.started), new Date(job.ended))}
+    </span>
+  );
+};
+
 export const LatestJobs = ({ appName, jobs }) => (
   <ul className="item-list">
     {jobs.slice(0, 5).map(job => (
@@ -20,38 +32,53 @@ export const LatestJobs = ({ appName, jobs }) => (
           <div
             className={`job-summary job-summary--${job.status.toLowerCase()}`}
           >
-            <h2>
-              <Link
-                to={routeWithParams(routes.appJob, {
-                  appName,
-                  jobName: job.name,
-                })}
-              >
-                {job.name}
-              </Link>
-            </h2>
             <ul className="job-summary__data">
-              <li>
-                <FontAwesomeIcon icon={faHashtag} />
-                Commit <CommitHash commit={job.commitID} />
+              <li className="job-summary__data-section">
+                <div className="job-summary__icon">
+                  <FontAwesomeIcon icon={faHashtag} size="lg" />
+                </div>
+                <div className="job-summary__data-list">
+                  <strong>User Name</strong>
+                  <CommitHash commit={job.commitID} />
+                </div>
               </li>
-              <li>
-                <FontAwesomeIcon icon={faClock} />
-                {formatDateTime(job.started)}
-                {job.ended && (
-                  <>
-                    {' '}
-                    ({distanceInWords(new Date(job.started), new Date(job.ended))})
-                  </>
-                )}
+              <li className="job-summary__data-section">
+                <div className="job-summary__icon">
+                  <FontAwesomeIcon icon={faClock} size="lg" />
+                </div>
+                <div className="job-summary__data-list">
+                  <div title="Start time">{formatDateTime(job.started)}</div>
+                  <Duration job={job} />
+                </div>
               </li>
-              <li>
-                <FontAwesomeIcon icon={faTruck} />
-                Deploy to
-                {(job.environments && job.environments.join(', ')) || '—'}
+              <li className="job-summary__data-section">
+                {job.environments &&
+                  job.environments.length && (
+                    <>
+                      <div className="job-summary__icon">
+                        <FontAwesomeIcon icon={faTruck} size="lg" />
+                      </div>
+                      <div>{job.environments.join(', ')}</div>
+                    </>
+                  )}
               </li>
-              <li>{job.status}</li>
-              <li>{job.pipeline}</li>
+              <li className="job-summary__data-section">
+                <span className="job-summary__status">{job.status}</span>
+              </li>
+              <li className="job-summary__data-section">
+                <div className="job-summary__data-list">
+                  <Link
+                    className="job-summary__link"
+                    to={routeWithParams(routes.appJob, {
+                      appName,
+                      jobName: job.name,
+                    })}
+                  >
+                    {job.name.slice(-5)}
+                  </Link>
+                  <div>{job.pipeline}</div>
+                </div>
+              </li>
             </ul>
           </div>
         </Clickbox>
