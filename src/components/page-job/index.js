4c840a27ecdadf2111cbd5dd7a832c399f17570f
Nelson Menezes <nemzes@gmail.com>
2019-01-16 14:55:59 +0100

    Basic job info

--- src/components/page-job/index.js
+++ src/components/page-job/index.js
@@ -1,14 +1,10 @@
 import { connect } from 'react-redux';
-import AnsiUp from 'ansi_up';
-import format from 'date-fns/format';
 import PropTypes from 'prop-types';
 import React from 'react';
 
 import Breadcrumb from '../breadcrumb';
-import Code from '../code';
+import CommitHash from '../commit-hash';
 import DocumentTitle from '../document-title';
-import Panel from '../panel';
-import Toggler from '../toggler';
 
 import { getJob } from '../../state/job';
 import { getJobStepLog } from '../../state/job-logs';
@@ -23,12 +19,6 @@ import {
 } from '../../state/subscriptions/action-creators';
 
 import { mapRouteParamsToProps } from '../../utils/routing';
-
-const TIME_FORMAT = 'YYYY-MM-DD HH:mm';
-
-const makeHeader = text => (
-  <h3 className="o-heading-section o-heading--lean">{text}</h3>
-);
 
 export class PageApplicationJob extends React.Component {
   componentWillMount() {
@@ -51,19 +41,9 @@ export class PageApplicationJob extends React.Component {
   render() {
     const { appName, jobName, job } = this.props;
 
-    if (!job) {
-      return 'Loading…';
-    }
-
-    const getFormattedLog = log => {
-      return {
-        __html: new AnsiUp().ansi_to_html(log),
-      };
-    };
-
     return (
       <React.Fragment>
-        <DocumentTitle title={`${job.name} (job)`} />
+        <DocumentTitle title={`Job ${jobName}`} />
         <Breadcrumb
           links={[
             { label: appName, to: routeWithParams(routes.app, { appName }) },
@@ -72,54 +52,28 @@ export class PageApplicationJob extends React.Component {
           ]}
         />
         <main>
-          <h1 className="o-heading-page">Job: {job.name}</h1>
-          <Panel>
-            <h3 className="o-heading-section o-heading--first">Summary</h3>
-            <div className="o-layout-columns">
-              <div>
-                <dl className="o-key-values">
-                  <div className="o-key-values__group">
-                    <dt>Started at</dt>
-                    <dd>{format(job.started, TIME_FORMAT)}</dd>
-                  </div>
-                  <div className="o-key-values__group">
-                    <dt>Finished at</dt>
-                    <dd>
-                      {job.ended ? format(job.ended, TIME_FORMAT) : 'Pending…'}
-                    </dd>
-                  </div>
-                </dl>
-              </div>
-              <div>
-                <dl className="o-key-values">
-                  <div className="o-key-values__group">
-                    <dt>Status</dt>
-                    <dd>{job.status}</dd>
-                  </div>
-                </dl>
-              </div>
-            </div>
-          </Panel>
-
-          {job.steps.map(step => (
-            <Panel key={step.name}>
-              <Toggler summary={makeHeader(`Log: ${step.name}`)}>
-                <Code>
-                  <div
-                    dangerouslySetInnerHTML={getFormattedLog(
-                      this.props.getJobStepLog(step.name)
-                    )}
-                  />
-                </Code>
-              </Toggler>
-            </Panel>
-          ))}
-
-          <Panel>
-            <Toggler summary={makeHeader('Resource')}>
-              <Code>{JSON.stringify(job, null, 2)}</Code>
-            </Toggler>
-          </Panel>
+          {!job && 'No job…'}
+          {job && (
+            <>
+              <h2 className="o-heading-section">Summary</h2>
+              <p>Triggered by</p>
+              <p>
+                <strong>User Name</strong>
+                <CommitHash commit={job.commitID} />
+              </p>
+              <p>Started</p>
+              <p>{job.started}</p>
+              <p>Ended</p>
+              <p>{job.ended} (diff)</p>
+              <h2 className="o-heading-section">Artefacts</h2>
+              {job.deployments &&
+                job.deployments.map(deployment => (
+                  <p>
+                    Deployment {deployment.name} to {deployment.environment}
+                  </p>
+                ))}
+            </>
+          )}
         </main>
       </React.Fragment>
     );
