be62a24c335e22acdb31aa90e6662fac5127cd26
Nelson Menezes <nemzes@gmail.com>
2019-02-27 14:42:33 +0100

    Add new config param

--- run_nginx.sh
+++ run_nginx.sh
@@ -1,5 +1,11 @@
 # Substitute environment variables in the index.html file using the values in the current container environment
-envsubst '${RADIX_CLUSTERNAME} ${RADIX_ENVIRONMENT} ${RADIX_API_ENVIRONMENT} ${RADIX_DNS_ZONE}' < /app/index.html > /app/index.html
+envsubst '
+  ${RADIX_CLUSTERNAME}
+  ${RADIX_ENVIRONMENT}
+  ${RADIX_API_ENVIRONMENT}
+  ${RADIX_DNS_ZONE}
+  ${PLATFORM_USERS_GROUP}
+  ' < /app/index.html > /app/index.html
 
 # Start Nginx
 nginx -g "daemon off;"
--- src/api/apps.js
+++ src/api/apps.js
@@ -1,14 +1,18 @@
 import cloneDeep from 'lodash/cloneDeep';
 
-import applicationRegistrationNormaliser from '../models/application-registration/normaliser';
 import { postJson, deleteRequest } from './api-helpers';
+import { keys as configKeys } from '../utils/config/keys';
+import applicationRegistrationNormaliser from '../models/application-registration/normaliser';
+import configHandler from '../utils/config';
 
 // TODO: Move this somewhere it can be tested against Swagger
 const apiPaths = {
   apps: '/applications',
 };
 
-const RADIX_PLATFORM_USER_GROUP_ID = '7552642f-ad75-4e9d-a140-3ab8f3742c16';
+const DEFAULT_AD_GROUP = configHandler.getConfig(
+  configKeys.PLATFORM_USERS_GROUP
+);
 
 const guidValidator = new RegExp(
   '^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$',
@@ -21,7 +25,7 @@ export async function createApp(app) {
   // If we have auto mode set for ad group, we assign radix platform user group
   // TODO: Move this logic to the API server
   if (app.adModeAuto) {
-    appConfig.adGroups = RADIX_PLATFORM_USER_GROUP_ID;
+    appConfig.adGroups = DEFAULT_AD_GROUP;
   } else {
     appConfig.adGroups = app.adGroups;
   }
