777b71e9ccb4b0bee7bdca6bb7cfba4a42e9bc39
Nelson Menezes <nemzes@gmail.com>
2019-04-30 14:29:51 +0200

    Add new app IDs for authorised access

--- src/api/api-config.js
+++ src/api/api-config.js
@@ -4,19 +4,21 @@ import { keys as configKeys } from '../utils/config/keys';
 const defaultResourceId = 'radix_dev_playground_radix';
 
 const domain = configHandler.getDomain();
-const apiEnvironment = configHandler.getConfig(
-  configKeys.RADIX_API_ENVIRONMENT
-);
-const isQaEnvironment =
-  configHandler.getConfig(configKeys.RADIX_ENVIRONMENT) === 'qa';
+const apiEnv = configHandler.getConfig(configKeys.RADIX_API_ENVIRONMENT);
+const clusterType = configHandler.getConfig(configKeys.RADIX_CLUSTER_TYPE);
 
 const config = Object.freeze({
   /**
-   * Azure AD Application ID of this app
+   * Azure AD application ID of this app. This varies depending on the cluster
+   * type, so that the application has different authorisation requirements
+   * (i.e. permitted groups) for a playground cluster than for production or
+   * development
    */
-  azureADAppId: isQaEnvironment
-    ? 'ce1ed2b2-cdbf-459a-9d77-5582b866a264'
-    : 'a593a59c-8f76-490e-937b-a90779039a90',
+  azureADAppId: {
+    development: '5687b237-eda3-4ec3-a2a1-023e85a2bd84',
+    playground: 'fc082d3c-f792-4190-99f6-b63c1b270ea9',
+    production: 'a593a59c-8f76-490e-937b-a90779039a90',
+  }[clusterType],
 
   /**
    * Azure AD Tenant
@@ -42,8 +44,17 @@ const config = Object.freeze({
    */
   resources: Object.freeze({
     radix_api: Object.freeze({
-      azureADAppId: '58f71060-187f-4f7b-b0de-da2e245a2ec1',
-      baseUri: `server-radix-api-${apiEnvironment}.${domain}/api/v1`,
+      /**
+       * Azure AD application ID of Radix API. Different cluster types have
+       * different app IDs to enforce authorisation, so we pick the correct
+       * ID for the app that represents the type of cluster.
+       */
+      azureADAppId: {
+        development: '1025aa65-09e1-41a8-8c59-68ede2e41340',
+        playground: '1025aa65-09e1-41a8-8c59-68ede2e41340', // NB: same as development for now
+        production: '58f71060-187f-4f7b-b0de-da2e245a2ec1',
+      }[clusterType],
+      baseUri: `server-radix-api-${apiEnv}.${domain}/api/v1`,
     }),
   }),
 });
