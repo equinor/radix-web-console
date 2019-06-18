627b3072be341428d3707b97de82e5119855a09b
Nelson Menezes <nemzes@gmail.com>
2019-06-18 15:17:03 +0200

    Fix normalizers

--- src/models/environment-summary/normaliser.js
+++ src/models/environment-summary/normaliser.js
@@ -1,8 +1,18 @@
 import pick from 'lodash/pick';
 
+import deploymentSummaryNormaliser from '../deployment-summary/normaliser';
+
 import model from '.';
 
 /**
  * Create an Environment Summary object
  */
-export default props => Object.freeze(pick(props, Object.keys(model)));
+export default props => {
+  const env = pick(props, Object.keys(model));
+
+  env.activeDeployment = env.activeDeployment
+    ? deploymentSummaryNormaliser(env.activeDeployment)
+    : null;
+
+  return Object.freeze(env);
+};
