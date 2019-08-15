4c570080405c69fe8fb5394563d01537a6520749
INGE KNUDSEN <ingeknudsen@INGEs-MacBook-Pro.local>
2019-08-15 15:38:12 +0200

    Fix job summary

--- src/models/job-summary/index.js
+++ src/models/job-summary/index.js
@@ -3,13 +3,13 @@ import PropTypes from 'prop-types';
 import ProgressStatusModel from '../progress-status';
 
 export default Object.freeze({
+  // triggeredBy: PropTypes.string.isRequired,
   commitID: PropTypes.string,
+  created: PropTypes.instanceOf(Date).isRequired,
   ended: PropTypes.instanceOf(Date),
   environments: PropTypes.arrayOf(PropTypes.string),
   name: PropTypes.string.isRequired,
   pipeline: PropTypes.string.isRequired,
-  created: PropTypes.instanceOf(Date).isRequired,
-  started: PropTypes.instanceOf(Date).isRequired,
+  started: PropTypes.instanceOf(Date),
   status: ProgressStatusModel.isRequired,
-  // triggeredBy: PropTypes.string.isRequired,
 });
