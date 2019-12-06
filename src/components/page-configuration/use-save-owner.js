931fb07bbbfe39002ff440fcf2587ae62784fcb1
Kjell Erik Reed Anda <kjell.erik.anda@gmail.com>
2019-12-06 08:09:18 +0100

    Feature/ra 81 (#159)
    * possible to set owner on app creation
    
    * manage owner

---
+++ src/components/page-configuration/use-save-owner.js
@@ -0,0 +1,10 @@
+import usePatchJson from '../../effects/use-patch-json';
+
+const useSaveOwner = (appName, newOwner) => {
+  const path = `/applications/${appName}`;
+  const data = { owner: newOwner ? newOwner.toString() : null };
+
+  return usePatchJson(path, data, !newOwner);
+};
+
+export default useSaveOwner;
