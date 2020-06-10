faf82ae62b23bce0eab8a829538015b7469c2737
Sergey Smolnikov <ssmol@equinor.com>
2020-06-10 13:19:02 +0200

    RA-1329/RA-1464 Reworked Deploy Only option with use of hooks. Removed this option from app-configuration page

--- src/components/configure-application-github/index.js
+++ src/components/configure-application-github/index.js
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { useState } from 'react';
 
 import Button from '../button';
 import Code from '../code';
@@ -21,145 +21,149 @@ const imageWebhook = require('./webhook02.png');
 const radixZoneDNS = configHandler.getConfig(configKeys.RADIX_CLUSTER_BASE);
 const webhookURL = `https://webhook.${radixZoneDNS}/events/github`;
 
-export class ConfigureApplicationGithub extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { deployOnly: false };
-    this.handleSetUseOtherCiTool = this.handleSetUseOtherCiTool.bind(this);
-  }
-  handleSetUseOtherCiTool() {
-    this.setState({ useOtherCiTool: !this.state.useOtherCiTool });
-  }
-  render() {
-    let { app, startVisible, deployKeyTitle, webhookTitle } = this.props;
-    return (
-      <div className="configure-application-github">
-        <p>To integrate with GitHub you must add a deploy key and a webhook</p>
-        <Panel>
-          <Toggler summary={deployKeyTitle} startVisible={startVisible}>
-            <div className="o-body-text">
-              <p>
-                This allows Radix to clone the repository. Open the{' '}
-                <a
-                  href={`${app.repository}/settings/keys/new`}
-                  rel="noopener noreferrer"
-                  target="_blank"
-                >
-                  Add New Deploy Key page
-                </a>{' '}
-                and follow the steps below
-              </p>
-              <img
-                alt="'Add deploy key' steps on GitHub"
-                src={imageDeployKey}
-                srcSet={`${imageDeployKey} 2x`}
-              />
-              <ol>
-                <li>Give the key a name, e.g. "Radix deploy key"</li>
-                <li>
-                  Copy and paste this key:
-                  <Code copy wrap>
-                    {app.publicKey}
-                  </Code>
-                </li>
-                <li>Press "Add key"</li>
-              </ol>
-            </div>
-          </Toggler>
-        </Panel>
-        <Panel>
+export const ConfigureApplicationGithub = ({
+  app,
+  startVisible,
+  deployKeyTitle,
+  webhookTitle,
+  useOtherCiToolOptionVisible,
+}) => {
+  const [useOtherCiTool, setUseOtherCiTool] = useState(false);
+  const deployOnlyHelp = (
+    <span>
+      Select this option if your project is hosted on multiple repositories
+      and/or requires external control of building. Radix will no longer need a
+      webhook and will instead deploy your app through the API/CLI.
+      <br />
+      See{' '}
+      <a
+        href={externalUrls.deployOnlyGuide}
+        rel="noopener noreferrer"
+        target="_blank"
+      >
+        Deployment Guide
+      </a>{' '}
+      for details.
+    </span>
+  );
+
+  return (
+    <div className="configure-application-github">
+      <p>To integrate with GitHub you must add a deploy key and a webhook</p>
+      <Panel>
+        <Toggler summary={deployKeyTitle} startVisible={startVisible}>
+          <div className="o-body-text">
+            <p>
+              This allows Radix to clone the repository. Open the{' '}
+              <a
+                href={`${app.repository}/settings/keys/new`}
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                Add New Deploy Key page
+              </a>{' '}
+              and follow the steps below
+            </p>
+            <img
+              alt="'Add deploy key' steps on GitHub"
+              src={imageDeployKey}
+              srcSet={`${imageDeployKey} 2x`}
+            />
+            <ol>
+              <li>Give the key a name, e.g. "Radix deploy key"</li>
+              <li>
+                Copy and paste this key:
+                <Code copy wrap>
+                  {app.publicKey}
+                </Code>
+              </li>
+              <li>Press "Add key"</li>
+            </ol>
+          </div>
+        </Toggler>
+      </Panel>
+      <Panel>
+        {useOtherCiToolOptionVisible && (
           <div>
             <fieldset>
               <FormField help={deployOnlyHelp}>
                 <input
                   name="deployOnly"
                   type="checkbox"
-                  value={this.state.useOtherCiTool}
-                  checked={this.state.useOtherCiTool}
-                  onChange={this.handleSetUseOtherCiTool}
+                  value={useOtherCiTool}
+                  checked={useOtherCiTool}
+                  onChange={() => setUseOtherCiTool(!useOtherCiTool)}
                 />
                 Use other CI tool than Radix
               </FormField>
             </fieldset>
           </div>
-          {!this.state.useOtherCiTool && (
-            <div>
-              <Toggler summary={webhookTitle} startVisible={startVisible}>
-                <div className="o-body-text">
-                  <p>
-                    GitHub notifies Radix using a webhook whenever a code push
-                    is made. Open the{' '}
-                    <a
-                      href={`${app.repository}/settings/hooks/new`}
-                      rel="noopener noreferrer"
-                      target="_blank"
+        )}
+        {!useOtherCiTool && (
+          <div>
+            <Toggler summary={webhookTitle} startVisible={startVisible}>
+              <div className="o-body-text">
+                <p>
+                  GitHub notifies Radix using a webhook whenever a code push is
+                  made. Open the{' '}
+                  <a
+                    href={`${app.repository}/settings/hooks/new`}
+                    rel="noopener noreferrer"
+                    target="_blank"
+                  >
+                    Add Webhook page
+                  </a>{' '}
+                  and follow the steps below
+                </p>
+                <img
+                  alt="'Add webhook' steps on GitHub"
+                  src={imageWebhook}
+                  srcSet={`${imageWebhook} 2x`}
+                />
+                <ol>
+                  <li>
+                    As Payload URL, use <code>{webhookURL}</code>{' '}
+                    <Button
+                      onClick={() => copyToClipboard(webhookURL)}
+                      btnType={['default', 'tiny']}
                     >
-                      Add Webhook page
-                    </a>{' '}
-                    and follow the steps below
-                  </p>
-                  <img
-                    alt="'Add webhook' steps on GitHub"
-                    src={imageWebhook}
-                    srcSet={`${imageWebhook} 2x`}
-                  />
-                  <ol>
-                    <li>
-                      As Payload URL, use <code>{webhookURL}</code>{' '}
-                      <Button
-                        onClick={() => copyToClipboard(webhookURL)}
-                        btnType={['default', 'tiny']}
-                      >
-                        Copy
-                      </Button>
-                    </li>
-                    <li>
-                      Choose <code>application/json</code> as Content type
-                    </li>
-                    <li>
-                      The Shared Secret for this application is{' '}
-                      <code>{app.sharedSecret}</code>{' '}
-                      <Button
-                        onClick={() => copyToClipboard(app.sharedSecret)}
-                        btnType={['default', 'tiny']}
-                      >
-                        Copy
-                      </Button>
-                    </li>
-                    <li>Press "Add webhook"</li>
-                  </ol>
-                </div>
-              </Toggler>
-            </div>
-          )}
-        </Panel>
-      </div>
-    );
-  }
-}
-
-const deployOnlyHelp = (
-  <span>
-    Select this option if your project is hosted on multiple repositories and/or
-    requires external control of building. Radix will no longer need a webhook
-    and will instead deploy your app through the API/CLI.
-    <br />
-    See{' '}
-    <a href={externalUrls.deployOnlyGuide} target="_blank">
-      Deployment Guide
-    </a>{' '}
-    for details.
-  </span>
-);
+                      Copy
+                    </Button>
+                  </li>
+                  <li>
+                    Choose <code>application/json</code> as Content type
+                  </li>
+                  <li>
+                    The Shared Secret for this application is{' '}
+                    <code>{app.sharedSecret}</code>{' '}
+                    <Button
+                      onClick={() => copyToClipboard(app.sharedSecret)}
+                      btnType={['default', 'tiny']}
+                    >
+                      Copy
+                    </Button>
+                  </li>
+                  <li>Press "Add webhook"</li>
+                </ol>
+              </div>
+            </Toggler>
+          </div>
+        )}
+      </Panel>
+    </div>
+  );
+};
 
 ConfigureApplicationGithub.propTypes = {
   app: PropTypes.shape(applicationRegistrationModel).isRequired,
   startVisible: PropTypes.bool,
+  useOtherCiToolOptionVisible: PropTypes.bool,
 };
 
 ConfigureApplicationGithub.defaultProps = {
   deployKeyTitle: 'Add deploy key',
   webhookTitle: 'Add webhook',
+  useOtherCiToolOptionVisible: false,
 };
 
 export default ConfigureApplicationGithub;
