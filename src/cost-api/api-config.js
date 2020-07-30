68416084cd3aace9d25e8a97f9455d7b148dfa22
Sergey Smolnikov <ssmol@equinor.com>
2020-07-30 17:51:14 +0200

    Work in progress

--- src/cost-api/api-config.js
+++ src/cost-api/api-config.js
@@ -1,4 +1,4 @@
-export const apiBaseUri = `${window.location.host}/cost-api/v1`;
+export const apiBaseUri = `${window.location.host}/api/v1`;
 
 let dummyAuthentication = false;
 
--- src/cost-api/resource-application-cost.js
+++ src/cost-api/resource-application-cost.js
@@ -1,4 +1,7 @@
-export const makeUrl = (appName) => `/totalcost/${encodeURIComponent(appName)}`;
+export const makeUrl = (appName) =>
+  `/totalcost/${encodeURIComponent(
+    appName
+  )}?fromTime=2020-03-18&toTime=2020-09-18`;
 
 const regexp = new RegExp('^/totalcost/([^/]+)$');
 export const urlMatches = (resource) => resource.match(regexp);
