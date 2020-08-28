36d2a4ece15d02cf01f2495c0338a0b05475e6d8
Kjell Erik Reed Anda <kjell.erik.anda@gmail.com>
2020-08-28 13:22:28 +0200

    use hooks for cost instead of redux/sagas

--- src/components/application-cost/index.js
+++ src/components/application-cost/index.js
@@ -1,60 +1,37 @@
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import React, { useEffect } from 'react';
-import AsyncResource from '../async-resource';
+import React from 'react';
+import AsyncResource from '../async-resource/simple-async-resource';
+import useGetApplicationCost from './use-get-application-cost';
 import './style.css';
-import { getApplicationCost } from '../../state/application-cost';
 import applicationCostSet from '../../models/application-cost-set';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChartArea } from '@fortawesome/free-solid-svg-icons';
 import { CostContent } from './cost-content';
-import Spinner from '../spinner';
-import * as subscriptionCostApiActions from '../../state/subscriptions-cost-api/action-creators';
 import moment from 'moment';
 
 const periodDateFormat = 'YYYY-MM-DD';
 
 export const ApplicationCost = (props) => {
-  const {
-    appName,
-    from,
-    to,
-    applicationCostSet,
-    subscriptionCostApiActions,
-    unsubscriptionCostApiActions,
-  } = props;
-  useEffect(() => {
-    subscriptionCostApiActions(appName, from, to);
-    return () => {
-      unsubscriptionCostApiActions(appName, from, to);
-    };
-  }, [
-    subscriptionCostApiActions,
-    unsubscriptionCostApiActions,
-    appName,
-    from,
-    to,
-  ]);
+  const { appName, from, to } = props;
+  const [applicationCost] = useGetApplicationCost(appName, from, to);
+
   return (
-    <AsyncResource resource="APP_COST" resourceParams={[appName]}>
-      <div className="app-overview__info-tile">
-        <h3 className="app-overview__info-tile-head">Cost estimate</h3>
-        <FontAwesomeIcon
-          className="app-overview__info-tile-image"
-          icon={faChartArea}
-          size="6x"
-        />
-        <React.Fragment>
-          <div className="app-overview__info-tile-body">
-            {applicationCostSet === null ? (
-              <Spinner />
-            ) : (
-              <CostContent applicationCostSet={applicationCostSet} />
-            )}
-          </div>
-        </React.Fragment>
-      </div>
-    </AsyncResource>
+    <div className="app-overview__info-tile">
+      <h3 className="app-overview__info-tile-head">Cost estimate</h3>
+      <FontAwesomeIcon
+        className="app-overview__info-tile-image"
+        icon={faChartArea}
+        size="6x"
+      />
+      <React.Fragment>
+        <div className="app-overview__info-tile-body">
+          <AsyncResource asyncState={applicationCost}>
+            <CostContent applicationCostSet={applicationCost.data} />
+          </AsyncResource>
+        </div>
+      </React.Fragment>
+    </div>
   );
 };
 
@@ -78,23 +55,9 @@ function getDefaultToDate() {
   return moment.utc().clone().startOf('day').format(periodDateFormat);
 }
 
-const mapStateToProps = (state) => ({
-  applicationCostSet: getApplicationCost(state),
+const mapStateToProps = () => ({
   from: getDefaultFromDate(),
   to: getDefaultToDate(),
 });
 
-const mapDispatchToProps = (dispatch) => ({
-  subscriptionCostApiActions: (appName, from, to) => {
-    return dispatch(
-      subscriptionCostApiActions.subscribeApplicationCost(appName, from, to)
-    );
-  },
-  unsubscriptionCostApiActions: (appName, from, to) => {
-    return dispatch(
-      subscriptionCostApiActions.unsubscribeApplicationCost(appName, from, to)
-    );
-  },
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(ApplicationCost);
+export default connect(mapStateToProps)(ApplicationCost);
