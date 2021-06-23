7289fbd8eb7f2e350566eb7e627b227727b7d0ed
Ole Markus Kristiansen Ternø <OLMT@equinor.com>
2021-06-23 13:00:35 +0200

    updated top navation to be responsive

--- src/components/global-top-nav/index.js
+++ src/components/global-top-nav/index.js
@@ -7,17 +7,21 @@ import {
   notifications,
   accessible,
   account_circle,
+  menu,
+  close,
 } from '@equinor/eds-icons';
 import './style.css';
 // TODO: Make responsive.
 export const GlobalTopNav = () => {
+  const [menuIsClosed, setOpenMenu] = React.useState(false);
+  const handleClick = () => setOpenMenu(!menuIsClosed);
   return (
     <>
       <TopBar className="global-top-nav">
         <TopBar.Header>
           <HomeLogo />
         </TopBar.Header>
-        <Tabs.List style={{ display: 'block', textAlign: 'center' }}>
+        <Tabs.List className={menuIsClosed ? 'nav-links active' : 'nav-links'}>
           <a href={externalUrls.playgroundWebConsole}>
             <Tabs.Tab active>Playground</Tabs.Tab>
           </a>
@@ -27,6 +31,20 @@ export const GlobalTopNav = () => {
           <a href={externalUrls.documentation}>
             <Tabs.Tab>Documentation</Tabs.Tab>
           </a>
+          <div className="mobile-icon-links">
+            <Button variant="ghost_icon">
+              <Icon data={info_circle} />
+            </Button>
+            <Button variant="ghost_icon">
+              <Icon data={notifications} />
+            </Button>
+            <Button variant="ghost_icon">
+              <Icon data={accessible} />
+            </Button>
+            <Button variant="ghost_icon">
+              <Icon data={account_circle} />
+            </Button>
+          </div>
         </Tabs.List>
         <TopBar.Actions className="nav-icon-links">
           <Button variant="ghost_icon">
@@ -42,6 +60,20 @@ export const GlobalTopNav = () => {
             <Icon data={account_circle} />
           </Button>
         </TopBar.Actions>
+        {menuIsClosed && (
+          <div className="mobile-menu">
+            <Button variant="ghost_icon" onClick={handleClick}>
+              <Icon data={close} />
+            </Button>
+          </div>
+        )}
+        {!menuIsClosed && (
+          <div className="mobile-menu">
+            <Button variant="ghost_icon" onClick={handleClick}>
+              <Icon data={menu} />
+            </Button>
+          </div>
+        )}
       </TopBar>
     </>
   );
