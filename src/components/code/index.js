4cdc9f30b849469bfea52e6c3e0f799aceab22a4
Ole Markus Kristiansen Ternø <OLMT@equinor.com>
2021-06-24 16:16:46 +0200

    updated component "ConfigureApplicationGithub" to match figma

--- src/components/code/index.js
+++ src/components/code/index.js
@@ -1,8 +1,8 @@
 import classNames from 'classnames';
-import PropTypes from 'prop-types';
-import React, { useCallback, useState } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCopy } from '@fortawesome/free-solid-svg-icons';
+import React from 'react';
+import { Button, Icon, Card } from '@equinor/eds-core-react';
+import { copy as copy_icon } from '@equinor/eds-icons';
+import { tokens } from '@equinor/eds-tokens';
 
 import { copyToClipboard } from '../../utils/string';
 
@@ -15,54 +15,24 @@ export const Code = ({ copy, wrap, children }) => {
     'code--with-toolbar': copy,
   });
 
-  // Monitor scroll state; if scrolled to bottom, keep scroll at bottom to follow updates
-  // TODO: Move to custom hook
-
-  const [scrollOffsetFromBottom, setScrollOffsetFromBottom] = useState(0);
-
-  const handleScroll = (ev) => {
-    const node = ev.target;
-    setScrollOffsetFromBottom(
-      node.scrollHeight - node.scrollTop - node.clientHeight
-    );
-  };
-
-  const scrollableRef = useCallback(
-    (node) => {
-      if (node !== null && scrollOffsetFromBottom === 0) {
-        node.scrollTop = node.scrollHeight;
-      }
-    },
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    [children]
-  );
-
   return (
     <div className={className}>
       {copy && (
         <div className="code__toolbar">
-          <button className="code__btn-copy" onClick={handleClick} title="Copy">
-            <FontAwesomeIcon icon={faCopy} size="2x" />
+          <Button variant="ghost" color="primary" onClick={handleClick}>
+            <Icon data={copy_icon} />
             Copy
-          </button>
+          </Button>
         </div>
       )}
-      <pre ref={scrollableRef} onScroll={handleScroll}>
-        <samp>{children}</samp>
-      </pre>
+      <Card
+        className="code__card"
+        style={{ boxShadow: tokens.elevation.raised }}
+      >
+        {children}
+      </Card>
     </div>
   );
-};
-
-Code.propTypes = {
-  children: PropTypes.node,
-  copy: PropTypes.bool,
-  wrap: PropTypes.bool,
-};
-
-Code.defaultProps = {
-  copy: false,
-  wrap: false,
 };
 
 export default Code;
