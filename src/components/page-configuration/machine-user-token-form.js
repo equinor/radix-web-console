93e6a38f8600a3e352dd2e3e70a394e59b0802c3
Ole Markus Kristiansen Ternø <OLMT@equinor.com>
2021-06-29 09:37:48 +0200

    added eds accordion

--- src/components/page-configuration/machine-user-token-form.js
+++ src/components/page-configuration/machine-user-token-form.js
@@ -1,7 +1,5 @@
 import React from 'react';
 
-import Panel from '../panel';
-import Toggler from '../toggler';
 import Code from '../code';
 import PropTypes from 'prop-types';
 
@@ -10,6 +8,7 @@ import Spinner from '../spinner';
 import Alert from '../alert';
 import requestStates from '../../state/state-utils/request-states';
 import useRegenerateMachineUserToken from './use-regenerate-machine-user-token';
+import { Accordion } from '@equinor/eds-core-react';
 
 const MachineUserTokenForm = (props) => {
   const { appName } = props;
@@ -22,58 +21,64 @@ const MachineUserTokenForm = (props) => {
   const tokenResponse = regenerateMachineUserTokenState.data;
 
   return (
-    <Panel>
-      <Toggler summary="Machine user token">
-        <div className="o-body-text">
-          <p>
-            Machine user token will allow for full app admin access of this
-            application only. You should not be able to access other
-            applications, even other applications that you own or that has the
-            same app admin group. It is a per-application token.
-          </p>
-          <p>
-            Please ensure that the token does not fall into the hands of others.
-            The Radix team will be able to renew the token for you, if need be.
-            Note also that the token may expire when we migrate cluster
-          </p>
-          <p>
-            If you have lost or forgotten this token, you can regenerate it, but
-            be aware that any scripts or applications using this token will need
-            to be updated.
-          </p>
+    <Accordion chevronPosition="right" headerLevel="p">
+      <Accordion.Item className="accordion__item">
+        <Accordion.Header className="accordion__header body_short">
+          Machine user token
+        </Accordion.Header>
+        <Accordion.Panel className="accordion__panel">
+          <div className="o-body-text">
+            <p>
+              Machine user token will allow for full app admin access of this
+              application only. You should not be able to access other
+              applications, even other applications that you own or that has the
+              same app admin group. It is a per-application token.
+            </p>
+            <p>
+              Please ensure that the token does not fall into the hands of
+              others. The Radix team will be able to renew the token for you, if
+              need be. Note also that the token may expire when we migrate
+              cluster
+            </p>
+            <p>
+              If you have lost or forgotten this token, you can regenerate it,
+              but be aware that any scripts or applications using this token
+              will need to be updated.
+            </p>
 
-          {tokenResponse && (
-            <Code copy wrap>
-              {tokenResponse.token}
-            </Code>
-          )}
-
-          <div className="o-action-bar">
-            {regenerateMachineUserTokenState.status ===
-              requestStates.IN_PROGRESS && <Spinner>Regenerating…</Spinner>}
-            {regenerateMachineUserTokenState.status ===
-              requestStates.FAILURE && (
-              <Alert type="danger">
-                Failed to regenerate token.{' '}
-                {regenerateMachineUserTokenState.error}
-              </Alert>
+            {tokenResponse && (
+              <Code copy wrap>
+                {tokenResponse.token}
+              </Code>
             )}
-            {
-              <Button
-                onClick={() => regenerateMachineUserTokenFunc()}
-                btnType="danger"
-                disabled={
-                  regenerateMachineUserTokenState.status ===
-                  requestStates.IN_PROGRESS
-                }
-              >
-                Regenerate token
-              </Button>
-            }
+
+            <div className="o-action-bar">
+              {regenerateMachineUserTokenState.status ===
+                requestStates.IN_PROGRESS && <Spinner>Regenerating…</Spinner>}
+              {regenerateMachineUserTokenState.status ===
+                requestStates.FAILURE && (
+                <Alert type="danger">
+                  Failed to regenerate token.{' '}
+                  {regenerateMachineUserTokenState.error}
+                </Alert>
+              )}
+              {
+                <Button
+                  onClick={() => regenerateMachineUserTokenFunc()}
+                  btnType="danger"
+                  disabled={
+                    regenerateMachineUserTokenState.status ===
+                    requestStates.IN_PROGRESS
+                  }
+                >
+                  Regenerate token
+                </Button>
+              }
+            </div>
           </div>
-        </div>
-      </Toggler>
-    </Panel>
+        </Accordion.Panel>
+      </Accordion.Item>
+    </Accordion>
   );
 };
 
