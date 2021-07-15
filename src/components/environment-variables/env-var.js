393a69a4847858f1dd96f74f780c0df9544f7b0e
Sergey Smolnikov <ssmol@equinor.com>
2021-07-15 17:53:39 +0200

    Added env vars control

--- src/components/environment-variables/env-var.js
+++ src/components/environment-variables/env-var.js
@@ -1,18 +1,38 @@
 import PropTypes from 'prop-types';
 import React, { useState } from 'react';
 import environmentVariable from '../../models/environment-variable';
-import { Button, Icon, Table } from '@equinor/eds-core-react';
+import {
+  Button,
+  CircularProgress,
+  Icon,
+  Input,
+  Table,
+} from '@equinor/eds-core-react';
 import { edit, layers, restore } from '@equinor/eds-icons';
 import useSaveEnvVar from './use-save-env-var';
+import requestStates from '../../state/state-utils/request-states';
+import Alert from '../alert';
 
 const EnvironmentVariable = (props) => {
   const { appName, envName, componentName, includeRadixVars, envVar } = props;
-  const [savedEnvVar, setSavedEnvVar] = useState(props.envVar);
-  const [saveState, saveFunc, resetState] = useSaveEnvVar(props.appName);
+  const [envVarValue, setEnvVarValue] = useState(props.envVar.value);
+  const [savedEnvVarValue, setSavedEnvVar] = useState(props.envVar);
+  const [saveState, saveFunc, resetState] = useSaveEnvVar(
+    appName,
+    envName,
+    componentName,
+    envVar.name
+  );
   const handleSubmit = (ev) => {
     ev.preventDefault();
     saveFunc(envVar);
     setSavedEnvVar(envVar);
+  };
+  const setEnvVarAndResetSaveState = (varValue) => {
+    if (saveState.status !== requestStates.IDLE) {
+      resetState();
+    }
+    setEnvVarValue(varValue);
   };
 
   let hasRadixVars = false;
@@ -29,6 +49,44 @@ const EnvironmentVariable = (props) => {
                   <Table.Cell>
                     {envVar.metadata != null && <Icon data={layers} />}
                     <strong>{envVar.value}</strong>
+                    <form onSubmit={handleSubmit}>
+                      {saveState.status === requestStates.FAILURE && (
+                        <Alert type="danger" className="gap-bottom">
+                          Failed to change environment variable.
+                          {saveState.error}
+                        </Alert>
+                      )}
+                      <Input
+                        disabled={
+                          saveState.status === requestStates.IN_PROGRESS
+                        }
+                        type="text"
+                        value={envVarValue}
+                        onChange={(ev) =>
+                          setEnvVarAndResetSaveState(ev.target.value)
+                        }
+                      />
+                      <div className="o-action-bar">
+                        {saveState.status === requestStates.IN_PROGRESS && (
+                          <>
+                            <CircularProgress size="24" />
+                            <span className="progress">Updating…</span>
+                          </>
+                        )}
+                        {saveState.status !== requestStates.IN_PROGRESS && (
+                          <Button
+                            variant="ghost"
+                            color="primary"
+                            className="o-heading-page-button"
+                            type="submit"
+                            disabled={savedEnvVarValue === envVarValue}
+                          >
+                            <Icon data={edit} />
+                            Edit
+                          </Button>
+                        )}
+                      </div>
+                    </form>
                   </Table.Cell>
                 </Table.Row>
                 {envVar.metadata != null &&
@@ -44,27 +102,7 @@ const EnvironmentVariable = (props) => {
             </Table>
           )}
         </Table.Cell>
-        <Table.Cell>
-          <Button
-            variant="ghost"
-            color="primary"
-            className="o-heading-page-button"
-          >
-            <Icon data={edit} />
-            Edit
-          </Button>
-          {envVar.metadata != null &&
-            envVar.metadata.radixConfigValue != envVar.value && (
-              <Button
-                variant="ghost"
-                color="primary"
-                className="o-heading-page-button"
-              >
-                <Icon data={restore} />
-                Restore
-              </Button>
-            )}
-        </Table.Cell>
+        <Table.Cell></Table.Cell>
       </Table.Row>
     );
   } else if (includeRadixVars === true) {
