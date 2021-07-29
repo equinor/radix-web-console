6fcc4e670904b37675727a7dce3ef4ee17346dc0
Ole Markus Kristiansen Ternø <OLMT@equinor.com>
2021-07-29 11:08:59 +0200

    added eds breadcrumbs

--- src/components/page-deployment/deployment-bread-crumb.js
+++ src/components/page-deployment/deployment-bread-crumb.js
@@ -1,21 +1,24 @@
-import Breadcrumb from '../breadcrumb';
 import { routeWithParams, smallDeploymentName } from '../../utils/string';
 import routes from '../../routes';
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Breadcrumbs } from '@equinor/eds-core-react';
 
 const DeploymentBreadCrumb = ({ appName, deploymentName }) => {
   return (
-    <Breadcrumb
-      links={[
-        { label: appName, to: routeWithParams(routes.app, { appName }) },
-        {
-          label: 'Deployments',
-          to: routeWithParams(routes.appDeployments, { appName }),
-        },
-        { label: smallDeploymentName(deploymentName) },
-      ]}
-    />
+    <Breadcrumbs>
+      <Breadcrumbs.Breadcrumb href={routeWithParams(routes.app, { appName })}>
+        {appName}
+      </Breadcrumbs.Breadcrumb>
+      <Breadcrumbs.Breadcrumb
+        href={routeWithParams(routes.appDeployments, { appName })}
+      >
+        Deployments
+      </Breadcrumbs.Breadcrumb>
+      <Breadcrumbs.Breadcrumb>
+        {smallDeploymentName(deploymentName)}
+      </Breadcrumbs.Breadcrumb>
+    </Breadcrumbs>
   );
 };
 
