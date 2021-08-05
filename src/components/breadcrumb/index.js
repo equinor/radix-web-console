691dffb665e476a9d277a1a609eb76b0152ef3d5
Fredrik Hatletvedt <fhatletvedt@gmail.com>
2021-08-05 13:02:13 +0200

    fixed breadcrumb not adding space between non-links

--- src/components/breadcrumb/index.js
+++ src/components/breadcrumb/index.js
@@ -1,3 +1,4 @@
+import { List } from '@equinor/eds-core-react';
 import PropTypes from 'prop-types';
 import React from 'react';
 import { NavLink } from 'react-router-dom';
@@ -5,28 +6,25 @@ import { NavLink } from 'react-router-dom';
 import './style.css';
 
 const BreadcrumbLink = (link) => {
-  if (link.to) {
-    return (
-      <>
-        <NavLink className="breadcrumb__link" to={link.to}>
-          {link.label}
-        </NavLink>
-        <span className="breadcrumb__link-space">/</span>
-      </>
-    );
-  }
-
-  return <span className="breadcrumb__text">{link.label}</span>;
+  return link.to ? (
+    <NavLink className="breadcrumb__link" to={link.to}>
+      {link.label}
+    </NavLink>
+  ) : (
+    <span className="breadcrumb__text">{link.label}</span>
+  );
 };
 
 export const Breadcrumb = ({ links }) => {
   const linksRender = links.map((link, idx) => (
-    <li key={link.to || idx}>{BreadcrumbLink(link)}</li>
+    <List.Item className="breadcrumb__item" key={link.to || idx}>
+      {BreadcrumbLink(link)}
+    </List.Item>
   ));
 
   return (
     <nav className="breadcrumb" role="navigation" aria-label="Page hierarchy">
-      <ul>{linksRender}</ul>
+      <List>{linksRender}</List>
     </nav>
   );
 };
