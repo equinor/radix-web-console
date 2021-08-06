553ba79e1633cb0178e4f2ec30e15add8661c71c
Ole Markus Kristiansen Ternø <OLMT@equinor.com>
2021-08-06 15:37:28 +0200

    remove console log

--- src/components/layout-app/index.js
+++ src/components/layout-app/index.js
@@ -5,7 +5,6 @@ import './style.css';
 
 export class LayoutApp extends React.Component {
   render() {
-    console.log(this.props);
     return (
       <div className="grid layout-app">
         <div className="layout-app__sidebar">
