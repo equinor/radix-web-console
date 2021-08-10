a0350098abfafc05df235b59fd34bce7769ec80b
Ole Markus Kristiansen Ternø <olmt@equinor.com>
2021-08-10 13:53:59 +0200

    styled input section

--- src/components/secret-form/index.js
+++ src/components/secret-form/index.js
@@ -2,16 +2,17 @@ import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 
 import Alert from '../alert';
-import Button from '../button';
-import FormField from '../form-field';
-import Panel from '../panel';
 import SecretStatus from '../secret-status';
-import Spinner from '../spinner';
 
 import requestStates from '../../state/state-utils/request-states';
 
 import './style.css';
-import { Typography } from '@equinor/eds-core-react';
+import {
+  Button,
+  CircularProgress,
+  TextField,
+  Typography,
+} from '@equinor/eds-core-react';
 
 const STATUS_OK = 'Consistent';
 
@@ -70,48 +71,50 @@ const SecretForm = ({
             </div>
           </div>
           <div className="secret-overview-form">
-            <Panel>
-              <form
-                onSubmit={(ev) => {
-                  ev.preventDefault();
-                  handleSubmit(value);
-                }}
+            <form
+              onSubmit={(ev) => {
+                ev.preventDefault();
+                handleSubmit(value);
+              }}
+            >
+              <fieldset
+                disabled={saveState === requestStates.IN_PROGRESS}
+                className="grid grid--gap-small"
               >
-                <fieldset disabled={saveState === requestStates.IN_PROGRESS}>
-                  <FormField
-                    label="Secret value"
-                    help={getSecretFieldHelpText(secret)}
-                  >
-                    <textarea
-                      onChange={(ev) => setValue(ev.target.value)}
-                      value={value}
-                    />
-                  </FormField>
-                  {saveState === requestStates.FAILURE && (
+                <TextField
+                  label="Secret value"
+                  helperText={getSecretFieldHelpText(secret)}
+                  onChange={(ev) => setValue(ev.target.value)}
+                  value={value}
+                  multiline
+                />
+                {saveState === requestStates.FAILURE && (
+                  <div>
                     <Alert type="danger">Error while saving. {saveError}</Alert>
-                  )}
-                  {saveState === requestStates.SUCCESS && (
-                    <Alert type="info">Saved</Alert>
-                  )}
-                  <div className="o-action-bar">
-                    {saveState === requestStates.IN_PROGRESS && (
-                      <Spinner>Saving…</Spinner>
-                    )}
-                    <Button
-                      btnType="primary"
-                      type="submit"
-                      disabled={shouldFormBeDisabled(
-                        saveState,
-                        value,
-                        savedValue
-                      )}
-                    >
-                      Save
-                    </Button>
                   </div>
-                </fieldset>
-              </form>
-            </Panel>
+                )}
+                {saveState === requestStates.SUCCESS && (
+                  <div>
+                    <Alert type="info">Saved</Alert>
+                  </div>
+                )}
+                {saveState === requestStates.IN_PROGRESS && (
+                  <CircularProgress>Saving…</CircularProgress>
+                )}
+                <div>
+                  <Button
+                    type="submit"
+                    disabled={shouldFormBeDisabled(
+                      saveState,
+                      value,
+                      savedValue
+                    )}
+                  >
+                    Save
+                  </Button>
+                </div>
+              </fieldset>
+            </form>
           </div>
         </React.Fragment>
       )}
