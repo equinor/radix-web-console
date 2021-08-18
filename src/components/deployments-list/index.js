20c2c95cfb9ae10144502bab21a04db5f2a8f5e9
Ole Markus Kristiansen Ternø <olmt@equinor.com>
2021-08-18 13:30:55 +0200

    added table headers and moved header to page-env

--- src/components/deployments-list/index.js
+++ src/components/deployments-list/index.js
@@ -15,24 +15,27 @@ export const DeploymentsList = ({
   inEnv = false,
 }) => (
   <div className="deployments-list grid grid--gap-medium">
-    <Typography variant="h4">Previous deployments</Typography>
     {deployments.length > 0 ? (
       <div className="grid--table-overflow">
         <Table>
           <Table.Head>
             <Table.Row>
               <Table.Cell>Date / Time</Table.Cell>
+              <Table.Cell>Environment</Table.Cell>
+              <Table.Cell>Status</Table.Cell>
               <Table.Cell>ID</Table.Cell>
+              <Table.Cell>Type of job</Table.Cell>
               <Table.Cell>
                 Github commit <Icon data={external_link} />
               </Table.Cell>
+              <Table.Cell>Promoted from</Table.Cell>
             </Table.Row>
           </Table.Head>
           <Table.Body>
             {deployments
               .slice(0, limit || deployments.length)
-              .map((deployment) => (
-                <Table.Row key={deployment.name}>
+              .map((deployment, i) => (
+                <Table.Row key={i}>
                   <DeploymentSummary
                     appName={appName}
                     deployment={deployment}
