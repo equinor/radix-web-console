40d4bad5546b9e4e761c6592eb3a5575a21a41f1
Ole Markus Kristiansen Ternø <olmt@equinor.com>
2021-08-20 09:39:34 +0200

    fixed top level of configuration page

--- src/components/configure-application-github/index.js
+++ src/components/configure-application-github/index.js
@@ -97,146 +97,148 @@ export const ConfigureApplicationGithub = (props) => {
       <Typography>
         To integrate with GitHub you must add a deploy key and a webhook
       </Typography>
-      <Accordion chevronPosition="right" headerLevel="p" className="accordion">
-        <Accordion.Item isExpanded={isExpanded} className="accordion__item">
-          <Accordion.Header className="accordion__header">
-            {deployKeyTitle}
-          </Accordion.Header>
-          <Accordion.Panel className="accordion__panel">
-            <div className="accordion__content">
-              <Typography>
-                This allows Radix to clone the repository. Open the{' '}
-                <Typography
-                  link
-                  href={`${app.repository}/settings/keys/new`}
-                  rel="noopener noreferrer"
-                  target="_blank"
-                >
-                  Add New Deploy Key page
-                </Typography>{' '}
-                and follow the steps below
-              </Typography>
-              <div className="grid grid--gap-medium o-body-text">
-                <img
-                  alt="'Add deploy key' steps on GitHub"
-                  src={imageDeployKey}
-                  srcSet={`${imageDeployKey} 2x`}
-                />
-                <List variant="numbered">
-                  <List.Item>
-                    Give the key a name, e.g. "Radix deploy key"
-                  </List.Item>
-                  <List.Item>Copy and paste this key:</List.Item>
-                </List>
-                <Code copy wrap>
-                  {deployKey}
-                </Code>
-                <List variant="numbered" start="3">
-                  <List.Item>Press "Add key"</List.Item>
-                </List>
-              </div>
-              <div>
-                <div className="o-action-bar">
-                  {saveState.status === requestStates.FAILURE && (
-                    <Alert type="danger">
-                      Failed to regenerate deploy key and webhook secret.
-                      {saveState.error}
-                    </Alert>
-                  )}
-                  {saveState.status === requestStates.IN_PROGRESS ? (
-                    <>
-                      <Progress.Circular size={16} />
-                      <span className="progress">Regenerating...</span>
-                    </>
-                  ) : (
-                    <Button onClick={() => saveDeployKeySetting()}>
-                      Regenerate deploy key and webhook secret
-                    </Button>
-                  )}
-                </div>
-              </div>
-            </div>
-          </Accordion.Panel>
-        </Accordion.Item>
-      </Accordion>
-      {useOtherCiToolOptionVisible && (
-        <fieldset>
-          <FormField>
-            <Checkbox
-              name="deployOnly"
-              value={useOtherCiTool}
-              checked={useOtherCiTool}
-              onChange={() => setUseOtherCiTool(!useOtherCiTool)}
-            />
-            <span className="check-input-span">
-              <Typography group="input" variant="text">
-                Use other CI tool than Radix
-              </Typography>
-              <Typography group="navigation" variant="label">
-                {deployOnlyHelp}
-              </Typography>
-            </span>
-          </FormField>
-        </fieldset>
-      )}
-      {!useOtherCiTool && (
-        <Accordion
-          chevronPosition="right"
-          headerLevel="p"
-          className="accordion"
-        >
+      <div className="grid grid--gap-small">
+        <Accordion chevronPosition="right" headerLevel="p">
           <Accordion.Item isExpanded={isExpanded} className="accordion__item">
             <Accordion.Header className="accordion__header">
-              {webhookTitle}
+              {deployKeyTitle}
             </Accordion.Header>
             <Accordion.Panel className="accordion__panel">
               <div className="accordion__content">
                 <Typography>
-                  GitHub notifies Radix using a webhook whenever a code push is
-                  made. Open the{' '}
+                  This allows Radix to clone the repository. Open the{' '}
                   <Typography
                     link
-                    href={`${app.repository}/settings/hooks/new`}
+                    href={`${app.repository}/settings/keys/new`}
                     rel="noopener noreferrer"
                     target="_blank"
                   >
-                    Add Webhook page
+                    Add New Deploy Key page
                   </Typography>{' '}
                   and follow the steps below
                 </Typography>
                 <div className="grid grid--gap-medium o-body-text">
                   <img
-                    alt="'Add webhook' steps on GitHub"
-                    src={imageWebhook}
-                    srcSet={`${imageWebhook} 2x`}
+                    alt="'Add deploy key' steps on GitHub"
+                    src={imageDeployKey}
+                    srcSet={`${imageDeployKey} 2x`}
                   />
                   <List variant="numbered">
                     <List.Item>
-                      As Payload URL, use <code>{webhookURL}</code>{' '}
-                      <Button onClick={() => copyToClipboard(webhookURL)}>
-                        <Icon data={copy} size={12} />
-                        Copy
-                      </Button>
+                      Give the key a name, e.g. "Radix deploy key"
                     </List.Item>
-                    <List.Item>
-                      Choose <code>application/json</code> as Content type
-                    </List.Item>
-                    <List.Item>
-                      The Shared Secret for this application is{' '}
-                      <code>{sharedSecret}</code>{' '}
-                      <Button onClick={() => copyToClipboard(sharedSecret)}>
-                        <Icon data={copy} size={12} />
-                        Copy
-                      </Button>
-                    </List.Item>
-                    <List.Item>Press "Add webhook"</List.Item>
+                    <List.Item>Copy and paste this key:</List.Item>
                   </List>
+                  <Code copy wrap>
+                    {deployKey}
+                  </Code>
+                  <List variant="numbered" start="3">
+                    <List.Item>Press "Add key"</List.Item>
+                  </List>
+                </div>
+                <div>
+                  <div className="o-action-bar">
+                    {saveState.status === requestStates.FAILURE && (
+                      <Alert type="danger">
+                        Failed to regenerate deploy key and webhook secret.
+                        {saveState.error}
+                      </Alert>
+                    )}
+                    {saveState.status === requestStates.IN_PROGRESS ? (
+                      <>
+                        <Progress.Circular size={16} />
+                        <span className="progress">Regenerating...</span>
+                      </>
+                    ) : (
+                      <Button onClick={() => saveDeployKeySetting()}>
+                        Regenerate deploy key and webhook secret
+                      </Button>
+                    )}
+                  </div>
                 </div>
               </div>
             </Accordion.Panel>
           </Accordion.Item>
         </Accordion>
-      )}
+        {useOtherCiToolOptionVisible && (
+          <fieldset>
+            <FormField>
+              <Checkbox
+                name="deployOnly"
+                value={useOtherCiTool}
+                checked={useOtherCiTool}
+                onChange={() => setUseOtherCiTool(!useOtherCiTool)}
+              />
+              <span className="check-input-span">
+                <Typography group="input" variant="text">
+                  Use other CI tool than Radix
+                </Typography>
+                <Typography group="navigation" variant="label">
+                  {deployOnlyHelp}
+                </Typography>
+              </span>
+            </FormField>
+          </fieldset>
+        )}
+        {!useOtherCiTool && (
+          <Accordion
+            chevronPosition="right"
+            headerLevel="p"
+            className="accordion"
+          >
+            <Accordion.Item isExpanded={isExpanded} className="accordion__item">
+              <Accordion.Header className="accordion__header">
+                {webhookTitle}
+              </Accordion.Header>
+              <Accordion.Panel className="accordion__panel">
+                <div className="accordion__content">
+                  <Typography>
+                    GitHub notifies Radix using a webhook whenever a code push
+                    is made. Open the{' '}
+                    <Typography
+                      link
+                      href={`${app.repository}/settings/hooks/new`}
+                      rel="noopener noreferrer"
+                      target="_blank"
+                    >
+                      Add Webhook page
+                    </Typography>{' '}
+                    and follow the steps below
+                  </Typography>
+                  <div className="grid grid--gap-medium o-body-text">
+                    <img
+                      alt="'Add webhook' steps on GitHub"
+                      src={imageWebhook}
+                      srcSet={`${imageWebhook} 2x`}
+                    />
+                    <List variant="numbered">
+                      <List.Item>
+                        As Payload URL, use <code>{webhookURL}</code>{' '}
+                        <Button onClick={() => copyToClipboard(webhookURL)}>
+                          <Icon data={copy} size={12} />
+                          Copy
+                        </Button>
+                      </List.Item>
+                      <List.Item>
+                        Choose <code>application/json</code> as Content type
+                      </List.Item>
+                      <List.Item>
+                        The Shared Secret for this application is{' '}
+                        <code>{sharedSecret}</code>{' '}
+                        <Button onClick={() => copyToClipboard(sharedSecret)}>
+                          <Icon data={copy} size={12} />
+                          Copy
+                        </Button>
+                      </List.Item>
+                      <List.Item>Press "Add webhook"</List.Item>
+                    </List>
+                  </div>
+                </div>
+              </Accordion.Panel>
+            </Accordion.Item>
+          </Accordion>
+        )}
+      </div>
     </div>
   );
 };
