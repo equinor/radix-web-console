fd6c162094081def09ca2295034e6a2708bf26a0
Fredrik Hatletvedt <fhatletvedt@gmail.com>
2021-08-24 16:29:55 +0200

    fix components copy buttons

--- src/components/docker-image/index.js
+++ src/components/docker-image/index.js
@@ -1,7 +1,6 @@
-import React from 'react';
-
 import { Button, Icon } from '@equinor/eds-core-react';
 import { copy } from '@equinor/eds-icons';
+import React from 'react';
 
 import { copyToClipboard } from '../../utils/string';
 
@@ -10,21 +9,16 @@ import './style.css';
 const tagRegEx = /\/(.*?)$/;
 
 const DockerImage = ({ path }) => {
-  let imageParts = tagRegEx.exec(path);
-  let tag = path;
+  const imageParts = tagRegEx.exec(path);
 
-  // If image is prefixed with container registry, strip
-  // that part from the tag
-  if (imageParts) {
-    tag = imageParts[1];
-  }
+  // If image is prefixed with container registry, strip that part from the tag
+  const tag = imageParts ? imageParts[1] : path;
 
   return (
     <>
       <strong>{tag}</strong>{' '}
-      <Button onClick={() => copyToClipboard(tag)} variant="ghost">
-        Copy
-        <Icon data={copy} />
+      <Button variant="ghost" onClick={() => copyToClipboard(tag)}>
+        <Icon data={copy} /> Copy
       </Button>
     </>
   );
