0c6c3161a5e2c1da49b625923f46a7949f166139
Nils Gustav Stråbø <nst@equinor.com>
2021-11-11 12:57:59 +0100

    fix error in function

--- src/components/alerting/edit-alerting.js
+++ src/components/alerting/edit-alerting.js
@@ -3,15 +3,12 @@ import PropTypes from 'prop-types';
 import { TextField } from '@equinor/eds-core-react';
 import * as React from 'react';
 
-const buildSlackReceiverNamesFromConfig = (config) => {
-  if (!config && !config.receivers) {
-    return [];
-  }
-
-  return Object.entries(config.receivers)
-    .filter((e) => e[1].slackConfig)
-    .map((e) => e[0]);
-};
+const buildSlackReceiverNamesFromConfig = (config) =>
+  config?.receivers
+    ? Object.entries(config.receivers)
+        .filter((e) => e[1].slackConfig)
+        .map((e) => e[0])
+    : [];
 
 const useBuildSlackReceiverNames = (config) => {
   const [slackReceivers, setSlackReceivers] = React.useState([]);
