9c78c12d03894578d6f152763661bd0c448d8cea
Ole Markus Kristiansen Ternø <olmt@equinor.com>
2021-11-18 12:33:54 +0100

    shorten and optimize

--- src/utils/config/index.js
+++ src/utils/config/index.js
@@ -1,19 +1,13 @@
 import * as configKeys from './keys';
 import * as jsonConfig from '../../config.json';
 
-const keys = configKeys.keys;
-var arr = [];
-Object.keys(keys).forEach(function (key) {
-  arr.push(keys[key]);
-});
-
-const configVariables = {};
-
-arr.map((c) => {
-  var value = window[c];
-  if (!window[c] || window[c].startsWith('${')) {
-    value = jsonConfig.default[c];
-  }
-  return (configVariables[c] = value);
-});
+const configVariables = Object.keys(configKeys.keys)
+  .map((key) => configKeys.keys[key])
+  .reduce((config, key) => {
+    config[key] =
+      !window[key] || window[key].startsWith('${')
+        ? jsonConfig.default[key]
+        : window[key];
+    return config;
+  }, {});
 export default configVariables;
