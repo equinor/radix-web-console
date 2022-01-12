686832b00284bf5edebc0f79105c7e07914e3468
Sergey Smolnikov <ssmol@equinor.com>
2022-01-12 14:49:39 +0100

    Fexed UI, rolled back proxy image version

--- src/components/component/active-component-secrets.js
+++ src/components/component/active-component-secrets.js
@@ -33,7 +33,7 @@ const ActiveComponentSecrets = ({
             );
             return (
               <List.Item key={secretName}>
-                {envSecret.status !== 'External' && (
+                {envSecret.status !== 'External' ? (
                   <Link
                     to={routing.getSecretUrl(
                       appName,
@@ -46,8 +46,7 @@ const ActiveComponentSecrets = ({
                       {secretName}
                     </Typography>
                   </Link>
-                )}
-                {envSecret.status === 'External' && (
+                ) : (
                   <Typography as="span">{secretName}</Typography>
                 )}{' '}
                 {envSecret.resource && (
