b166ac3835015a0de1bc778d010e6024bf298155
Fredrik Hatletvedt <fhatletvedt@gmail.com>
2022-02-11 16:50:47 +0100

    fix component list returning array and not element

--- src/components/page-environment/component-list.js
+++ src/components/page-environment/component-list.js
@@ -1,9 +1,9 @@
 import { Accordion, Table, Typography } from '@equinor/eds-core-react';
-import PropTypes from 'prop-types';
+import * as PropTypes from 'prop-types';
 import { useEffect, useState } from 'react';
 
-import ComponentListItem from './component-list-item';
-import ComponentItem from '../../models/component-summary';
+import { ComponentListItem } from './component-list-item';
+import componentModel from '../../models/component';
 import {
   buildComponentMap,
   buildComponentTypeLabelPluralMap,
@@ -14,45 +14,49 @@ export const ComponentList = ({ appName, environment, components }) => {
   const [compMap, setCompMap] = useState({});
   useEffect(() => setCompMap(buildComponentMap(components)), [components]);
 
-  return Object.keys(compMap).map((componentType) => (
-    <Accordion.Item
-      className="accordion elevated"
-      isExpanded
-      key={componentType}
-    >
-      <Accordion.Header>
-        <Typography variant="h4">
-          Active {buildComponentTypeLabelPluralMap(componentType)}
-        </Typography>
-      </Accordion.Header>
-      <Accordion.Panel>
-        <div className="grid grid--table-overflow">
-          <Table>
-            <Table.Head>
-              <Table.Row>
-                <Table.Cell>ID</Table.Cell>
-                <Table.Cell>Status</Table.Cell>
-                <Table.Cell>Replicas</Table.Cell>
-              </Table.Row>
-            </Table.Head>
-            <Table.Body>
-              <ComponentListItem
-                appName={appName}
-                environment={environment}
-                components={compMap[componentType]}
-              ></ComponentListItem>
-            </Table.Body>
-          </Table>
-        </div>
-      </Accordion.Panel>
-    </Accordion.Item>
-  ));
+  return (
+    <>
+      {Object.keys(compMap).map((componentType) => (
+        <Accordion.Item
+          className="accordion elevated"
+          isExpanded
+          key={componentType}
+        >
+          <Accordion.Header>
+            <Typography variant="h4">
+              Active {buildComponentTypeLabelPluralMap(componentType)}
+            </Typography>
+          </Accordion.Header>
+          <Accordion.Panel>
+            <div className="grid grid--table-overflow">
+              <Table>
+                <Table.Head>
+                  <Table.Row>
+                    <Table.Cell>ID</Table.Cell>
+                    <Table.Cell>Status</Table.Cell>
+                    <Table.Cell>Replicas</Table.Cell>
+                  </Table.Row>
+                </Table.Head>
+                <Table.Body>
+                  <ComponentListItem
+                    appName={appName}
+                    environment={environment}
+                    components={compMap[componentType]}
+                  ></ComponentListItem>
+                </Table.Body>
+              </Table>
+            </div>
+          </Accordion.Panel>
+        </Accordion.Item>
+      ))}
+    </>
+  );
 };
 
 ComponentList.propTypes = {
   appName: PropTypes.string.isRequired,
   environment: PropTypes.shape(environmentModel),
-  components: PropTypes.arrayOf(PropTypes.shape(ComponentItem)).isRequired,
+  components: PropTypes.arrayOf(PropTypes.shape(componentModel)).isRequired,
 };
 
 export default ComponentList;
