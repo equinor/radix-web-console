57926fb8c29833c2d5fff0853565d197f2fe2b36
Fredrik Hatletvedt <fhatletvedt@gmail.com>
2022-03-25 15:19:17 +0100

    cleanup ComponentSecret component

--- src/components/component/component-secrets.js
+++ src/components/component/component-secrets.js
@@ -1,28 +1,23 @@
 import { List, Typography } from '@equinor/eds-core-react';
-import { connect } from 'react-redux';
 import * as PropTypes from 'prop-types';
 
 import Component from '../../models/component';
-import { getEnvironment } from '../../state/environment';
 
-const mapStateToProps = (state) => ({
-  environment: getEnvironment(state),
-});
-
-const ComponentSecrets = ({ component }) => (
+export const ComponentSecrets = ({ component }) => (
   <>
     <Typography variant="h4">Secrets</Typography>
-    {component && component.secrets.length === 0 && (
-      <Typography variant="body_short">
-        This {component.type} uses no secrets
-      </Typography>
-    )}
-    {component && component.secrets.length > 0 && (
-      <List className="o-indent-list secrets">
-        {component.secrets.map((secret) => (
-          <List.Item key={secret}>{secret}</List.Item>
-        ))}
-      </List>
+    {component && (
+      <>
+        {component?.secrets.length > 0 ? (
+          <List className="o-indent-list secrets">
+            {component.secrets.map((secret) => (
+              <List.Item key={secret}>{secret}</List.Item>
+            ))}
+          </List>
+        ) : (
+          <Typography>This {component.type} uses no secrets</Typography>
+        )}
+      </>
     )}
   </>
 );
@@ -30,5 +25,3 @@ const ComponentSecrets = ({ component }) => (
 ComponentSecrets.propTypes = {
   component: PropTypes.shape(Component),
 };
-
-export default connect(mapStateToProps)(ComponentSecrets);
