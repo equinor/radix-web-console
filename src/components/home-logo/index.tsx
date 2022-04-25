2bbc4877f4c83dc2b3e331b2932373a10843db73
Fredrik Hatletvedt <fhatletvedt@gmail.com>
2022-04-25 13:54:40 +0200

    show 'Radix Platform 2' tab when applicable

--- src/components/home-logo/index.tsx
+++ src/components/home-logo/index.tsx
@@ -1,10 +1,17 @@
+import { Typography } from '@equinor/eds-core-react';
+
 import { HomeIcon } from '../home-icon';
 import { routes } from '../../routes';
 
-import './style.css';
-
-export const HomeLogo = () => (
-  <a href={routes.home} className="home-logo">
+export const HomeLogo = (): JSX.Element => (
+  <Typography
+    link
+    href={routes.home}
+    token={{
+      color: 'var(--eds_text_static_icons__default)',
+      textDecoration: 'none',
+    }}
+  >
     <HomeIcon /> Omnia Radix
-  </a>
+  </Typography>
 );
