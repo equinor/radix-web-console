7bb92d9fcc379b8af3dcb10249d527d04506f896
Fredrik Hatletvedt <fhatletvedt@gmail.com>
2022-05-11 10:01:22 +0200

    cleanup use-interval

--- src/effects/use-interval.ts
+++ src/effects/use-interval.ts
@@ -3,20 +3,17 @@ import { useEffect, useRef } from 'react';
 export function useInterval(callback: () => void, delay?: number): void {
   const savedCallback = useRef<() => void>();
 
-  // Remember the latest callback.
+  // Remember latest callback.
   useEffect(() => {
     savedCallback.current = callback;
   }, [callback]);
 
-  // Set up the interval.
+  // Set up interval.
   useEffect(() => {
-    function tick() {
-      savedCallback.current();
-    }
     if (delay !== null) {
-      const id = setInterval(tick, delay);
+      const id = setInterval(() => savedCallback.current(), delay);
       return () => clearInterval(id);
     }
-    return () => {}; // noop
+    return () => {};
   }, [delay]);
 }
