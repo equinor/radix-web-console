22100dbbe7e805f06388d5e7c9468e0229aef4d7
Fredrik Hatletvedt <fhatletvedt@gmail.com>
2022-05-12 12:55:37 +0200

    add vulnerability scan overview to active components and jobs list

--- src/components/page-environment/component-list.tsx
+++ src/components/page-environment/component-list.tsx
@@ -1,9 +1,14 @@
 import { Accordion, Table, Typography } from '@equinor/eds-core-react';
 import * as PropTypes from 'prop-types';
 import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
-import { ComponentListItemRow } from './component-list-item';
+import { useGetEnvironmentVulnerabilities } from './use-get-environment-vulnerabilities';
 
+import { SimpleAsyncResource } from '../async-resource/simple-async-resource';
+import { ComponentStatusBadge } from '../status-badges';
+import { ReplicaStatusTooltip } from '../status-tooltips';
+import { VulnerabilitySummary } from '../vulnerability-summary';
 import {
   ComponentModel,
   ComponentModelValidationMap,
@@ -11,11 +16,23 @@ import {
 import {
   buildComponentMap,
   buildComponentTypeLabelPlural,
+  ComponentType,
 } from '../../models/component-type';
 import {
   EnvironmentModel,
   EnvironmentModelValidationMap,
 } from '../../models/environment';
+import {
+  EnvironmentScanObjectModel,
+  EnvironmentScanSummaryModel,
+} from '../../models/environment-scan-summary';
+import { ReplicaSummaryNormalizedModel } from '../../models/replica-summary';
+import {
+  getActiveComponentUrl,
+  getActiveJobComponentUrl,
+  getReplicaUrl,
+} from '../../utils/routing';
+import { smallReplicaName } from '../../utils/string';
 
 import './style.css';
 
@@ -24,6 +41,77 @@ export interface ComponentListProps {
   environment?: EnvironmentModel;
   components: Array<ComponentModel>;
 }
+
+function getComponentUrl(
+  appName: string,
+  environment: EnvironmentModel,
+  component: ComponentModel
+): string {
+  return component.type === ComponentType.job
+    ? getActiveJobComponentUrl(appName, environment.name, component.name)
+    : getActiveComponentUrl(appName, environment.name, component.name);
+}
+
+function getComponentScanModel(
+  data: EnvironmentScanSummaryModel,
+  name: string,
+  type: ComponentType
+): EnvironmentScanObjectModel {
+  const compData =
+    data && data[type === ComponentType.component ? 'components' : 'jobs'];
+  return compData && compData[name];
+}
+
+const ReplicaLinks = ({
+  appName,
+  envName,
+  componentName,
+  replicaList,
+}: {
+  appName: string;
+  envName: string;
+  componentName: string;
+  replicaList?: Array<ReplicaSummaryNormalizedModel>;
+}): JSX.Element =>
+  replicaList?.length > 0 ? (
+    <span className="grid grid--auto-columns grid--gap-small">
+      {replicaList.map((x, i) => (
+        <Link
+          key={i}
+          className="component-replica__link"
+          to={getReplicaUrl(appName, envName, componentName, x.name)}
+        >
+          <ReplicaStatusTooltip status={x.status} />{' '}
+          <Typography link as="span">
+            {smallReplicaName(x.name)}
+          </Typography>
+        </Link>
+      ))}
+    </span>
+  ) : (
+    <Typography>No active replicas</Typography>
+  );
+
+const ComponentScanSummary = ({
+  scan,
+}: {
+  scan: EnvironmentScanObjectModel;
+}): JSX.Element =>
+  scan?.scanSuccess ? (
+    <VulnerabilitySummary summary={scan?.vulnerabilitySummary} />
+  ) : (
+    <Typography
+      group="table"
+      variant="cell_text"
+      {...(scan?.scanSuccess === false && { color: 'warning' })}
+    >
+      {!scan
+        ? 'No data'
+        : scan.scanSuccess === false
+        ? 'Scan failed'
+        : 'Not scanned'}
+    </Typography>
+  );
 
 export const ComponentList = ({
   appName,
@@ -34,6 +122,11 @@ export const ComponentList = ({
     [key: string]: Array<ComponentModel>;
   }>({});
   useEffect(() => setCompMap(buildComponentMap(components)), [components]);
+
+  const [environmentVulnerabilities] = useGetEnvironmentVulnerabilities(
+    appName,
+    environment.name
+  );
 
   return (
     <>
@@ -57,16 +150,47 @@ export const ComponentList = ({
                       <Table.Cell>ID</Table.Cell>
                       <Table.Cell>Status</Table.Cell>
                       <Table.Cell>Replicas</Table.Cell>
+                      <Table.Cell>Vulnerabilities</Table.Cell>
                     </Table.Row>
                   </Table.Head>
                   <Table.Body>
                     {compMap[type].map((x, i) => (
-                      <ComponentListItemRow
-                        key={i}
-                        appName={appName}
-                        environment={environment}
-                        component={x}
-                      />
+                      <Table.Row key={i}>
+                        <Table.Cell>
+                          <Link to={getComponentUrl(appName, environment, x)}>
+                            <Typography link as="span">
+                              {x.name}
+                            </Typography>
+                          </Link>
+                        </Table.Cell>
+                        <Table.Cell>
+                          <ComponentStatusBadge status={x.status} />
+                        </Table.Cell>
+                        <Table.Cell>
+                          <ReplicaLinks
+                            appName={appName}
+                            envName={environment.name}
+                            componentName={x.name}
+                            replicaList={x.replicaList}
+                          />
+                        </Table.Cell>
+                        <Table.Cell>
+                          <SimpleAsyncResource
+                            asyncState={environmentVulnerabilities}
+                            customError={
+                              <samp>{environmentVulnerabilities.error}</samp>
+                            }
+                          >
+                            <ComponentScanSummary
+                              scan={getComponentScanModel(
+                                environmentVulnerabilities.data,
+                                x.name,
+                                x.type
+                              )}
+                            />
+                          </SimpleAsyncResource>
+                        </Table.Cell>
+                      </Table.Row>
                     ))}
                   </Table.Body>
                 </Table>
