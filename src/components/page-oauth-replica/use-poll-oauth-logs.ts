8cbd43ac5b54f33c0aee2542de23d610b5a402a3
Fredrik Hatletvedt <fhatletvedt@gmail.com>
2022-05-18 12:39:33 +0200

    cleanup effects using usePollingPlain

--- src/components/page-oauth-replica/use-poll-oauth-logs.ts
+++ src/components/page-oauth-replica/use-poll-oauth-logs.ts
@@ -1,18 +1,19 @@
 import { usePollingPlain } from '../../effects';
+import { AsyncPollingResult } from '../../effects/use-async-polling';
 
-export const usePollOAuthLogs = (
+export function usePollOAuthLogs(
   appName: string,
   envName: string,
   componentName: string,
   replicaName: string
-) => {
+): AsyncPollingResult<Readonly<string>> {
   const encAppName = encodeURIComponent(appName);
   const encEnvName = encodeURIComponent(envName);
   const encComponentName = encodeURIComponent(componentName);
   const encReplicaName = encodeURIComponent(replicaName);
 
-  return usePollingPlain<string>(
+  return usePollingPlain(
     `/applications/${encAppName}/environments/${encEnvName}/components/${encComponentName}/aux/oauth/replicas/${encReplicaName}/logs`,
     5000
   );
-};
+}
--- src/components/page-scheduled-batch/use-poll-batch-logs.ts
+++ src/components/page-scheduled-batch/use-poll-batch-logs.ts
@@ -1,18 +1,19 @@
 import { usePollingPlain } from '../../effects';
+import { AsyncPollingResult } from '../../effects/use-async-polling';
 
-export const usePollBatchLogs = (
+export function usePollBatchLogs(
   appName: string,
   envName: string,
   jobComponentName: string,
   scheduledBatchName: string
-) => {
+): AsyncPollingResult<Readonly<string>> {
   const encAppName = encodeURIComponent(appName);
   const encEnvName = encodeURIComponent(envName);
   const encJobComponentName = encodeURIComponent(jobComponentName);
   const encScheduledBatchName = encodeURIComponent(scheduledBatchName);
 
-  return usePollingPlain<string>(
+  return usePollingPlain(
     `/applications/${encAppName}/environments/${encEnvName}/jobcomponents/${encJobComponentName}/scheduledjobs/${encScheduledBatchName}/logs`,
     5000
   );
-};
+}
