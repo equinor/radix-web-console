de49dade2463d73bc4608e6cb04d792dab50eb9a
Sergey Smolnikov <ssmol@equinor.com>
2022-05-24 17:23:52 +0200

    Update src/state/pipeline-run-task-steps/index.js
    Co-authored-by: Fredrik Hatletvedt <32248439+Pespiri@users.noreply.github.com>

--- src/state/pipeline-run-task-steps/index.js
+++ src/state/pipeline-run-task-steps/index.js
@@ -1,7 +1,4 @@
-import { makeLocalGetter } from '../../utils/object';
 import get from 'lodash/get';
-
-const localGetter = makeLocalGetter('pipelineRunTaskSteps');
 
 export const getPipelineRunTaskSteps = (state) =>
   get(state, 'pipelineRunTaskSteps', []);
