07bab0e6cc6ada5a812202e5ca9e299b6c92fa0d
Sergey Smolnikov <ssmol@equinor.com>
2022-05-24 13:54:33 +0200

    Update src/components/component/execution-state.tsx
    Co-authored-by: Fredrik Hatletvedt <32248439+Pespiri@users.noreply.github.com>

--- src/components/component/execution-state.tsx
+++ src/components/component/execution-state.tsx
@@ -1,6 +1,6 @@
 import { ProgressStatus } from '../../models/progress-status';
 
-export const getExecutionState = (status) => {
+export const getExecutionState = (status: ProgressStatus): string => {
   switch (status) {
     case ProgressStatus.Queued:
       return 'will execute';
