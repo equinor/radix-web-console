978fbb8f47f0d45c0f59c8108c6a485bb0fdf07c
Sergey Smolnikov <ssmol@equinor.com>
2022-05-24 17:24:16 +0200

    Update src/state/pipeline-run-tasks/index.js
    Co-authored-by: Fredrik Hatletvedt <32248439+Pespiri@users.noreply.github.com>

--- src/state/pipeline-run-tasks/index.js
+++ src/state/pipeline-run-tasks/index.js
@@ -1,7 +1,4 @@
-import { makeLocalGetter } from '../../utils/object';
 import get from 'lodash/get';
-
-const localGetter = makeLocalGetter('pipelineRunTasks');
 
 export const getPipelineRunTasks = (state) =>
   get(state, 'pipelineRunTasks', []);
