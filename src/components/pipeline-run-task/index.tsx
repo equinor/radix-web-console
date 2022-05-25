7db221b048e209dd6b72c16fa1a7a3e55e7b10fc
Sergey Smolnikov <ssmol@equinor.com>
2022-05-25 17:13:57 +0200

    Update src/components/pipeline-run-task/index.tsx
    Co-authored-by: Fredrik Hatletvedt <32248439+Pespiri@users.noreply.github.com>

--- src/components/pipeline-run-task/index.tsx
+++ src/components/pipeline-run-task/index.tsx
@@ -18,60 +18,51 @@ export const PipelineRunTask = (props: PipelineRunTaskProps): JSX.Element => {
   const { task } = props;
   const [now] = useState(new Date());
   return (
-    <>
-      <main className="grid grid--gap-large">
-        <>
-          {!task ? (
-            <Typography variant="h4">No task…</Typography>
-          ) : (
-            <>
-              <section className="grid grid--gap-medium">
-                <Typography variant="h4">Overview</Typography>
-                <div className="grid grid--gap-medium grid--overview-columns">
-                  <div className="grid grid--gap-medium">
-                    <Typography>
-                      Task <strong>{task.status.toLowerCase()};</strong>
-                    </Typography>
-                    <Typography>
-                      {getExecutionState(task.status)} task{' '}
-                      <strong>{task.name}</strong>
-                    </Typography>
-                  </div>
-                  {task.started && (
-                    <div className="grid grid--gap-medium">
-                      <Typography>
-                        Started{' '}
-                        <strong>
-                          <RelativeToNow time={task.started} />
-                        </strong>
-                      </Typography>
-                      {task.ended ? (
-                        <Typography>
-                          Task took{' '}
-                          <strong>
-                            <Duration start={task.started} end={task.ended} />
-                          </strong>
-                        </Typography>
-                      ) : (
-                        <Typography>
-                          Duration so far is{' '}
-                          <strong>
-                            <Duration
-                              start={task.started}
-                              end={task.ended ?? now}
-                            />
-                          </strong>
-                        </Typography>
-                      )}
-                    </div>
-                  )}
-                </div>
-              </section>
-            </>
-          )}
-        </>
-      </main>
-    </>
+    <main className="grid grid--gap-large">
+      {!task ? (
+        <Typography variant="h4">No task…</Typography>
+      ) : (
+        <section className="grid grid--gap-medium">
+          <Typography variant="h4">Overview</Typography>
+          <div className="grid grid--gap-medium grid--overview-columns">
+            <div className="grid grid--gap-medium">
+              <Typography>
+                Task <strong>{task.status.toLowerCase()};</strong>
+              </Typography>
+              <Typography>
+                {getExecutionState(task.status)} task{' '}
+                <strong>{task.name}</strong>
+              </Typography>
+            </div>
+            {task.started && (
+              <div className="grid grid--gap-medium">
+                <Typography>
+                  Started{' '}
+                  <strong>
+                    <RelativeToNow time={task.started} />
+                  </strong>
+                </Typography>
+                {task.ended ? (
+                  <Typography>
+                    Task took{' '}
+                    <strong>
+                      <Duration start={task.started} end={task.ended} />
+                    </strong>
+                  </Typography>
+                ) : (
+                  <Typography>
+                    Duration so far is{' '}
+                    <strong>
+                      <Duration start={task.started} end={task.ended ?? now} />
+                    </strong>
+                  </Typography>
+                )}
+              </div>
+            )}
+          </div>
+        </section>
+      )}
+    </main>
   );
 };
 
