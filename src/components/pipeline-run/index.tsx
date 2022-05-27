1143d7170f5a5ec3feb4f15e58781ae222f2c1f6
Sergey Smolnikov <ssmol@equinor.com>
2022-05-27 10:24:08 +0200

    Update src/components/pipeline-run/index.tsx
    Co-authored-by: Fredrik Hatletvedt <32248439+Pespiri@users.noreply.github.com>

--- src/components/pipeline-run/index.tsx
+++ src/components/pipeline-run/index.tsx
@@ -18,67 +18,61 @@ export const PipelineRun = (props: PipelineRunProps): JSX.Element => {
   const { pipelineRun } = props;
   const [now] = useState(new Date());
   return (
-    <>
-      <main className="grid grid--gap-large">
-        <>
-          {!pipelineRun ? (
-            <Typography variant="h4">No pipeline run…</Typography>
-          ) : (
-            <>
-              <section className="grid grid--gap-medium">
-                <Typography variant="h4">Overview</Typography>
-                <div className="grid grid--gap-medium grid--overview-columns">
-                  <div className="grid grid--gap-medium">
-                    <Typography>
-                      Pipeline run{' '}
-                      <strong>{pipelineRun.status.toLowerCase()};</strong>
-                    </Typography>
-                    <Typography>
-                      {getExecutionState(pipelineRun.status)} pipeline{' '}
-                      <strong>{pipelineRun.name}</strong>
-                    </Typography>
-                    <Typography>
-                      Environment <strong>{pipelineRun.env}</strong>
-                    </Typography>
-                  </div>
-                  {pipelineRun.started && (
-                    <div className="grid grid--gap-medium">
-                      <Typography>
-                        Started{' '}
-                        <strong>
-                          <RelativeToNow time={pipelineRun.started} />
-                        </strong>
-                      </Typography>
-                      {pipelineRun.ended ? (
-                        <Typography>
-                          Pipeline run took{' '}
-                          <strong>
-                            <Duration
-                              start={pipelineRun.started}
-                              end={pipelineRun.ended}
-                            />
-                          </strong>
-                        </Typography>
-                      ) : (
-                        <Typography>
-                          Duration so far is{' '}
-                          <strong>
-                            <Duration
-                              start={pipelineRun.started}
-                              end={pipelineRun.ended ?? now}
-                            />
-                          </strong>
-                        </Typography>
-                      )}
-                    </div>
-                  )}
-                </div>
-              </section>
-            </>
-          )}
-        </>
-      </main>
-    </>
+    <main className="grid grid--gap-large">
+      {!pipelineRun ? (
+        <Typography variant="h4">No pipeline run…</Typography>
+      ) : (
+        <section className="grid grid--gap-medium">
+          <Typography variant="h4">Overview</Typography>
+          <div className="grid grid--gap-medium grid--overview-columns">
+            <div className="grid grid--gap-medium">
+              <Typography>
+                Pipeline run{' '}
+                <strong>{pipelineRun.status.toLowerCase()};</strong>
+              </Typography>
+              <Typography>
+                {getExecutionState(pipelineRun.status)} pipeline{' '}
+                <strong>{pipelineRun.name}</strong>
+              </Typography>
+              <Typography>
+                Environment <strong>{pipelineRun.env}</strong>
+              </Typography>
+            </div>
+            {pipelineRun.started && (
+              <div className="grid grid--gap-medium">
+                <Typography>
+                  Started{' '}
+                  <strong>
+                    <RelativeToNow time={pipelineRun.started} />
+                  </strong>
+                </Typography>
+                {pipelineRun.ended ? (
+                  <Typography>
+                    Pipeline run took{' '}
+                    <strong>
+                      <Duration
+                        start={pipelineRun.started}
+                        end={pipelineRun.ended}
+                      />
+                    </strong>
+                  </Typography>
+                ) : (
+                  <Typography>
+                    Duration so far is{' '}
+                    <strong>
+                      <Duration
+                        start={pipelineRun.started}
+                        end={pipelineRun.ended ?? now}
+                      />
+                    </strong>
+                  </Typography>
+                )}
+              </div>
+            )}
+          </div>
+        </section>
+      )}
+    </main>
   );
 };
 
