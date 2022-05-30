81de0ae46779979352a3c2038da69d25993e07fb
Fredrik Hatletvedt <fhatletvedt@gmail.com>
2022-05-30 15:55:01 +0200

    update and wrap component horizontal scaling summary in accordion

---
+++ src/components/page-active-component/horizontal-scaling-summary.tsx
@@ -0,0 +1,81 @@
+import { Accordion, Typography } from '@equinor/eds-core-react';
+import * as PropTypes from 'prop-types';
+
+import {
+  HorizontalScalingSummaryModel,
+  HorizontalScalingSummaryModelValidationMap,
+} from '../../models/horizontal-scaling-summary';
+import { isNullOrUndefined } from '../../utils/object';
+
+export interface HorizontalScalingSummaryProps {
+  data?: HorizontalScalingSummaryModel;
+}
+
+export const HorizontalScalingSummary = ({
+  data,
+}: HorizontalScalingSummaryProps): JSX.Element => (
+  <>
+    {data && (
+      <Accordion className="accordion elevated" chevronPosition="right">
+        <Accordion.Item isExpanded>
+          <Accordion.Header>
+            <Accordion.HeaderTitle>
+              <Typography className="whitespace-nowrap" variant="h4" as="span">
+                Horizontal scaling
+              </Typography>
+            </Accordion.HeaderTitle>
+          </Accordion.Header>
+          <Accordion.Panel>
+            <div className="grid grid--gap-medium">
+              <dl className="o-key-values">
+                {!isNullOrUndefined(data.minReplicas) && (
+                  <>
+                    <Typography as="dt">Min replicas:</Typography>
+                    <Typography as="dd" variant="body_short_bold">
+                      {data.minReplicas}
+                    </Typography>
+                  </>
+                )}
+
+                {!isNullOrUndefined(data.maxReplicas) && (
+                  <>
+                    <Typography as="dt">Max replicas:</Typography>
+                    <Typography as="dd" variant="body_short_bold">
+                      {data.maxReplicas}
+                    </Typography>
+                  </>
+                )}
+
+                {!isNullOrUndefined(data.currentCPUUtilizationPercentage) && (
+                  <>
+                    <Typography as="dt">
+                      Current average CPU utilization:
+                    </Typography>
+                    <Typography as="dd" variant="body_short_bold">
+                      {data.currentCPUUtilizationPercentage}%
+                    </Typography>
+                  </>
+                )}
+
+                {!isNullOrUndefined(data.targetCPUUtilizationPercentage) && (
+                  <>
+                    <Typography as="dt">
+                      Target average CPU utilization:
+                    </Typography>
+                    <Typography as="dd" variant="body_short_bold">
+                      {data.targetCPUUtilizationPercentage}%
+                    </Typography>
+                  </>
+                )}
+              </dl>
+            </div>
+          </Accordion.Panel>
+        </Accordion.Item>
+      </Accordion>
+    )}
+  </>
+);
+
+HorizontalScalingSummary.propTypes = {
+  data: PropTypes.shape(HorizontalScalingSummaryModelValidationMap),
+} as PropTypes.ValidationMap<HorizontalScalingSummaryProps>;
