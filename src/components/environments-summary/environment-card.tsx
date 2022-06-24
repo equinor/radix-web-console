0de77d02c1663d841b52338e3957def1842c6492
Fredrik Hatletvedt <fhatletvedt@gmail.com>
2022-06-24 12:08:15 +0200

    revamp environment card status

--- src/components/environments-summary/environment-card.tsx
+++ src/components/environments-summary/environment-card.tsx
@@ -1,32 +1,19 @@
-import {
-  Button,
-  Divider,
-  Icon,
-  Tooltip,
-  Typography,
-} from '@equinor/eds-core-react';
+import { Button, Divider, Icon, Typography } from '@equinor/eds-core-react';
 import { link, send } from '@equinor/eds-icons';
 import * as PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
-import {
-  EnvironmentCardBadgeStatus,
-  EnvironmentCardBadge,
-} from './environment-card-badge';
+import { EnvironmentCardStatus } from './environment-card-status';
 import { EnvironmentIngress } from './environment-ingress';
 import { useGetComponents } from './use-get-components';
 
 import { SimpleAsyncResource } from '../async-resource/simple-async-resource';
 import { RelativeToNow } from '../time/relative-to-now';
-import { ComponentModel } from '../../models/component';
-import { ComponentStatus } from '../../models/component-status';
 import { ConfigurationStatus } from '../../models/configuration-status';
 import {
   EnvironmentSummaryModel,
   EnvironmentSummaryModelValidationMap,
 } from '../../models/environment-summary';
-import { ReplicaStatus } from '../../models/replica-status';
-import { ReplicaSummaryNormalizedModel } from '../../models/replica-summary';
 import { routes } from '../../routes';
 import { routeWithParams } from '../../utils/string';
 
@@ -38,17 +25,6 @@ export interface EnvironmentCardProps {
   appName: string;
   env: EnvironmentSummaryModel;
 }
-
-const ComponentCardStatus = {
-  [ComponentStatus.ConsistentComponent]: EnvironmentCardBadgeStatus.Consistent,
-  [ComponentStatus.StoppedComponent]: EnvironmentCardBadgeStatus.Stopped,
-};
-
-const ReplicaCardStatus = {
-  [ReplicaStatus.Succeeded]: EnvironmentCardBadgeStatus.Consistent,
-  [ReplicaStatus.Running]: EnvironmentCardBadgeStatus.Running,
-  [ReplicaStatus.Starting]: EnvironmentCardBadgeStatus.Starting,
-};
 
 const activeDeployment = (
   appName: string,
@@ -80,73 +56,21 @@ const activeDeployment = (
     </Button>
   );
 
-function deriveComponentStatus(
-  components: Array<ComponentModel>
-): Array<{ title: string; status: EnvironmentCardBadgeStatus }> {
-  if (!(components?.length > 0)) return [];
-
-  const status: Array<{ title: string; status: EnvironmentCardBadgeStatus }> =
-    [];
-  const replicas = components.reduce<Array<ReplicaSummaryNormalizedModel>>(
-    (obj, x) => {
-      x.replicaList?.forEach((x) => obj.push(x));
-      return obj;
-    },
-    []
-  );
-
-  if (components.length > 0) {
-    status.push({
-      title: 'Components',
-      status: components.reduce<EnvironmentCardBadgeStatus>(
-        (obj, x) =>
-          Math.max(
-            ComponentCardStatus[x.status] ?? EnvironmentCardBadgeStatus.Notice,
-            obj
-          ),
-        EnvironmentCardBadgeStatus.Consistent
-      ),
-    });
-
-    if (replicas.length > 0) {
-      status.push({
-        title: 'Replicas',
-        status: replicas.reduce<EnvironmentCardBadgeStatus>(
-          (obj, x) =>
-            Math.max(
-              ReplicaCardStatus[x.status] ?? EnvironmentCardBadgeStatus.Notice,
-              obj
-            ),
-          EnvironmentCardBadgeStatus.Consistent
-        ),
-      });
-    }
-  }
-
-  return status;
-}
-
 function CardContentBuilder(
   appName: string,
   envName: string,
   deploymentName: string
 ): CardContent {
   const [componentsState] = useGetComponents(appName, deploymentName);
-
   const components = componentsState.data ?? [];
-  const status = deriveComponentStatus(components);
 
   return {
     header: (
       <div className="env_card-header_badges grid grid--auto-columns grid--gap-x-small">
         <SimpleAsyncResource asyncState={componentsState} customError={<></>}>
-          {status.map(({ title, status }) => (
-            <Tooltip key={title} title={title} placement="top">
-              <span>
-                <EnvironmentCardBadge title={title[0]} status={status} />
-              </span>
-            </Tooltip>
-          ))}
+          {components.length > 0 && (
+            <EnvironmentCardStatus components={components} />
+          )}
         </SimpleAsyncResource>
       </div>
     ),
