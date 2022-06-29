37afed4b858d0aaba6f35c9cfe3cba3825954c14
Fredrik Hatletvedt <fhatletvedt@gmail.com>
2022-06-29 12:14:32 +0200

    cleanup status-popover

--- src/components/status-popover/status-popover.tsx
+++ src/components/status-popover/status-popover.tsx
@@ -1,42 +1,48 @@
-import { Chip, Icon, Popover, PopoverProps } from '@equinor/eds-core-react';
+import {
+  Chip,
+  Icon,
+  Popover,
+  PopoverProps,
+  PopoverTitleProps,
+} from '@equinor/eds-core-react';
 import { info_circle } from '@equinor/eds-icons';
-import classNames from 'classnames';
 import { ReactNode, useRef, useState } from 'react';
 
 import './style.css';
 
 export type StatusPopoverType = 'success' | 'warning' | 'danger' | 'none';
 
-export interface StatusPopoverProps
-  extends Pick<PopoverProps, 'children' | 'className' | 'placement'> {
-  title?: ReactNode;
-  type?: StatusPopoverType;
+export type StatusPopoverProps = {
+  children: ReactNode;
+  className?: string;
   icon?: ReactNode;
-}
+  type?: StatusPopoverType;
+} & Pick<PopoverProps, 'placement'> &
+  Pick<PopoverTitleProps, 'title'>;
 
 export const StatusPopover = ({
   children,
   className,
   title,
-  type = 'none',
   icon = <Icon data={info_circle} />,
+  type = 'none',
   ...rest
 }: StatusPopoverProps): JSX.Element => {
   const [popoverOpen, setPopoverOpen] = useState(false);
   const chipRef = useRef<HTMLDivElement>();
 
   return (
-    <div className={classNames('status-popover', { className: !!className })}>
+    <div className="status-popover">
       <Popover open={popoverOpen} anchorEl={chipRef.current} {...rest}>
         {!!title && (
           <Popover.Header>
             <Popover.Title children={title} />
           </Popover.Header>
         )}
-        <Popover.Content children={children} />
+        <Popover.Content className={className}>{children}</Popover.Content>
       </Popover>
       <Chip
-        className={`status-popover__chip status-popover__chip-type__${type}`}
+        className={`status-popover-chip status-popover-chip-type__${type}`}
         ref={chipRef}
         onMouseEnter={() => setPopoverOpen(true)}
         onMouseLeave={() => setPopoverOpen(false)}
