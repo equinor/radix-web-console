ae9b4d7d890f03cc2b0cccd3078c502c82dd42e8
sondresjolyst <sosj@equinor.com>
2022-08-05 13:06:53 +0200

    adGroups multiselect

---
+++ src/components/graph/adGroups.tsx
@@ -0,0 +1,66 @@
+import { Button } from '@equinor/eds-core-react';
+import AsyncSelect from 'react-select/async';
+import { useState } from 'react';
+
+interface State {
+  readonly inputValue: string;
+}
+
+export const ADGroups = () => {
+  const [state, setState] = useState<State>({
+    inputValue: '',
+  });
+
+  const options = {
+    value: [
+      {
+        displayName: 'Karabagh project - Country Risk CAR/QAA/QC',
+        id: '0000870f-da30-4e72-99cc-7072640f8d1c',
+      },
+      {
+        displayName: 'Fremtidens renhold ',
+        id: '0001700e-a198-4939-89e1-5398925c5073',
+      },
+      {
+        displayName: 'Common_Read',
+        id: '0001e49c-dc8b-4231-b76e-c9055a53ffb4',
+      },
+      {
+        displayName: 'CPS_Read',
+        id: '000230df-a363-4962-b684-f61b4f250f63',
+      },
+      {
+        displayName: 'fg_EPI DEV PMO All',
+        id: '00026041-3b7b-4c8d-9a10-56a0c483badb',
+      },
+      {
+        displayName: 'APPL JIT IPGFC',
+        id: '00028ff0-0b94-450a-8409-0cd61437ecd9',
+      },
+    ],
+  };
+
+  const loadOptions = (inputValue: string, callback: (options) => void) => {
+    callback(console.log(inputValue));
+  };
+
+  const handleInputChange = (inputValue: string) => {
+    // console.log(inputValue);
+    setState({ inputValue });
+  };
+
+  return (
+    <>
+      <pre>inputValue: "{state.inputValue}"</pre>
+      <AsyncSelect
+        name="ADGroups"
+        defaultOptions={options.value}
+        loadOptions={loadOptions}
+        getOptionLabel={(options: any) => options.displayName}
+        getOptionValue={(options: any) => options.id}
+        isMulti
+      />
+      <Button disabled>Save</Button>
+    </>
+  );
+};
