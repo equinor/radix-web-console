0738874644a4e06137d84f5f3fd8d54f0e20a067
Magnus Longva <61229595+magnus-longva-bouvet@users.noreply.github.com>
2022-08-12 12:43:41 +0200

    Update src/api/api-config.ts
    Co-authored-by: Fredrik Hatletvedt <32248439+Pespiri@users.noreply.github.com>

--- src/api/api-config.ts
+++ src/api/api-config.ts
@@ -1,30 +1,15 @@
-export let costApiBaseUri;
-if (process.env.REACT_APP_COST_API_BASE_URI) {
-  costApiBaseUri = `${process.env.REACT_APP_COST_API_BASE_URI}/cost-api`;
-} else {
-  costApiBaseUri = `${window.location.host}/cost-api`;
-}
-
-export let dynatraceApiBaseUri;
-if (process.env.REACT_APP_DYNATRACE_API_BASE_URI) {
-  dynatraceApiBaseUri = `${process.env.REACT_APP_DYNATRACE_API_BASE_URI}/dynatrace/api`;
-} else {
-  dynatraceApiBaseUri = `${window.location.host}/dynatrace/api`;
-}
-
-export let scanApiBaseUri;
-if (process.env.REACT_APP_SCAN_API_BASE_URI) {
-  scanApiBaseUri = `${process.env.REACT_APP_SCAN_API_BASE_URI}/scan-api`;
-} else {
-  scanApiBaseUri = `${window.location.host}/scan-api`;
-}
-
-export let radixApiBaseUri;
-if (process.env.REACT_APP_RADIX_API_BASE_URI) {
-  radixApiBaseUri = `${process.env.REACT_APP_RADIX_API_BASE_URI}/api/v1`;
-} else {
-  radixApiBaseUri = `${window.location.host}/api/v1`;
-}
+export const costApiBaseUri = `${
+  process.env.REACT_APP_COST_API_BASE_URI || window.location.host
+}/cost-api`;
+export const dynatraceApiBaseUri = `${
+  process.env.REACT_APP_DYNATRACE_API_BASE_URI || window.location.host
+}/dynatrace/api`;
+export const scanApiBaseUri = `${
+  process.env.REACT_APP_SCAN_API_BASE_URI || window.location.host
+}/scan-api`;
+export const radixApiBaseUri = `${
+  process.env.REACT_APP_RADIX_API_BASE_URI || window.location.host
+}/api/v1`;
 
 /**
  * Create a full URL to the API
