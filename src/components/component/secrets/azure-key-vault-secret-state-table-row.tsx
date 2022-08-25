1cf70e645a44c0de592d3c57c6466bb8a57ccefa
Sergey Smolnikov <ssmol@equinor.com>
2022-08-25 13:05:35 +0200

    Show secret versions for job scheduler wo job pods (#604)
    * Added model
    
    * Show job and batch secret versions
    
    * Show job and batch secret versions
    
    * Fixed secret versions for regular component
    
    * Update src/components/component/secrets/secret-list-item-title-azure-key-vault-item.tsx
    
    Co-authored-by: Fredrik Hatletvedt <32248439+Pespiri@users.noreply.github.com>
    
    * Fixed suggested changes
    
    Co-authored-by: Fredrik Hatletvedt <32248439+Pespiri@users.noreply.github.com>

--- src/components/component/secrets/azure-key-vault-secret-state-table-row.tsx
+++ src/components/component/secrets/azure-key-vault-secret-state-table-row.tsx
@@ -1,31 +1,71 @@
 import { Table, Typography } from '@equinor/eds-core-react';
 import * as PropTypes from 'prop-types';
 
-import { smallReplicaName } from '../../../utils/string';
+import { Duration } from '../../time/duration';
 import {
   AzureKeyVaultSecretStatusModel,
   AzureKeyVaultSecretStatusModelValidationMap,
 } from '../../../models/azure-key-vault-secret-status';
+import { smallScheduledBatchName } from '../../../utils/string';
 
 export interface AzureKeyVaultSecretStateTableRowProps {
-  secretInReplica: AzureKeyVaultSecretStatusModel;
+  secret: AzureKeyVaultSecretStatusModel;
 }
 
-export const AzureKeyVaultSecretStateTableRow = (
-  props: AzureKeyVaultSecretStateTableRowProps
-): JSX.Element => {
-  return (
-    <Table.Row>
-      <Table.Cell>{props.secretInReplica.version}</Table.Cell>
-      <Table.Cell>
-        <Typography as="span">
-          {smallReplicaName(props.secretInReplica.replicaName)}
-        </Typography>
-      </Table.Cell>
-    </Table.Row>
-  );
+const ConsumerSecretName = ({
+  secret,
+}: {
+  secret: AzureKeyVaultSecretStatusModel;
+}): JSX.Element => {
+  let consumer: string;
+  if (secret.batchName?.length > 0) {
+    // show only first secret-version entry for pods of this batch
+    consumer = `batch: ${smallScheduledBatchName(secret.batchName)}`;
+  } else if (secret.jobName?.length > 0) {
+    consumer = `job: ${smallScheduledBatchName(secret.jobName)}`;
+  } else if (secret.replicaName.toLowerCase() === 'new jobs') {
+    consumer = 'New job';
+  } else {
+    consumer = `replica: ${smallScheduledBatchName(secret.replicaName)}`;
+  }
+
+  return <Typography as="span">{consumer}</Typography>;
 };
 
+const ConsumerSecretCreated = ({
+  secret,
+}: {
+  secret: AzureKeyVaultSecretStatusModel;
+}): JSX.Element => {
+  if (secret.batchName?.length > 0) {
+    return <Duration start={secret.batchCreated} end={new Date()} />;
+  }
+  if (secret.jobName?.length > 0) {
+    return <Duration start={secret.jobCreated} end={new Date()} />;
+  }
+  if (secret.replicaName.toLowerCase() === 'new jobs') {
+    return <></>;
+  }
+
+  return <Duration start={secret.replicaCreated} end={new Date()} />;
+};
+
+export const AzureKeyVaultSecretStateTableRow = ({
+  secret,
+}: AzureKeyVaultSecretStateTableRowProps): JSX.Element => (
+  <Table.Row>
+    <Table.Cell>{secret.version}</Table.Cell>
+    <Table.Cell>
+      <Typography as="span">
+        <ConsumerSecretName secret={secret} />
+      </Typography>
+    </Table.Cell>
+    <Table.Cell>
+      <ConsumerSecretCreated secret={secret} />
+    </Table.Cell>
+  </Table.Row>
+);
+
 AzureKeyVaultSecretStateTableRow.propTypes = {
-  secretInReplica: PropTypes.shape(AzureKeyVaultSecretStatusModelValidationMap),
+  secret: PropTypes.shape(AzureKeyVaultSecretStatusModelValidationMap),
 } as PropTypes.ValidationMap<AzureKeyVaultSecretStateTableRowProps>;
--- src/models/azure-key-vault-secret-status/normalizer.ts
+++ src/models/azure-key-vault-secret-status/normalizer.ts
@@ -1,9 +1,16 @@
 import { AzureKeyVaultSecretStatusModel } from '.';
 import { ModelNormalizerType } from '../model-types';
+import { dateNormalizer } from '../model-utils';
 
 /**
  * Create a AzureKeyVaultSecretStatusModel object
  */
 export const AzureKeyVaultSecretStatusModelNormalizer: ModelNormalizerType<
   AzureKeyVaultSecretStatusModel
-> = (props) => Object.freeze({ ...(props as AzureKeyVaultSecretStatusModel) });
+> = (props) => {
+  const normalized = { ...(props as AzureKeyVaultSecretStatusModel) };
+  normalized.replicaCreated = dateNormalizer(normalized.replicaCreated);
+  normalized.jobCreated = dateNormalizer(normalized.jobCreated);
+  normalized.batchCreated = dateNormalizer(normalized.batchCreated);
+  return Object.freeze(normalized);
+};
