bc6f75d65f6d74842de9b37d6fbf476a84be335c
Sergey Smolnikov <ssmol@equinor.com>
2022-10-03 17:55:45 +0200

    Removed outdated code

--- src/components/page-configuration/use-save-owner.js
+++ src/components/page-configuration/use-save-owner.js
@@ -3,17 +3,13 @@ import { usePatchJson } from '../../effects';
 const useSaveOwner = (appName) => {
   const path = `/applications/${appName}`;
 
-  return usePatchJson(
-    path,
-    (newOwner) => {
-      return {
-        applicationRegistrationPatch: {
-          owner: newOwner ? newOwner.toString() : null,
-        },
-      };
-    },
-    (responseData) => responseData
-  );
+  return usePatchJson(path, (newOwner) => {
+    return {
+      applicationRegistrationPatch: {
+        owner: newOwner ? newOwner.toString() : null,
+      },
+    };
+  });
 };
 
 export default useSaveOwner;
