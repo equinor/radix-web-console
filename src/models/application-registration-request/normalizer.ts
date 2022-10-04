82d38a0a13b6c3794b9cfed5a9ffc0476e1ae707
Sergey Smolnikov <ssmol@equinor.com>
2022-10-04 14:52:22 +0200

    Update src/models/application-registration-request/normalizer.ts
    Co-authored-by: Fredrik <32248439+Pespiri@users.noreply.github.com>

--- src/models/application-registration-request/normalizer.ts
+++ src/models/application-registration-request/normalizer.ts
@@ -1,15 +1,20 @@
 import { ApplicationRegistrationRequestModel } from '.';
+
+import { ApplicationRegistrationModelNormalizer } from '../application-registration/normalizer';
 import { ModelNormalizerType } from '../model-types';
 import { filterUndefinedFields } from '../model-utils';
 
 /**
- * Create an ApplicationRegistrationRequestModelNormalizer object
+ * Create an ApplicationRegistrationRequestModel object
  */
 export const ApplicationRegistrationRequestModelNormalizer: ModelNormalizerType<
   ApplicationRegistrationRequestModel
-> = (props) =>
-  Object.freeze(
-    filterUndefinedFields({
-      ...(props as ApplicationRegistrationRequestModel),
-    })
-  );
+> = (props) => {
+  const normalized = { ...(props as ApplicationRegistrationRequestModel) };
+
+  normalized.applicationRegistration =
+    normalized.applicationRegistration &&
+    ApplicationRegistrationModelNormalizer(normalized.applicationRegistration);
+
+  return Object.freeze(filterUndefinedFields(normalized));
+};
