392dcdaaa0e3d466f14cd0ee79773ce0cc34a93f
Sergey Smolnikov <ssmol@equinor.com>
2022-10-04 14:59:52 +0200

    Update src/models/application-registration-patch-request/test-data.ts
    Co-authored-by: Fredrik <32248439+Pespiri@users.noreply.github.com>

--- src/models/application-registration-patch-request/test-data.ts
+++ src/models/application-registration-patch-request/test-data.ts
@@ -1,6 +1,7 @@
-import { TestDependencyDataType } from '../model-types';
-
 import { ApplicationRegistrationPatchRequestModel } from '.';
+
+import { testData as ApplicationRegistrationPatchData } from '../application-registration-patch/test-data';
+import { TestDependencyDataType } from '../model-types';
 
 /*
  * TestData array
@@ -10,70 +11,17 @@ import { ApplicationRegistrationPatchRequestModel } from '.';
 export const testData: TestDependencyDataType<ApplicationRegistrationPatchRequestModel> =
   [
     {
-      __testDescription: 'Valid object with application',
-      applicationRegistrationPatch: {
-        adGroups: ['group1', 'group2'],
-        owner: "lil' U",
-        machineUser: true,
-        repository: 'repo',
-        wbs: 'money',
-        configBranch: 'branch',
-      },
-    },
-    {
-      __testDescription: 'Valid object with acknowledgeWarnings',
-      applicationRegistrationPatch: undefined,
+      __testDescription: 'Valid full object',
+      applicationRegistrationPatch: ApplicationRegistrationPatchData[0],
       acknowledgeWarnings: true,
     },
     {
-      __testDescription: 'Valid partial object',
-      applicationRegistrationPatch: {
-        repository: 'repo',
-        owner: "lil' U",
-        machineUser: true,
-        wbs: 'money',
-        configBranch: 'branch',
-      },
+      __testDescription: 'Valid empty object',
     },
     {
       __testDescription: 'Invalid full object',
       __testIsInvalidSample: true,
-      applicationRegistrationPatch: {
-        repository: 'repo',
-        adGroups: 'group1, group2' as unknown as Array<string>,
-        owner: "lil' U",
-        machineUser: true,
-        wbs: 'money',
-        configBranch: 'branch',
-      },
-    },
-    {
-      __testDescription: 'Invalid partial object',
-      __testIsInvalidSample: true,
-      applicationRegistrationPatch: {
-        repository: 'repo',
-        owner: "lil' U",
-        machineUser: 'cake' as unknown as boolean,
-        wbs: 'money',
-        configBranch: 'branch',
-      },
-    },
-    {
-      __testDescription: 'Invalid empty object',
-      __testIsInvalidSample: true,
-      applicationRegistrationPatch: {
-        repository: undefined,
-        owner: undefined,
-        machineUser: undefined,
-        wbs: undefined,
-        configBranch: undefined,
-      },
-      acknowledgeWarnings: undefined,
-    },
-    {
-      __testDescription: 'Invalid empty object',
-      __testIsInvalidSample: true,
-      applicationRegistrationPatch: undefined,
-      acknowledgeWarnings: undefined,
+      applicationRegistrationPatch: ApplicationRegistrationPatchData[0],
+      acknowledgeWarnings: 'cake' as unknown as boolean,
     },
   ];
