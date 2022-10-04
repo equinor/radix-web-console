f6e32f91c9529fa4df7955d1b52bd6d60b9b36ef
Sergey Smolnikov <ssmol@equinor.com>
2022-10-04 14:56:24 +0200

    Update src/models/application-registration-upsert-response/test-data.ts
    Co-authored-by: Fredrik <32248439+Pespiri@users.noreply.github.com>

--- src/models/application-registration-upsert-response/test-data.ts
+++ src/models/application-registration-upsert-response/test-data.ts
@@ -1,6 +1,7 @@
-import { TestDependencyDataType } from '../model-types';
-
 import { ApplicationRegistrationUpsertResponseModel } from '.';
+
+import { testData as ApplicationRegistrationData } from '../application-registration/test-data';
+import { TestDependencyDataType } from '../model-types';
 
 /*
  * TestData array
@@ -10,89 +11,17 @@ import { ApplicationRegistrationUpsertResponseModel } from '.';
 export const testData: TestDependencyDataType<ApplicationRegistrationUpsertResponseModel> =
   [
     {
-      __testDescription: 'Valid object with application',
-      applicationRegistration: {
-        name: 'dat app',
-        repository: 'repo',
-        sharedSecret: 'a1b2c3',
-        adGroups: ['group1', 'group2'],
-        owner: "lil' U",
-        creator: 'muh dude!',
-        publicKey: 'abc123',
-        privateKey: '123abc',
-        machineUser: true,
-        wbs: 'money',
-        configBranch: 'branch',
-      },
+      __testDescription: 'Valid full object',
+      applicationRegistration: ApplicationRegistrationData[0],
+      warnings: ['warn_1', 'warn_2'],
     },
     {
-      __testDescription: 'Valid object with warnings',
-      applicationRegistration: undefined,
-      warnings: ['some-warning', 'another warning'],
-    },
-    {
-      __testDescription: 'Valid partial object',
-      applicationRegistration: {
-        name: 'dat app',
-        repository: 'repo',
-        sharedSecret: 'a1b2c3',
-        owner: "lil' U",
-        creator: 'muh dude!',
-        machineUser: true,
-        wbs: 'money',
-        configBranch: 'branch',
-      },
+      __testDescription: 'Valid empty object',
     },
     {
       __testDescription: 'Invalid full object',
       __testIsInvalidSample: true,
-      applicationRegistration: {
-        name: 'dat app',
-        repository: 'repo',
-        sharedSecret: 'a1b2c3',
-        adGroups: 'group1, group2' as unknown as Array<string>,
-        owner: "lil' U",
-        creator: 'muh dude!',
-        publicKey: 'abc123',
-        privateKey: '123abc',
-        machineUser: true,
-        wbs: 'money',
-        configBranch: 'branch',
-      },
-    },
-    {
-      __testDescription: 'Invalid partial object',
-      __testIsInvalidSample: true,
-      applicationRegistration: {
-        name: 'dat app',
-        repository: 'repo',
-        sharedSecret: 'a1b2c3',
-        owner: "lil' U",
-        creator: 'muh dude!',
-        machineUser: 'cake' as unknown as boolean,
-        wbs: 'money',
-        configBranch: 'branch',
-      },
-    },
-    {
-      __testDescription: 'Invalid empty object',
-      __testIsInvalidSample: true,
-      applicationRegistration: {
-        name: undefined,
-        repository: undefined,
-        sharedSecret: undefined,
-        owner: undefined,
-        creator: undefined,
-        machineUser: undefined,
-        wbs: undefined,
-        configBranch: undefined,
-      },
-      warnings: undefined,
-    },
-    {
-      __testDescription: 'Invalid empty object',
-      __testIsInvalidSample: true,
-      applicationRegistration: undefined,
-      warnings: undefined,
+      applicationRegistration: ApplicationRegistrationData[0],
+      warnings: true as unknown as Array<string>,
     },
   ];
