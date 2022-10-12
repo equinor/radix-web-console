5cd49756f93b4f66477185cf7aa4d7cebfc9ac6c
Fredrik Hatletvedt <fhatletvedt@gmail.com>
2022-10-12 14:35:56 +0200

    convert page-application to typescript

--- src/components/page-application/index.tsx
+++ src/components/page-application/index.tsx
@@ -1,29 +1,35 @@
-import { Route } from 'react-router';
 import * as PropTypes from 'prop-types';
+import { Route } from 'react-router';
 
 import AppOverview from '../app-overview';
 import { DocumentTitle } from '../document-title';
 import { GlobalTopNav } from '../global-top-nav';
 import { LayoutApp } from '../layout-app';
+import PageBuildSecret from '../page-build-secret';
 import PageConfiguration from '../page-configuration';
-import PageDeployments from '../page-deployments';
 import PageDeployment from '../page-deployment';
+import PageDeployments from '../page-deployments';
 import PageEnvironment from '../page-environment';
 import PageEnvironments from '../page-environments';
-import PageBuildSecret from '../page-build-secret';
-import PagePrivateImageHub from '../page-private-image-hub';
 import PageJob from '../page-pipeline-job';
 import PageJobNew from '../page-pipeline-job-new';
 import PageJobs from '../page-pipeline-jobs';
 import PagePipelineRun from '../page-pipeline-run';
 import PagePipelineRunTask from '../page-pipeline-run-task';
+import PagePrivateImageHub from '../page-private-image-hub';
 import PageStep from '../page-step';
-import { mapRouteParamsToProps } from '../../utils/routing';
 import { routes } from '../../routes';
+import { mapRouteParamsToProps } from '../../utils/routing';
 
 import './style.css';
 
-export const PageApplication = ({ appName }) => (
+export interface PageApplicationProps {
+  appName: string;
+}
+
+export const PageApplication = ({
+  appName,
+}: PageApplicationProps): JSX.Element => (
   <div className="o-layout-main">
     <DocumentTitle title={appName} />
     <GlobalTopNav />
@@ -31,30 +37,22 @@ export const PageApplication = ({ appName }) => (
       <div className="o-layout-main__content">
         <div className="page-application__content">
           <div className="o-layout-constrained">
-            <Route
-              path={routes.app}
-              exact
-              render={() => <AppOverview appName={appName} />}
-            />
+            <Route path={routes.app} exact render={() => <AppOverview />} />
             <Route
               path={routes.appJobNew}
               exact
-              render={() => <PageJobNew appName={appName} />}
+              render={() => <PageJobNew />}
             />
             <Route
               path={routes.appEnvironments}
               exact
-              render={() => <PageEnvironments appName={appName} />}
+              render={() => <PageEnvironments />}
             />
-            <Route
-              path={routes.appJobs}
-              exact
-              render={() => <PageJobs appName={appName} />}
-            />
+            <Route path={routes.appJobs} exact render={() => <PageJobs />} />
             <Route
               path={routes.appDeployments}
               exact
-              render={() => <PageDeployments appName={appName} />}
+              render={() => <PageDeployments />}
             />
             <Route path={routes.appEnvironment} component={PageEnvironment} />
             <Route path={routes.appJob} component={PageJob} />
@@ -84,6 +82,6 @@ export const PageApplication = ({ appName }) => (
 
 PageApplication.propTypes = {
   appName: PropTypes.string.isRequired,
-};
+} as PropTypes.ValidationMap<PageApplicationProps>;
 
 export default mapRouteParamsToProps(['appName'], PageApplication);
