6974617373e6f6820bbfba3816cb2d399c03ced2
Fredrik Hatletvedt <fhatletvedt@gmail.com>
2022-10-12 14:37:53 +0200

    update application-summary model

--- src/models/application-summary/index.ts
+++ src/models/application-summary/index.ts
@@ -1,10 +1,12 @@
 import * as PropTypes from 'prop-types';
 
+import { DeploymentModel, DeploymentModelValidationMap } from '../deployment';
 import { JobSummaryModel, JobSummaryModelValidationMap } from '../job-summary';
 
 export interface ApplicationSummaryModel {
   name: string;
   latestJob?: JobSummaryModel;
+  activeDeployments?: Array<DeploymentModel>;
 }
 
 /* PropTypes validation map for ApplicationSummaryModel */
@@ -13,5 +15,10 @@ export const ApplicationSummaryModelValidationMap: PropTypes.ValidationMap<Appli
     name: PropTypes.string.isRequired,
     latestJob: PropTypes.shape(
       JobSummaryModelValidationMap
-    ) as PropTypes.Requireable<JobSummaryModel>,
+    ) as PropTypes.Validator<JobSummaryModel>,
+    activeDeployments: PropTypes.arrayOf(
+      PropTypes.shape(
+        DeploymentModelValidationMap
+      ) as PropTypes.Validator<DeploymentModel>
+    ),
   };
--- src/models/application-summary/normalizer.ts
+++ src/models/application-summary/normalizer.ts
@@ -1,8 +1,9 @@
 import { ApplicationSummaryModel } from '.';
 
+import { DeploymentModelNormalizer } from '../deployment/normalizer';
 import { JobSummaryModelNormalizer } from '../job-summary/normalizer';
 import { ModelNormalizerType } from '../model-types';
-import { filterUndefinedFields } from '../model-utils';
+import { arrayNormalizer, filterUndefinedFields } from '../model-utils';
 
 /**
  * Create an ApplicationSummaryModel object
@@ -14,6 +15,10 @@ export const ApplicationSummaryModelNormalizer: ModelNormalizerType<
 
   normalized.latestJob =
     normalized.latestJob && JobSummaryModelNormalizer(normalized.latestJob);
+  normalized.activeDeployments = arrayNormalizer(
+    normalized.activeDeployments,
+    DeploymentModelNormalizer
+  );
 
   return Object.freeze(filterUndefinedFields(normalized));
 };
