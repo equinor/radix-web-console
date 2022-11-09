99324f5c1777b71cd9b3e8c2ec735cdeeb770c10
Fredrik Hatletvedt <fhatletvedt@gmail.com>
2022-11-09 10:39:40 +0100

    update ChangeMachineUserForm to typescript

---
+++ src/components/page-configuration/change-machine-user-form.tsx
@@ -0,0 +1,98 @@
+import {
+  Accordion,
+  Button,
+  Checkbox,
+  CircularProgress,
+  Typography,
+} from '@equinor/eds-core-react';
+import * as PropTypes from 'prop-types';
+import { useEffect, useState } from 'react';
+
+import { useSaveMachineUser } from './use-save-machine-user';
+
+import { Alert } from '../alert';
+import { RequestState } from '../../state/state-utils/request-states';
+
+export interface ChangeMachineUserFormProps {
+  appName: string;
+  machineUser: boolean;
+  onMachineUserChange: (appName: string) => void;
+}
+
+export const ChangeMachineUserForm = ({
+  appName,
+  machineUser,
+  onMachineUserChange,
+}: ChangeMachineUserFormProps): JSX.Element => {
+  const [machineUserState, setMachineUserState] = useState(machineUser);
+  const [savedMachineUserState, setSavedMachineUserState] =
+    useState(machineUser);
+  const [{ status, error }, saveFunc, resetSaveState] =
+    useSaveMachineUser(appName);
+
+  useEffect(() => {
+    setMachineUserState(machineUser);
+  }, [machineUser]);
+
+  useEffect(() => {
+    if (status === RequestState.SUCCESS) {
+      setSavedMachineUserState(machineUserState);
+      onMachineUserChange(appName);
+      resetSaveState();
+    }
+  }, [appName, machineUserState, status, resetSaveState, onMachineUserChange]);
+
+  return (
+    <Accordion className="accordion" chevronPosition="right">
+      <Accordion.Item>
+        <Accordion.Header>
+          <Accordion.HeaderTitle>
+            <Typography>Machine user</Typography>
+          </Accordion.HeaderTitle>
+        </Accordion.Header>
+        <Accordion.Panel>
+          <div className="grid grid--gap-medium">
+            <Typography>
+              Check this option if you intend to create an application that
+              communicates with Radix API.
+            </Typography>
+            <Checkbox
+              label="Enable machine user"
+              name="machineUser"
+              checked={machineUserState}
+              onChange={({ target: { checked } }) => {
+                setMachineUserState(checked);
+              }}
+              disabled={status === RequestState.IN_PROGRESS}
+            />
+            <div>
+              {status === RequestState.FAILURE ? (
+                <Alert type="danger">
+                  Failed to save machine user setting. {error}
+                </Alert>
+              ) : status === RequestState.IN_PROGRESS ? (
+                <>
+                  <CircularProgress size={24} /> Saving…
+                </>
+              ) : (
+                <Button
+                  onClick={() => saveFunc(machineUserState)}
+                  color="danger"
+                  disabled={savedMachineUserState === machineUserState}
+                >
+                  Save
+                </Button>
+              )}
+            </div>
+          </div>
+        </Accordion.Panel>
+      </Accordion.Item>
+    </Accordion>
+  );
+};
+
+ChangeMachineUserForm.propTypes = {
+  appName: PropTypes.string.isRequired,
+  machineUser: PropTypes.bool.isRequired,
+  onMachineUserChange: PropTypes.func.isRequired,
+} as PropTypes.ValidationMap<ChangeMachineUserFormProps>;
--- src/components/page-configuration/use-save-machine-user.ts
+++ src/components/page-configuration/use-save-machine-user.ts
@@ -18,5 +18,3 @@ export function useSaveMachineUser(
       } as Partial<ApplicationRegistrationPatchRequestModel>)
   );
 }
-
-export default useSaveMachineUser;
