a1914aa5cdc591c2371d2b0dd9ca74153de97de2
Fredrik Hatletvedt <fhatletvedt@gmail.com>
2022-11-09 09:31:47 +0100

    add MachineUser model

---
+++ src/models/machine-user/test-data.ts
@@ -0,0 +1,25 @@
+import { MachineUserModel } from '.';
+
+import { TestDependencyDataType } from '../model-types';
+
+/*
+ * TestData array
+ *
+ * Note: First object should always be valid
+ */
+export const testData: TestDependencyDataType<MachineUserModel> = [
+  {
+    __testDescription: 'Valid full object',
+    token: 'token',
+  },
+  {
+    __testDescription: 'Invalid full object',
+    __testIsInvalidSample: true,
+    token: true as unknown as string,
+  },
+  {
+    __testDescription: 'Invalid empty object',
+    __testIsInvalidSample: true,
+    token: undefined,
+  },
+];
