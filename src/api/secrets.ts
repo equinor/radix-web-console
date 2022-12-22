5f72acadade59fb16723cf360de180c27a494933
Fredrik Hatletvedt <fhatletvedt@gmail.com>
2022-12-22 13:04:27 +0100

    update secrets api helper to typescript

--- src/api/secrets.ts
+++ src/api/secrets.ts
@@ -1,30 +1,28 @@
 import { createRadixApiUrl } from './api-config';
-import { getJson, putJson } from './api-helpers';
+import { putJson } from './api-helpers';
 
-export async function getComponentSecret(namespace, componentName) {
-  return await getJson(
-    createRadixApiUrl(`namespaces/${namespace}/${componentName}`)
-  );
-}
+import { SecretType } from '../models/secret-type';
 
 export async function saveComponentSecret(
-  appName,
-  envName,
-  componentName,
-  secretName,
-  value
-) {
+  appName: string,
+  envName: string,
+  componentName: string,
+  secretName: string,
+  value: string
+): Promise<string> {
   const encAppName = encodeURIComponent(appName);
   const encEnvName = encodeURIComponent(envName);
   const encComponentName = encodeURIComponent(componentName);
   const encSecretName = encodeURIComponent(secretName);
 
-  const body = { secretValue: value.toString() };
+  const body: { secretValue: string; type?: SecretType } = {
+    secretValue: value.toString(),
+  };
 
-  return await putJson(
+  return await putJson<string>(
     createRadixApiUrl(
       `/applications/${encAppName}/environments/${encEnvName}/components/${encComponentName}/secrets/${encSecretName}`
     ),
-    body
+    JSON.stringify(body)
   );
 }
