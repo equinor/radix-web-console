5332e79010e8ad036bbe4f2094f0586379ddd738
Fredrik Hatletvedt <fhatletvedt@gmail.com>
2022-12-22 13:05:44 +0100

    update oauth-auxiliary-resource api helper to typescript

--- src/api/oauth-auxiliary-resource.ts
+++ src/api/oauth-auxiliary-resource.ts
@@ -5,12 +5,16 @@ export async function restartOAuthAuxiliaryResource({
   appName,
   envName,
   componentName,
-}) {
+}: {
+  appName: string;
+  envName: string;
+  componentName: string;
+}): Promise<string> {
   const encAppName = encodeURIComponent(appName);
   const encEnvName = encodeURIComponent(envName);
   const encComponentName = encodeURIComponent(componentName);
 
-  return await postJsonWithoutBody(
+  return await postJsonWithoutBody<string>(
     createRadixApiUrl(
       `/applications/${encAppName}/environments/${encEnvName}/components/${encComponentName}/aux/oauth/restart`
     )
