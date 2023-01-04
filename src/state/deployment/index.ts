d419221a925d8e4ba8851fe8026ba3649452eec3
Nils Gustav Stråbø <nst@equinor.com>
2023-01-04 08:37:51 +0100

    config and model for federated credentials

--- src/state/deployment/index.ts
+++ src/state/deployment/index.ts
@@ -14,6 +14,7 @@ import { DeploymentModelNormalizer } from '../../models/deployment/normalizer';
 
 const initialState: DeploymentModel = {
   name: '',
+  namespace: '',
   environment: '',
   createdByJob: '',
 };
