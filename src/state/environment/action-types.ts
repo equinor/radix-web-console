9f39dc016e747e12ebefaa2c8d50f34969a01a89
Fredrik Hatletvedt <fhatletvedt@gmail.com>
2023-01-13 13:30:36 +0100

    update environment state sagas to typescript

--- src/state/environment/action-types.ts
+++ src/state/environment/action-types.ts
@@ -1,6 +1,8 @@
 import { defineRequestActions } from '../state-utils/request';
 
-export default Object.freeze({
+export const actionTypes = Object.freeze({
   ENVIRONMENT_SNAPSHOT: 'ENVIRONMENT_SNAPSHOT',
   ...defineRequestActions('ENVIRONMENT_DELETE'),
 });
+
+export default actionTypes;
