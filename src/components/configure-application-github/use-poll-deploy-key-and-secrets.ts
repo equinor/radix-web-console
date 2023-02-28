a6b3e5f4fa40af2887e6bbd30b78efe10833ce75
Fredrik Hatletvedt <fhatletvedt@gmail.com>
2023-02-28 14:01:55 +0100

    update typing in usePollDeployKeyAndSecret

--- src/components/configure-application-github/use-poll-deploy-key-and-secrets.ts
+++ src/components/configure-application-github/use-poll-deploy-key-and-secrets.ts
@@ -1,10 +1,16 @@
 import { usePollingJson } from '../../effects';
 import { AsyncPollingResult } from '../../effects/use-async-polling';
+import { DeployKeyAndSecretModel } from '../../models/deploy-key-and-secret';
+import { DeployKeyAndSecretModelNormalizer } from '../../models/deploy-key-and-secret/normalizer';
 
 export function usePollDeployKeyAndSecret(
   appName: string
-): AsyncPollingResult<Readonly<any>> {
+): AsyncPollingResult<Readonly<DeployKeyAndSecretModel>> {
   const encAppName = encodeURIComponent(appName);
 
-  return usePollingJson(`/applications/${encAppName}/deploy-key-and-secret`, 0);
+  return usePollingJson(
+    `/applications/${encAppName}/deploy-key-and-secret`,
+    0,
+    DeployKeyAndSecretModelNormalizer
+  );
 }
