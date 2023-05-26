fa3b8fc6cd74fa33b51f9f3d58cec817165523e1
Magnus Longva <61229595+magnus-longva-bouvet@users.noreply.github.com>
2023-05-26 12:47:44 +0200

    Update src/models/horizontal-scaling-summary/index.ts
    Co-authored-by: Fredrik Hatletvedt <32248439+Pespiri@users.noreply.github.com>

--- src/models/horizontal-scaling-summary/index.ts
+++ src/models/horizontal-scaling-summary/index.ts
@@ -14,8 +14,8 @@ export const HorizontalScalingSummaryModelValidationMap: PropTypes.ValidationMap
   {
     minReplicas: PropTypes.number.isRequired,
     maxReplicas: PropTypes.number.isRequired,
-    currentCPUUtilizationPercentage: PropTypes.number.isRequired,
-    targetCPUUtilizationPercentage: PropTypes.number.isRequired,
-    currentMemoryUtilizationPercentage: PropTypes.number.isRequired,
-    targetMemoryUtilizationPercentage: PropTypes.number.isRequired,
+    currentCPUUtilizationPercentage: PropTypes.number,
+    targetCPUUtilizationPercentage: PropTypes.number,
+    currentMemoryUtilizationPercentage: PropTypes.number,
+    targetMemoryUtilizationPercentage: PropTypes.number,
   };
