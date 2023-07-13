e4b3e4b19156051f5a9e74772055882936af7b81
Fredrik Hatletvedt <fhatletvedt@gmail.com>
2023-07-13 12:43:39 +0200

    omit dev.* components from build

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,10 @@ import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
 import eslintPlugin from 'vite-plugin-eslint';
 
+// regex filter for dev.* components
+const devComponentRegex =
+  /\.\.\/(components|pages)\/([A-Za-z0-9_-]+(\/[A-Za-z0-9_-]+)?)\/dev\.(tsx|jsx|js)/i;
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -17,7 +21,11 @@ export default defineConfig({
     port: 3000,
   },
   build: {
+    sourcemap: false,
     outDir: 'build',
+    rollupOptions: {
+      external: (source) => !!devComponentRegex.exec(source),
+    },
   },
   appType: 'spa',
   test: {
