41c4badbc672646f0be3d832fdc17ce47f352ef3
Fredrik Hatletvedt <fhatletvedt@gmail.com>
2023-07-14 12:11:53 +0200

    fix page-about

--- src/components/page-about/index.tsx
+++ src/components/page-about/index.tsx
@@ -6,7 +6,8 @@ import { AvailabilityCharts } from '../data-chart';
 export const PageAbout = (): JSX.Element => (
   <div className="panel grid grid--gap-small">
     <Typography variant="overline">
-      Radix Web Console [{process.env.VITE_NAME}@{process.env.VITE_VERSION}]
+      Radix Web Console [{import.meta.env.PACKAGE_NAME}@
+      {import.meta.env.PACKAGE_VERSION}]
     </Typography>
     <AvailabilityCharts />
     <Typography variant="h4">Configuration</Typography>
--- vite.config.ts
+++ vite.config.ts
@@ -2,20 +2,15 @@ import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
 import eslintPlugin from 'vite-plugin-eslint';
 
+import packageJson from './package.json';
+
 // regex filter for dev.* components
 const devComponentRegex =
   /\.\.\/(components|pages)\/([A-Za-z0-9_-]+(\/[A-Za-z0-9_-]+)?)\/dev\.(tsx|jsx|js)/i;
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    eslintPlugin({
-      failOnError: true,
-      failOnWarning: false,
-      useEslintrc: true,
-    }),
-  ],
+  appType: 'spa',
   server: {
     host: '0.0.0.0',
     port: 3000,
@@ -27,7 +22,18 @@ export default defineConfig({
       external: (source) => !!devComponentRegex.exec(source),
     },
   },
-  appType: 'spa',
+  define: {
+    'import.meta.env.PACKAGE_VERSION': JSON.stringify(packageJson.version),
+    'import.meta.env.PACKAGE_NAME': JSON.stringify(packageJson.name),
+  },
+  plugins: [
+    react(),
+    eslintPlugin({
+      failOnError: true,
+      failOnWarning: false,
+      useEslintrc: true,
+    }),
+  ],
   test: {
     globals: true,
     environment: 'jsdom',
