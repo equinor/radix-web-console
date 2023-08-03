684c1192dae47d7ff07828d4b60b548c0d8f17b0
Fredrik Hatletvedt <fhatletvedt@gmail.com>
2023-08-03 14:15:12 +0200

    cleanup

--- src/components/page-scheduled-job/index.tsx
+++ src/components/page-scheduled-job/index.tsx
@@ -1,6 +1,6 @@
 import { Typography } from '@equinor/eds-core-react';
 import * as PropTypes from 'prop-types';
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import { useGetFullJobLogs } from './use-get-job-full-logs';
 import { usePollJobLogs } from './use-poll-job-logs';
