33bfb4fa1998e19b3b218b61ed4be76a6b5b53c8
Fredrik Hatletvedt <fhatletvedt@gmail.com>
2023-08-29 10:17:22 +0200

    update packages

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,7 +1,20 @@
+import { fetch, Request, Response } from '@remix-run/web-fetch';
 import { randomBytes } from 'crypto';
 
+Object.defineProperties(global, {
+  // Built-in lib.dom.d.ts expects `fetch(Request | string, ...)` but the web
+  // fetch API allows a URL so @remix-run/web-fetch defines
+  // `fetch(string | URL | Request, ...)`
+  // @ts-expect-error
+  fetch: { value: fetch },
+  // Same as above, lib.dom.d.ts doesn't allow a URL to the Request constructor
+  // @ts-expect-error
+  Request: { value: Request },
+  // web-std/fetch Response does not currently implement Response.error()
+  // @ts-expect-error
+  Response: { value: Response },
+});
+
 Object.defineProperty(global.self, 'crypto', {
-  value: {
-    getRandomValues: (arr) => randomBytes(arr.length),
-  },
+  value: { getRandomValues: (arr) => randomBytes(arr.length) },
 });
