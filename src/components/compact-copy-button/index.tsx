ae715918a10e70c02e87a94dbc72222a0967c846
Fredrik Hatletvedt <fhatletvedt@gmail.com>
2023-10-17 14:23:20 +0200

    add copy button for azure identity fields

--- src/components/compact-copy-button/index.tsx
+++ src/components/compact-copy-button/index.tsx
@@ -6,21 +6,23 @@ import { copyToClipboard } from '../../utils/string';
 
 export const CompactCopyButton: FunctionComponent<{
   content: string;
-  size?: IconProps['size'];
+  size?: IconProps['size'] | 14 | 12;
 }> = ({ content, size = 16 }) => {
   const [copyTitle, setCopyTitle] = useState('Copy');
+  const buttonSize = size + 8 + Math.floor(size / 8);
 
   return (
     <Tooltip title={copyTitle} placement="top" enterDelay={300}>
       <Button
         variant="ghost_icon"
+        style={{ width: `${buttonSize}px`, height: `${buttonSize}px` }}
         onMouseLeave={() => setCopyTitle('Copy')}
         onClick={() => {
           copyToClipboard(content);
           setCopyTitle('Copied');
         }}
       >
-        <Icon data={copy} size={size} />
+        <Icon data={copy} size={size as IconProps['size']} />
       </Button>
     </Tooltip>
   );
