a9c69ba08af8868fcdb8e3c7019d7fcdb8bc1fe1
Fredrik Hatletvedt <fhatletvedt@gmail.com>
2023-11-14 14:33:54 +0100

    hack together a loading component for generated apis

--- src/components/application-cost/index.tsx
+++ src/components/application-cost/index.tsx
@@ -1,12 +1,10 @@
 import { Typography } from '@equinor/eds-core-react';
-import { QueryStatus } from '@reduxjs/toolkit/query';
 import { format } from 'date-fns';
 import * as PropTypes from 'prop-types';
 import { FunctionComponent } from 'react';
 import { connect } from 'react-redux';
 
-import AsyncResource from '../async-resource/simple-async-resource';
-import { RequestState } from '../../state/state-utils/request-states';
+import AsyncResource from '../async-resource/another-async-resource';
 import { ApplicationCostSet, useGetTotalCostQuery } from '../../store/cost-api';
 import { formatDateTimeYear } from '../../utils/datetime';
 
@@ -40,7 +38,7 @@ export const ApplicationCost: FunctionComponent<ApplicationCostProps> = ({
   from,
   to,
 }) => {
-  const { data: cost, status } = useGetTotalCostQuery({
+  const { data: cost, ...state } = useGetTotalCostQuery({
     appName,
     fromTime: from,
     toTime: to,
@@ -49,18 +47,7 @@ export const ApplicationCost: FunctionComponent<ApplicationCostProps> = ({
   return (
     <div className="grid grid--gap-medium">
       <Typography variant="h6">Cost estimate</Typography>
-      <AsyncResource
-        asyncState={{
-          status:
-            status === QueryStatus.pending
-              ? RequestState.IN_PROGRESS
-              : status === QueryStatus.fulfilled
-              ? RequestState.SUCCESS
-              : status === QueryStatus.uninitialized
-              ? RequestState.IDLE
-              : RequestState.FAILURE,
-        }}
-      >
+      <AsyncResource asyncState={state}>
         {cost ? (
           <div className="cost-section grid grid--gap-medium">
             <div className="grid grid--gap-small">
--- src/components/application-future-cost/index.tsx
+++ src/components/application-future-cost/index.tsx
@@ -2,13 +2,11 @@ import { Typography } from '@equinor/eds-core-react';
 import * as PropTypes from 'prop-types';
 import { FunctionComponent } from 'react';
 
-import AsyncResource from '../async-resource/simple-async-resource';
+import AsyncResource from '../async-resource/another-async-resource';
 import { ApplicationCost, useGetFutureCostQuery } from '../../store/cost-api';
 import { formatDateTimeYear } from '../../utils/datetime';
 
 import '../application-cost/style.css';
-import { QueryStatus } from '@reduxjs/toolkit/query';
-import { RequestState } from '../../state/state-utils/request-states';
 
 function getCostEstimate({ cost, currency }: ApplicationCost): string {
   return !Number.isNaN(cost) ? `${cost.toFixed()} ${currency}` : 'No data';
@@ -29,23 +27,12 @@ export interface FutureApplicationCostProps {
 export const FutureApplicationCost: FunctionComponent<
   FutureApplicationCostProps
 > = ({ appName }) => {
-  const { data: cost, status } = useGetFutureCostQuery({ appName });
+  const { data: cost, ...state } = useGetFutureCostQuery({ appName });
 
   return (
     <div className="grid grid--gap-medium">
       <Typography variant="h6">Cost forecast</Typography>
-      <AsyncResource
-        asyncState={{
-          status:
-            status === QueryStatus.pending
-              ? RequestState.IN_PROGRESS
-              : status === QueryStatus.fulfilled
-              ? RequestState.SUCCESS
-              : status === QueryStatus.uninitialized
-              ? RequestState.IDLE
-              : RequestState.FAILURE,
-        }}
-      >
+      <AsyncResource asyncState={state}>
         {cost ? (
           <div className="cost-section grid grid--gap-medium">
             <div className="grid grid--gap-small">
