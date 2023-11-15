9a689c97cf933b9db45cabd0373c49453164704e
Fredrik Hatletvedt <fhatletvedt@gmail.com>
2023-11-15 10:32:49 +0100

    better asyncState object typing

--- src/components/async-resource/another-async-resource.tsx
+++ src/components/async-resource/another-async-resource.tsx
@@ -1,16 +1,21 @@
 import { CircularProgress, Typography } from '@equinor/eds-core-react';
 import { SerializedError } from '@reduxjs/toolkit';
-import { QueryStatus, FetchBaseQueryError } from '@reduxjs/toolkit/query';
+import { BaseQueryFn, FetchBaseQueryError } from '@reduxjs/toolkit/query';
+import { TypedUseQueryHookResult } from '@reduxjs/toolkit/query/react';
 import React, { FunctionComponent, PropsWithChildren, ReactNode } from 'react';
 
 import { Alert } from '../alert';
 import { externalUrls } from '../../externalUrls';
 import { isNullOrUndefined } from '../../utils/object';
 
-type AnotherAsyncStatus = {
-  status: QueryStatus;
-  error?: FetchBaseQueryError | SerializedError;
-};
+type AnotherAsyncStatus = Pick<
+  TypedUseQueryHookResult<
+    unknown,
+    unknown,
+    BaseQueryFn<[unknown], unknown, FetchBaseQueryError>
+  >,
+  'error' | 'isError' | 'isLoading'
+>;
 
 export interface AnotherAsyncResourceProps<T extends AnotherAsyncStatus> {
   asyncState: T;
@@ -29,19 +34,14 @@ const LoadingComponent: FunctionComponent<{
     defaultContent
   );
 
-const ErrorCodePanel: FunctionComponent<Pick<AnotherAsyncStatus, 'error'>> = ({
-  error,
-}) => {
+const ErrorPanel: FunctionComponent<
+  Required<Pick<AnotherAsyncStatus, 'error'>>
+> = ({ error }) => {
   let errObj: { code?: string | number; message: string } = { message: '' };
-  if (!error) {
-    /* noop */
-  } else if (
-    (error as SerializedError).message ||
-    (error as SerializedError).code
-  ) {
+  if ((error as SerializedError).message || (error as SerializedError).code) {
     const { code, message } = error as SerializedError;
     errObj = { code, message };
-  } else {
+  } else if ((error as FetchBaseQueryError).status) {
     const err = error as FetchBaseQueryError;
     errObj.message = err.data as string;
     if (typeof err.status === 'number') {
@@ -68,7 +68,7 @@ export const AnotherAsyncResource = <T extends AnotherAsyncStatus>({
   loadingContent = true,
   errorContent = true,
 }: PropsWithChildren<AnotherAsyncResourceProps<T>>): React.JSX.Element =>
-  !asyncState || asyncState.status === QueryStatus.pending ? (
+  !asyncState || asyncState.isLoading ? (
     <LoadingComponent
       content={loadingContent}
       defaultContent={
@@ -77,7 +77,7 @@ export const AnotherAsyncResource = <T extends AnotherAsyncStatus>({
         </span>
       }
     />
-  ) : asyncState.error ? (
+  ) : asyncState.isError ? (
     <LoadingComponent
       content={errorContent}
       defaultContent={
@@ -89,7 +89,7 @@ export const AnotherAsyncResource = <T extends AnotherAsyncStatus>({
             </span>
           </Typography>
           <div className="grid grid--gap-small">
-            <ErrorCodePanel error={asyncState.error} />
+            {asyncState.error && <ErrorPanel error={asyncState.error} />}
             <Typography>
               You may want to refresh the page. If the problem persists, get in
               touch on our Slack{' '}
