48cf3a910532836f0e4819eb61092d0c2579bfda
Fredrik Hatletvedt <fhatletvedt@gmail.com>
2023-11-16 14:24:12 +0100

    setup scan api generate config

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,15 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const costApi = createApi({
+  reducerPath: 'costApi',
   baseQuery: fetchBaseQuery({ baseUrl: '/cost-api' }),
   endpoints: () => ({}),
 });
 
-export default [costApi];
+export const scanApi = createApi({
+  reducerPath: 'scanApi',
+  baseQuery: fetchBaseQuery({ baseUrl: '/scan-api' }),
+  endpoints: () => ({}),
+});
+
+export default [costApi, scanApi];
