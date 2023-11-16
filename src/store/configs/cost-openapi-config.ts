5d7143b212ad126d5b20c764e222f10516978d97
Fredrik Hatletvedt <fhatletvedt@gmail.com>
2023-11-16 08:50:53 +0100

    use schema from api

--- src/store/configs/cost-openapi-config.ts
+++ src/store/configs/cost-openapi-config.ts
@@ -1,7 +1,8 @@
 import type { ConfigFile } from '@rtk-query/codegen-openapi';
 
 const config: ConfigFile = {
-  schemaFile: '../../../costApi.json',
+  schemaFile:
+    'https://server-radix-cost-allocation-api-qa.radix.equinor.com/swaggerui/swagger.json',
   apiFile: '../index.ts',
   apiImport: 'costApi',
   outputFile: '../cost-api.ts',
