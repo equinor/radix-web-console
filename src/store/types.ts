8b7f5236d464da071c90cc4b9f4711993c48ea84
Fredrik Hatletvedt <fhatletvedt@gmail.com>
2023-11-22 09:23:16 +0100

    better status messages

---
+++ src/store/types.ts
@@ -0,0 +1,13 @@
+import type { BaseQueryFn, FetchBaseQueryError } from '@reduxjs/toolkit/query';
+import type { TypedUseQueryHookResult } from '@reduxjs/toolkit/query/react';
+
+export type FetchQueryHookResult<T = unknown> = TypedUseQueryHookResult<
+  T,
+  unknown,
+  BaseQueryFn<[unknown], unknown, FetchBaseQueryError>
+>;
+
+export type FetchQueryResult<T = unknown> = Omit<
+  FetchQueryHookResult<T>,
+  'refetch'
+>;
