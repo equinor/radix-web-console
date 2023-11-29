2a3d32ed00fcf1b7fb573ec09bca83af3ea2da53
Fredrik Hatletvedt <fhatletvedt@gmail.com>
2023-11-29 14:12:28 +0100

    better error decoding

--- src/components/page-active-component/component-replica-log-accordion.tsx
+++ src/components/page-active-component/component-replica-log-accordion.tsx
@@ -17,9 +17,7 @@ import {
   useState,
 } from 'react';
 
-import AsyncResource, {
-  getErrorData,
-} from '../async-resource/another-async-resource';
+import AsyncResource from '../async-resource/another-async-resource';
 import { errorToast } from '../global-top-nav/styled-toaster';
 import { Duration } from '../time/duration';
 import { RelativeToNow } from '../time/relative-to-now';
@@ -30,6 +28,7 @@ import {
   useGetComponentInventoryQuery,
 } from '../../store/log-api';
 import { FetchQueryResult } from '../../store/types';
+import { getFetchErrorData } from '../../store/utils';
 import { sortCompareDate, sortDirection } from '../../utils/sort-utils';
 import {
   copyToTextFile,
@@ -76,7 +75,7 @@ function saveLog(
     const extension = !fileName.endsWith('.txt') ? '.txt' : '';
     copyToTextFile(`${fileName}${extension}`, query.data as string);
   } else if (query.isError) {
-    const { code, message } = getErrorData(query.error);
+    const { code, message } = getFetchErrorData(query.error);
     errorToast(`${errMsg}: ${code && `[${code}] `}${message}`);
   }
 }
