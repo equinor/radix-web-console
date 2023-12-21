a46219bcc6b8d4e197940c8bf5e44ac49a858f16
Fredrik Hatletvedt <fhatletvedt@gmail.com>
2023-12-21 10:59:52 +0100

    simplify logapi log download

--- src/components/code/log-helper.ts
+++ src/components/code/log-helper.ts
@@ -1,20 +1,20 @@
 import { BaseQueryFn, QueryDefinition } from '@reduxjs/toolkit/query';
-import { UseLazyQuery } from '@reduxjs/toolkit/dist/query/react/buildHooks';
+import { LazyQueryTrigger } from '@reduxjs/toolkit/dist/query/react/buildHooks';
 
 import { errorToast } from '../global-top-nav/styled-toaster';
 import { getFetchErrorMessage } from '../../store/utils';
 import { copyToTextFile } from '../../utils/string';
 
+export type LazyQueryTriggerPlain<Args extends Record<string, unknown>> =
+  LazyQueryTrigger<QueryDefinition<Args, BaseQueryFn, string, string>>;
+
 export function downloadLazyLogCb<
-  T extends ReturnType<
-    UseLazyQuery<QueryDefinition<unknown, BaseQueryFn, string, string>>
-  >[0],
+  T extends LazyQueryTriggerPlain<Record<string, unknown>>,
 >(filename: string, func: T, ...args: Parameters<T>): () => Promise<void> {
   return async () => {
-    const { data, error, isError, isSuccess } = await func(
-      args?.[0],
-      args?.[1]
-    );
+    const { data, error, isError, isSuccess } = await (
+      func as unknown as (...args: Parameters<T>) => ReturnType<T>
+    )(...((args || []) as Parameters<T>));
 
     if (isSuccess && data) {
       copyToTextFile(filename, data);
