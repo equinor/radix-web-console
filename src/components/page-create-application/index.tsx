aed571f6528dc6d541726336cb7a22683cf661c4
Richard Hagen <richard.hagen@bouvet.no>
2024-01-10 11:19:17 +0100

    migrate create-application-form

--- src/components/page-create-application/index.tsx
+++ src/components/page-create-application/index.tsx
@@ -1,79 +1,36 @@
 import { Button, Icon, Typography } from '@equinor/eds-core-react';
 import { add } from '@equinor/eds-icons';
-import * as PropTypes from 'prop-types';
-import { FunctionComponent, useEffect, useRef, useState } from 'react';
-import { connect } from 'react-redux';
+import { useRef, useState } from 'react';
 import { Link } from 'react-router-dom';
-import { Dispatch } from 'redux';
 
 import { ConfigureApplicationGithub } from '../configure-application-github';
 import CreateApplicationForm from '../create-application-form';
 import { ScrimPopup } from '../scrim-popup';
-import { RootState } from '../../init/store';
-import {
-  ApplicationRegistrationUpsertResponseModel,
-  ApplicationRegistrationUpsertResponseModelValidationMap,
-} from '../../models/radix-api/applications/application-registration-upsert-response';
 import { routes } from '../../routes';
-import {
-  getCreationResult,
-  getCreationState,
-} from '../../state/application-creation';
-import { actions as appsActions } from '../../state/application-creation/action-creators';
-import { RequestState } from '../../state/state-utils/request-states';
 import { routeWithParams } from '../../utils/string';
 
 import './style.css';
-
-interface PageCreateApplicationState {
-  creationState: RequestState;
-  creationResponse?: ApplicationRegistrationUpsertResponseModel;
-}
-
-interface PageCreateApplicationDispatch {
-  resetCreate: () => void;
-}
-
-export interface PageCreateApplicationProps
-  extends PageCreateApplicationState,
-    PageCreateApplicationDispatch {}
+import { ApplicationRegistration } from '../../store/radix-api';
 
 function scrollToPosition(elementRef: Element, x: number, y: number): void {
   elementRef.scrollTo?.(x, y);
 }
 
-const PageCreateApplication: FunctionComponent<PageCreateApplicationProps> = ({
-  creationState,
-  creationResponse,
-  resetCreate,
-}) => {
+type Props = { refetch: Function };
+export default function PageCreateApplication({ refetch }: Props) {
   const [visibleScrim, setVisibleScrim] = useState(false);
   const containerRef = useRef<HTMLDivElement>(null);
+  const [registration, setRegistration] =
+    useState<ApplicationRegistration | null>(null);
 
-  useEffect(() => {
-    if (!visibleScrim) {
-      resetCreate();
-    }
-  }, [resetCreate, visibleScrim]);
+  const onCloseScrim = () => {
+    setVisibleScrim(false);
+  };
 
-  useEffect(() => {
-    if (!visibleScrim) return;
-
-    if (creationState === RequestState.FAILURE) {
-      scrollToPosition(
-        containerRef.current,
-        0,
-        containerRef.current?.scrollHeight
-      );
-    } else if (creationState === RequestState.SUCCESS) {
-      if (
-        creationResponse.applicationRegistration &&
-        !creationResponse.warnings
-      ) {
-        scrollToPosition(containerRef.current, 0, 0);
-      }
-    }
-  }, [creationState, creationResponse, visibleScrim]);
+  const onCreated = (newRegistration: ApplicationRegistration) => {
+    setRegistration(newRegistration);
+    scrollToPosition(containerRef.current, 0, 0);
+  };
 
   return (
     <>
@@ -89,22 +46,19 @@ const PageCreateApplication: FunctionComponent<PageCreateApplicationProps> = ({
       <ScrimPopup
         title="Create new app"
         open={visibleScrim}
-        onClose={() => setVisibleScrim(false)}
+        onClose={onCloseScrim}
       >
         <div className="create-app-content" ref={containerRef}>
-          {creationState !== RequestState.SUCCESS ||
-          !creationResponse.applicationRegistration ||
-          creationResponse.warnings ? (
-            <CreateApplicationForm />
+          {!registration ? (
+            <CreateApplicationForm onCreated={onCreated} />
           ) : (
             <div className="grid grid--gap-medium">
               <Typography>
-                The application{' '}
-                <strong>{creationResponse.applicationRegistration.name}</strong>{' '}
-                has been set up
+                The application <strong>{registration.name}</strong> has been
+                set up
               </Typography>
               <ConfigureApplicationGithub
-                app={creationResponse.applicationRegistration}
+                app={registration}
                 startVisible
                 onDeployKeyChange={() => void 0}
                 useOtherCiToolOptionVisible
@@ -115,7 +69,7 @@ const PageCreateApplication: FunctionComponent<PageCreateApplicationProps> = ({
                 <Typography
                   as={Link}
                   to={routeWithParams(routes.app, {
-                    appName: creationResponse.applicationRegistration.name,
+                    appName: registration.name,
                   })}
                   link
                 >
@@ -128,28 +82,4 @@ const PageCreateApplication: FunctionComponent<PageCreateApplicationProps> = ({
       </ScrimPopup>
     </>
   );
-};
-
-PageCreateApplication.propTypes = {
-  creationState: PropTypes.oneOf(Object.values(RequestState)).isRequired,
-  resetCreate: PropTypes.func.isRequired,
-  creationResponse: PropTypes.shape(
-    ApplicationRegistrationUpsertResponseModelValidationMap
-  ),
-};
-
-function mapStateToProps(state: RootState): PageCreateApplicationState {
-  return {
-    creationState: getCreationState(state),
-    creationResponse: getCreationResult(state),
-  };
 }
-
-function mapDispatchToProps(dispatch: Dispatch): PageCreateApplicationDispatch {
-  return { resetCreate: () => dispatch(appsActions.addAppReset()) };
-}
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(PageCreateApplication);
