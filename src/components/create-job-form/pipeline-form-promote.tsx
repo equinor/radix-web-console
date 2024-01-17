b40e396e538ed28c3be74fa10e724b614f80f542
Richard Hagen <richard.hagen@bouvet.no>
2024-01-17 09:12:34 +0100

    cleanup create job with new types

--- src/components/create-job-form/pipeline-form-promote.tsx
+++ src/components/create-job-form/pipeline-form-promote.tsx
@@ -4,7 +4,7 @@ import {
   NativeSelect,
   Typography,
 } from '@equinor/eds-core-react';
-import { FormEvent, ReactNode, useState } from 'react';
+import { FormEvent, useState } from 'react';
 import { useSearchParams } from 'react-router-dom';
 
 import { RelativeToNow } from '../time/relative-to-now';
@@ -19,14 +19,13 @@ import {
 import { Alert } from '../alert';
 import { getFetchErrorMessage } from '../../store/utils';
 import { handlePromiseWithToast } from '../global-top-nav/styled-toaster';
+import { FormProp } from './index';
 
-interface Props {
-  children: ReactNode;
-  onSuccess: (jobName: string) => void;
-  appName: string;
-}
-
-export function PipelineFormPromote({ children, appName, onSuccess }: Props) {
+export function PipelineFormPromote({
+  children,
+  appName,
+  onSuccess,
+}: FormProp) {
   const [searchParams] = useSearchParams();
   const [trigger, state] = useTriggerPipelinePromoteMutation();
   const { data: deployments } = useGetDeploymentsQuery({ appName });
