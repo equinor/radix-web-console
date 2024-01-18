4adfb84154f35391eae49351778e811587b676ae
Richard Hagen <richard.hagen@gmail.com>
2024-01-18 13:49:46 +0100

    check if time is valid before formatting (#935)
    * check if time is valid before formatting
    
    * reformat
    
    * reformat
    
    * add tests
    
    ---------
    
    Co-authored-by: Richard Hagen <richard.hagen@bouvet.no>

--- src/components/time/duration.tsx
+++ src/components/time/duration.tsx
@@ -1,13 +1,14 @@
 import { FunctionComponent } from 'react';
 
 import { differenceInWords } from '../../utils/datetime';
+import { isValid } from 'date-fns';
 
 export const Duration: FunctionComponent<{
   start: number | string | Date;
   end: number | string | Date;
   title?: string;
 }> = ({ start, end, title }) => {
-  if (!end) {
+  if (!end || !isValid(end) || !isValid(start)) {
     return null;
   }
 
