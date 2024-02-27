7f63426d47d910a00e8869dfc5a23ad163f5c2ca
Sergey Smolnikov <ssmol@equinor.com>
2024-02-27 15:24:40 +0100

    Fixed fail

--- src/components/component/dns-aliases.tsx
+++ src/components/component/dns-aliases.tsx
@@ -15,56 +15,57 @@ export const DNSAliases: FunctionComponent<DefaultAppAliasProps> = ({
   title,
 }) => (
   <>
-    {dnsAliases?.length > 0 && dnsAliases.length == 1 ? (
-      <div className="grid grid--gap-x-small">
-        <Typography variant="h4">{title}</Typography>
-        <Typography as="span">
-          <DNSAlias
-            appName={appName}
-            url={dnsAliases[0].url}
-            componentName={dnsAliases[0].componentName}
-            environmentName={dnsAliases[0].environmentName}
-          />
-        </Typography>
-      </div>
-    ) : (
-      <>
-        <Typography className="whitespace-nowrap" variant="h4" as="span">
-          {title}
-        </Typography>
-        <Accordion className="accordion elevated" chevronPosition="right">
-          <Accordion.Item isExpanded={false}>
-            <Accordion.Header>
-              <Accordion.HeaderTitle>
-                <Typography className="whitespace-nowrap" as="span">
-                  <DNSAlias
-                    appName={appName}
-                    url={dnsAliases[0].url}
-                    componentName={dnsAliases[0].componentName}
-                    environmentName={dnsAliases[0].environmentName}
-                  />
-                </Typography>
-              </Accordion.HeaderTitle>
-            </Accordion.Header>
-            <Accordion.Panel>
-              <List>
-                {dnsAliases.slice(1)?.map((dnsAlias, index) => (
-                  <div key={index} className="o-item-list">
-                    <Typography as="span">
-                      <DNSAlias
-                        appName={appName}
-                        url={dnsAlias.url}
-                        componentName={dnsAlias.componentName}
-                        environmentName={dnsAlias.environmentName}
-                      />
-                    </Typography>
-                  </div>
-                ))}
-              </List>
-            </Accordion.Panel>
-          </Accordion.Item>
-        </Accordion>
-      </>
-    )}
+    {dnsAliases?.length > 0 &&
+      (dnsAliases.length == 1 ? (
+        <div className="grid grid--gap-x-small">
+          <Typography variant="h4">{title}</Typography>
+          <Typography as="span">
+            <DNSAlias
+              appName={appName}
+              url={dnsAliases[0].url}
+              componentName={dnsAliases[0].componentName}
+              environmentName={dnsAliases[0].environmentName}
+            />
+          </Typography>
+        </div>
+      ) : (
+        <>
+          <Typography className="whitespace-nowrap" variant="h4" as="span">
+            {title}
+          </Typography>
+          <Accordion className="accordion elevated" chevronPosition="right">
+            <Accordion.Item isExpanded={false}>
+              <Accordion.Header>
+                <Accordion.HeaderTitle>
+                  <Typography className="whitespace-nowrap" as="span">
+                    <DNSAlias
+                      appName={appName}
+                      url={dnsAliases[0].url}
+                      componentName={dnsAliases[0].componentName}
+                      environmentName={dnsAliases[0].environmentName}
+                    />
+                  </Typography>
+                </Accordion.HeaderTitle>
+              </Accordion.Header>
+              <Accordion.Panel>
+                <List>
+                  {dnsAliases.slice(1)?.map((dnsAlias, index) => (
+                    <div key={index} className="o-item-list">
+                      <Typography as="span">
+                        <DNSAlias
+                          appName={appName}
+                          url={dnsAlias.url}
+                          componentName={dnsAlias.componentName}
+                          environmentName={dnsAlias.environmentName}
+                        />
+                      </Typography>
+                    </div>
+                  ))}
+                </List>
+              </Accordion.Panel>
+            </Accordion.Item>
+          </Accordion>
+        </>
+      ))}
   </>
 );
--- src/components/page-active-component/dns-aliases.tsx
+++ src/components/page-active-component/dns-aliases.tsx
@@ -12,41 +12,42 @@ export const DNSAliases: FunctionComponent<DefaultAppAliasProps> = ({
   title,
 }) => (
   <>
-    {urls.length > 0 && urls.length == 1 ? (
-      <div className="grid grid--gap-x-small">
-        <Typography variant="h4">{title}</Typography>
-        <Typography as="span">
-          <DNSAlias url={urls[0]} />
-        </Typography>
-      </div>
-    ) : (
-      <>
-        <Typography className="whitespace-nowrap" variant="h4" as="span">
-          {title}
-        </Typography>
-        <Accordion className="accordion elevated" chevronPosition="right">
-          <Accordion.Item isExpanded={false}>
-            <Accordion.Header>
-              <Accordion.HeaderTitle>
-                <Typography className="whitespace-nowrap" as="span">
-                  <DNSAlias url={urls[0]} />
-                </Typography>
-              </Accordion.HeaderTitle>
-            </Accordion.Header>
-            <Accordion.Panel>
-              <List>
-                {urls.slice(1)?.map((url, index) => (
-                  <div key={index} className="o-item-list">
-                    <Typography as="span">
-                      <DNSAlias url={url} />
-                    </Typography>
-                  </div>
-                ))}
-              </List>
-            </Accordion.Panel>
-          </Accordion.Item>
-        </Accordion>
-      </>
-    )}
+    {urls.length > 0 &&
+      (urls.length == 1 ? (
+        <div className="grid grid--gap-x-small">
+          <Typography variant="h4">{title}</Typography>
+          <Typography as="span">
+            <DNSAlias url={urls[0]} />
+          </Typography>
+        </div>
+      ) : (
+        <>
+          <Typography className="whitespace-nowrap" variant="h4" as="span">
+            {title}
+          </Typography>
+          <Accordion className="accordion elevated" chevronPosition="right">
+            <Accordion.Item isExpanded={false}>
+              <Accordion.Header>
+                <Accordion.HeaderTitle>
+                  <Typography className="whitespace-nowrap" as="span">
+                    <DNSAlias url={urls[0]} />
+                  </Typography>
+                </Accordion.HeaderTitle>
+              </Accordion.Header>
+              <Accordion.Panel>
+                <List>
+                  {urls.slice(1)?.map((url, index) => (
+                    <div key={index} className="o-item-list">
+                      <Typography as="span">
+                        <DNSAlias url={url} />
+                      </Typography>
+                    </div>
+                  ))}
+                </List>
+              </Accordion.Panel>
+            </Accordion.Item>
+          </Accordion>
+        </>
+      ))}
   </>
 );
