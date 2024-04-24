c02b0257e8e183856dfc931e8ca3bd44dd24b20d
Sergey Smolnikov <ssmol@equinor.com>
2024-04-24 14:59:49 +0200

    fix-missed-ad-group-message (#1012)
    * Shown error on missing AD group
    
    * Shown error on missing AD group
    
    * Shown error on missing AD group
    
    * Shown error on missing AD group
    
    * add loading check
    
    * ...
    
    * Restored commented component
    
    * show deleted groups in multi select
    
    * reorganize imports
    
    * add AsyncResource to handle error states
    
    ---------
    
    Co-authored-by: Richard87 <richard.hagen@gmail.com>
    Co-authored-by: Nils Gustav Stråbø <nst@equinor.com>

---
+++ src/components/component/unknown-ad-groups-alert.tsx
@@ -0,0 +1,22 @@
+import { Alert } from '../alert';
+import { List } from '@equinor/eds-core-react';
+
+interface Props {
+  unknownADGroups?: Array<string>;
+}
+export function UnknownADGroupsAlert({ unknownADGroups }: Props) {
+  return (
+    <>
+      {unknownADGroups?.length > 0 && (
+        <Alert type="danger">
+          Unknown or deleted AD group(s)
+          <List className="o-indent-list">
+            {unknownADGroups.map((adGroup) => (
+              <List.Item key={adGroup}>{adGroup}</List.Item>
+            ))}
+          </List>
+        </Alert>
+      )}
+    </>
+  );
+}
