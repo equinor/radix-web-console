fd5fc6b1174230414a6ea489778fe23de9685263
Richard87 <richard.hagen@gmail.com>
2024-04-25 15:18:49 +0200

    Update dependencies, use react router for navigation to home screen

--- src/components/home-logo/index.tsx
+++ src/components/home-logo/index.tsx
@@ -3,11 +3,13 @@ import { FunctionComponent } from 'react';
 
 import { HomeIcon } from '../home-icon';
 import { routes } from '../../routes';
+import { Link } from 'react-router-dom';
 
 export const HomeLogo: FunctionComponent = () => (
   <Typography
     link
-    href={routes.home}
+    as={Link}
+    to={routes.home}
     token={{
       color: 'var(--eds_text_static_icons__default)',
       textDecoration: 'none',
