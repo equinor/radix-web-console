19b85a1f9e030f32033c628046d959c55bce9553
Richard87 <richard.hagen@gmail.com>
2025-01-16 09:24:18 +0100

    Remove Ingress IP From Configuration

--- src/utils/config/index.ts
+++ src/utils/config/index.ts
@@ -11,7 +11,6 @@ export const configVariables = {
   "RADIX_CLUSTER_TYPE": getVariable("RADIX_CLUSTER_TYPE", "development"),
   "RADIX_ENVIRONMENT": getVariable("RADIX_ENVIRONMENT", "prod"),
   "CLUSTER_EGRESS_IPS": getVariable("CLUSTER_EGRESS_IPS", "1.1.1.1,1.1.1.1"),
-  "CLUSTER_INGRESS_IPS": getVariable("CLUSTER_INGRESS_IPS", "2.2.2.2,2.2.4.4"),
   "OAUTH2_CLIENT_ID": getVariable("OAUTH2_CLIENT_ID", "5687b237-eda3-4ec3-a2a1-023e85a2bd84"),
   "OAUTH2_AUTHORITY": getVariable("OAUTH2_AUTHORITY", "https://login.microsoftonline.com/3aa4a235-b6e2-48d5-9195-7fcf05b459b0"),
   "SERVICENOW_PROXY_SCOPES": getVariable("SERVICENOW_PROXY_SCOPES", "1b4a22f1-d4a1-4b6a-81b2-fd936daf1786/Application.Read"),
