95fe2fcf1224aff5ea641f2bb4292c5a2dad3a7a
Richard87 <richard.hagen@gmail.com>
2025-01-16 09:05:23 +0100

    add IsRadixHttpError and wrapped line on newlines in Alerts

--- src/store/utils/index.ts
+++ src/store/utils/index.ts
@@ -28,13 +28,20 @@ export function getFetchErrorData(error: ManagedErrors): {
     }
   }
 
+  if (IsRadixHttpError(error)) {
+    return {
+      code: error.status,
+      error: error.data.error ?? "unknown server error",
+      message: error.data.message
+    }
+  }
+
   if (IsRTKQueryError(error)) {
     const code = isFetchBaseQueryError(error) ? error.status : IsParsingError(error) ? error.originalStatus : undefined;
-    let errorMessage = error?.error??error?.data?.error;
     return {
       code: code,
-      error: errorMessage,
-      message: "failed to fetch data: " + (code ? getReasonPhrase(code) : "unknown") + (errorMessage ? ". " + errorMessage : "")
+      error: isFetchBaseQueryError(error) ? "server error" : error.error,
+      message: "failed to fetch data: " + (code ? getReasonPhrase(code) : "unknown")
     }
   }
 
@@ -60,6 +67,25 @@ export function getFetchErrorMessage(
   return getFetchErrorData(error).message;
 }
 
+type RadixHttpError = {
+  status: number;
+  data: {
+    message: string
+    error?: string
+    type: "server"|"missing"|"user"|"forbidden"
+  }
+}
+function IsRadixHttpError(e: any): e is RadixHttpError {
+  if (typeof e !== "object" || e == null) return false;
+
+  if (!("data" in e && "status" in e)) return false;
+
+  if (typeof e.data !== "object" || e.data == null) return false;
+
+  if ("error" in e.data && "message" in e.data && "type" in e.data) return true;
+
+  return false;
+}
 
 function IsRTKQueryError(e: any): e is FetchBaseQueryError {
   return isFetchBaseQueryError(e) || IsFetchError(e) || IsParsingError(e) || IsTimeoutError(e) || IsCustomError(e)
