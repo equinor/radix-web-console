c1720c10376cf562436ef0471aa82b3b03e224e0
Richard Hagen <richard.hagen@gmail.com>
2025-01-24 12:30:59 +0100

    Navigate back and forth, footer is always visible (#1175)
    * Navigate back and forth, footer is always visible
    
    * clear error when submitting over again
    
    * clear warnings when creating app

--- src/components/create-application/create-application-form.tsx
+++ src/components/create-application/create-application-form.tsx
@@ -8,7 +8,7 @@ import {
   Typography,
 } from '@equinor/eds-core-react';
 import { info_circle } from '@equinor/eds-icons';
-import { type FormEvent, useState } from 'react';
+import { type FormEvent, useRef, useState } from 'react';
 import { externalUrls } from '../../externalUrls';
 import type { ApplicationRegistration } from '../../store/radix-api';
 import { getFetchErrorMessage } from '../../store/utils';
@@ -25,8 +25,14 @@ type Props = {
     application: ApplicationRegistration,
     acknowledgeWarnings: boolean
   ) => Promise<string[]>;
+  onNext: () => unknown;
 };
-export function CreateApplicationForm({ registration, onCreate }: Props) {
+export function CreateApplicationForm({
+  registration,
+  onCreate,
+  onNext,
+}: Props) {
+  const contentRef = useRef<HTMLDivElement>(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState<unknown>();
   const [warnings, setWarnings] = useState<string[]>([]);
@@ -35,6 +41,8 @@ export function CreateApplicationForm({ registration, onCreate }: Props) {
   const handleSubmit = async (ev: FormEvent<HTMLFormElement>) => {
     ev.preventDefault();
     setLoading(true);
+    setError(undefined);
+    setWarnings([]);
     try {
       const data = new FormData(ev.currentTarget);
       const applicationRegistration: Partial<ApplicationRegistration> = {
@@ -56,8 +64,13 @@ export function CreateApplicationForm({ registration, onCreate }: Props) {
 
       const wrns = await onCreate(application, acknowledgeWarnings);
       setWarnings(wrns);
+
+      if (wrns.length > 0) {
+        setTimeout(() => contentRef.current?.scrollIntoView(false), 0);
+      }
     } catch (e) {
       errorToast(`Error while saving. ${getFetchErrorMessage(e)}`);
+      setTimeout(() => contentRef.current?.scrollIntoView(false), 0);
       setError(e);
     } finally {
       setLoading(false);
@@ -65,123 +78,134 @@ export function CreateApplicationForm({ registration, onCreate }: Props) {
   };
 
   return (
-    <form onSubmit={handleSubmit} className="grid grid--gap-medium">
-      <Alert className="icon">
-        <Icon data={info_circle} color="primary" />
-        <div>
-          <Typography>
-            Your application needs a GitHub repository with a radixconfig.yaml
-            file and a Dockerfile.
-          </Typography>
-          <Typography>
-            You can read about{' '}
-            <ExternalLink href={externalUrls.referenceRadixConfig}>
-              radixconfig.yaml
-            </ExternalLink>{' '}
-            and{' '}
-            <ExternalLink href={externalUrls.guideDockerfileComponent}>
-              Dockerfile best practices
-            </ExternalLink>
-            .
-          </Typography>
-          <Typography>
-            Need help? Get in touch on our{' '}
-            <ExternalLink href={externalUrls.slackRadixSupport}>
-              Slack support channel
-            </ExternalLink>
-          </Typography>
-        </div>
-      </Alert>
-      <fieldset
-        disabled={loading || !!registration}
-        className="grid grid--gap-medium"
-      >
-        <TextField
-          id="name_field"
-          label="Name"
-          // @ts-expect-error Textfield does actually allow JSX
-          helperText={
-            <span>
-              Must be exactly the same as <em>name</em> in{' '}
-              <em>radixconfig.yaml</em> file. Lower case; no spaces or special
-              characters
-            </span>
-          }
-          name="name"
-          defaultValue={registration?.name}
-        />
-        <TextField
-          id="repository_field"
-          label="GitHub repository"
-          helperText="Full URL, e.g. 'https://github.com/equinor/my-app'"
-          name="repository"
-          defaultValue={registration?.repository}
-        />
-        <TextField
-          id="configbranch_field"
-          label="Config Branch"
-          // @ts-expect-error Textfield does actually allow JSX
-          helperText={
-            <span>
-              The name of the branch where Radix will read the{' '}
-              <em>radixconfig.yaml</em> from, e.g. 'main' or 'master'
-            </span>
-          }
-          name="configBranch"
-          defaultValue={registration?.configBranch ?? 'main'}
-        />
-        <TextField
-          id="radixconfig_file_field"
-          label="Config file"
-          helperText="The name and optionally the path of the Radix config file. Located in the repository root folder of the config branch"
-          name="radixConfigFullName"
-          defaultValue={registration?.radixConfigFullName ?? 'radixconfig.yaml'}
-        />
-        <AppConfigConfigurationItem
-          name="configurationItem"
-          configurationItem={registration?.configurationItem}
-        />
-        <AppConfigAdGroups
-          name="adGroups"
-          adGroups={adUsers?.map((x) => x.id) || registration?.adGroups}
-          onChange={(items) => setAdUsers([...items])}
-          labeling="Administrators"
-        />
-        {error ? (
-          <Alert type="danger">
-            Failed to create application.
-            <br />
-            {getFetchErrorMessage(error)}
-          </Alert>
-        ) : null}
-        <div className="o-action-bar grid grid--gap-medium">
-          {loading && (
-            <Typography>
-              <CircularProgress size={24} /> Creating…
-            </Typography>
-          )}
-          {warnings.length > 0 && (
-            <div className="grid grid--gap-medium">
-              <List>
-                {warnings.map((message, i) => (
-                  <List.Item key={i}>
-                    <Alert type="warning">{message}</Alert>
-                  </List.Item>
-                ))}
-              </List>
-              <Checkbox
-                label="Proceed with warnings"
-                name="acknowledgeWarnings"
-              />
-            </div>
-          )}
+    <form onSubmit={handleSubmit} className="create-app">
+      <div className="create-app-content">
+        <Alert className="icon">
+          <Icon data={info_circle} color="primary" />
           <div>
-            <Button color="primary" type="submit">
-              Create new app
-            </Button>
+            <Typography>
+              Your application needs a GitHub repository with a radixconfig.yaml
+              file and a Dockerfile.
+            </Typography>
+            <Typography>
+              You can read about{' '}
+              <ExternalLink href={externalUrls.referenceRadixConfig}>
+                radixconfig.yaml
+              </ExternalLink>{' '}
+              and{' '}
+              <ExternalLink href={externalUrls.guideDockerfileComponent}>
+                Dockerfile best practices
+              </ExternalLink>
+              .
+            </Typography>
+            <Typography>
+              Need help? Get in touch on our{' '}
+              <ExternalLink href={externalUrls.slackRadixSupport}>
+                Slack support channel
+              </ExternalLink>
+            </Typography>
           </div>
-        </div>
-      </fieldset>
+        </Alert>
+        <fieldset
+          disabled={loading || !!registration}
+          className="grid grid--gap-medium"
+        >
+          <TextField
+            id="name_field"
+            label="Name"
+            // @ts-expect-error Textfield does actually allow JSX
+            helperText={
+              <span>
+                Must be exactly the same as <em>name</em> in{' '}
+                <em>radixconfig.yaml</em> file. Lower case; no spaces or special
+                characters
+              </span>
+            }
+            name="name"
+            defaultValue={registration?.name}
+          />
+          <TextField
+            id="repository_field"
+            label="GitHub repository"
+            helperText="Full URL, e.g. 'https://github.com/equinor/my-app'"
+            name="repository"
+            defaultValue={registration?.repository}
+          />
+          <TextField
+            id="configbranch_field"
+            label="Config Branch"
+            // @ts-expect-error Textfield does actually allow JSX
+            helperText={
+              <span>
+                The name of the branch where Radix will read the{' '}
+                <em>radixconfig.yaml</em> from, e.g. 'main' or 'master'
+              </span>
+            }
+            name="configBranch"
+            defaultValue={registration?.configBranch ?? 'main'}
+          />
+          <TextField
+            id="radixconfig_file_field"
+            label="Config file"
+            helperText="The name and optionally the path of the Radix config file. Located in the repository root folder of the config branch"
+            name="radixConfigFullName"
+            defaultValue={
+              registration?.radixConfigFullName ?? 'radixconfig.yaml'
+            }
+          />
+          <AppConfigConfigurationItem
+            name="configurationItem"
+            configurationItem={registration?.configurationItem}
+          />
+          <AppConfigAdGroups
+            name="adGroups"
+            adGroups={adUsers?.map((x) => x.id) || registration?.adGroups}
+            onChange={(items) => setAdUsers([...items])}
+            labeling="Administrators"
+          />
+          {error ? (
+            <Alert type="danger">
+              Failed to create application.
+              <br />
+              {getFetchErrorMessage(error)}
+            </Alert>
+          ) : null}
+          <div className="o-action-bar grid grid--gap-medium" ref={contentRef}>
+            {loading && (
+              <Typography>
+                <CircularProgress size={24} /> Creating…
+              </Typography>
+            )}
+            {warnings.length > 0 && (
+              <div className="grid grid--gap-medium">
+                <List>
+                  {warnings.map((message, i) => (
+                    <List.Item key={i}>
+                      <Alert type="warning">{message}</Alert>
+                    </List.Item>
+                  ))}
+                </List>
+                <Checkbox
+                  disabled={loading || !!registration}
+                  label="Proceed with warnings"
+                  name="acknowledgeWarnings"
+                />
+              </div>
+            )}
+          </div>
+        </fieldset>
+      </div>
+      <div className="create-app-footer">
+        {registration ? (
+          <Button onClick={onNext} type="button">
+            Next: Configure Deploykey
+          </Button>
+        ) : (
+          <Button color="primary" type="submit">
+            Create new app
+          </Button>
+        )}
+      </div>
     </form>
   );
 }
