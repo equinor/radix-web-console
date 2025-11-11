847031695b6ea4ed5d7c69fafbe955b2af686379
Jacob Solberg Holm <jas@equinor.com>
2025-11-11 15:11:51 +0100

    Linting issue...

--- src/components/config-list/index.tsx
+++ src/components/config-list/index.tsx
@@ -9,7 +9,7 @@ const ConfigVariableTableCell: FunctionComponent<{
   <Table.Cell>
     <pre>
       <div style={{ userSelect: 'all' }}>
-      <Typography>{Array.isArray(value) ? value.join('\n') : JSON.stringify(value, null, 2)}</Typography>
+        <Typography>{Array.isArray(value) ? value.join('\n') : JSON.stringify(value, null, 2)}</Typography>
       </div>
     </pre>
   </Table.Cell>
