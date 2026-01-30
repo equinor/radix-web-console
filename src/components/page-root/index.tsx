f4bd84eaab8ef0bafa4da22c0c592023d4996d0e
Richard Hagen <richard.hagen@gmail.com>
2026-01-30 14:39:34 +0100

    feat: upgrade react-router to v7

--- src/components/page-root/index.tsx
+++ src/components/page-root/index.tsx
@@ -3,11 +3,11 @@ import { RouterProvider, type RouterProviderProps } from 'react-router-dom'
 
 import { LazyLoadFallback } from '../lazy-load-fallback'
 
-import './style.css'
 import { InteractionType } from '@azure/msal-browser'
 import { useMsalAuthentication } from '@azure/msal-react'
 import { useStore } from 'react-redux'
 import type { RootState } from '../../store/store'
+import './style.css'
 
 export const PageRouter: FunctionComponent<Pick<RouterProviderProps, 'router'>> = ({ router }) => {
   useMsalAuthentication(InteractionType.Redirect)
@@ -20,7 +20,7 @@ export const PageRouter: FunctionComponent<Pick<RouterProviderProps, 'router'>> 
   return (
     <div className="page-root">
       <div className="page-root-layout-base">
-        <RouterProvider future={{ v7_startTransition: true }} router={router} fallbackElement={<LazyLoadFallback />} />
+        <RouterProvider router={router} fallbackElement={<LazyLoadFallback />} />
       </div>
     </div>
   )
