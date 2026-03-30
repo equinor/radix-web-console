cc67a5da26ab59e04a31f7c0ebba9e75f54d357e
Nils Gustav Stråbø <65334626+nilsgstrabo@users.noreply.github.com>
2026-03-30 10:31:38 +0200

    fix dev.tsx

--- src/components/app-list/dev.tsx
+++ src/components/app-list/dev.tsx
@@ -15,10 +15,20 @@ import AppList from '.'
 const testApps: ShowApplicationsApiResponse = [
   {
     name: 'radix-canary-golang',
-    environmentActiveComponents: {
-      dev: [{ image: 'karpe', name: 'web', type: 'component' }],
-      prod: [{ image: 'diem', name: 'web', type: 'component' }],
-    },
+    environments: [
+      {
+        name: 'dev',
+        activeDeployment: {
+          name: 'any-dev-deployment',
+          activeFrom: new Date('2023-12-01T11:27:17Z').toISOString(),
+          environment: 'dev',
+          namespace: 'any-ns',
+          repository: 'any-repo',
+          status: 'Ready',
+        },
+      },
+    ],
+
     latestJob: {
       name: 'latest job',
       created: new Date('2023-12-01T11:27:17Z').toISOString(),
@@ -30,10 +40,19 @@ const testApps: ShowApplicationsApiResponse = [
   },
   {
     name: 'radix-web-console',
-    environmentActiveComponents: {
-      botnet: [{ image: 'yes', name: 'botnet-1', type: 'job', status: 'Consistent' }],
-      coinminer: [{ image: 'no', name: 'queen', type: 'component', status: 'Consistent' }],
-    },
+    environments: [
+      {
+        name: 'dev',
+        activeDeployment: {
+          name: 'any-dev-deployment',
+          activeFrom: new Date('2023-12-01T11:27:17Z').toISOString(),
+          environment: 'dev',
+          namespace: 'any-ns',
+          repository: 'any-repo',
+          status: 'Failed',
+        },
+      },
+    ],
     latestJob: {
       name: 'latest job',
       created: new Date('2023-12-01T11:27:17Z').toISOString(),
@@ -49,13 +68,13 @@ const testVulns: GetApplicationVulnerabilitySummariesApiResponse = [
     name: testApps[0].name!,
     components: {
       dev: {
-        image: testApps[0].environmentActiveComponents!.dev[0].image,
+        image: 'image1',
         scanSuccess: true,
         scanTime: new Date().toISOString(),
         vulnerabilitySummary: { high: 3 },
       },
       prod: {
-        image: testApps[0].environmentActiveComponents!.prod[0].image,
+        image: 'image2',
         scanSuccess: true,
         scanTime: new Date().toISOString(),
         vulnerabilitySummary: { critical: 17, high: 5 },
@@ -66,12 +85,12 @@ const testVulns: GetApplicationVulnerabilitySummariesApiResponse = [
     name: testApps[1].name!,
     components: {
       dev: {
-        image: testApps[1].environmentActiveComponents!.botnet[0].image,
+        image: 'image1',
         scanSuccess: true,
         scanTime: new Date().toISOString(),
       },
       prod: {
-        image: testApps[1].environmentActiveComponents!.coinminer[0].image,
+        image: 'image2',
         scanSuccess: true,
         scanTime: new Date().toISOString(),
         vulnerabilitySummary: { high: 1, medium: 1, low: 6 },
