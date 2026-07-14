66c8d9e316ee77d56e0474e995ee08835782bcdc
Kristin Pettersen <krpet@equinor.com>
2026-07-14 15:56:22 +0200

    storybook as part of CI

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -7,7 +7,8 @@ const config: StorybookConfig = {
   ],
   "addons": [
     "@storybook/addon-a11y",
-    "@storybook/addon-docs"
+    "@storybook/addon-docs",
+    "@storybook/addon-vitest"
   ],
   "framework": "@storybook/react-vite"
 };
