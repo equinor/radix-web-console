ff9a2dd5b1462eb55dd072ecadba3d03b1125a72
Richard Hagen <richard.hagen@gmail.com>
2026-07-27 16:29:31 +0200

    fix: add known authorities to allow msal to fetch oidc config

--- src/init/msal-config.ts
+++ src/init/msal-config.ts
@@ -1,11 +1,23 @@
 import { type Configuration, ProtocolMode, ServerResponseType } from '@azure/msal-browser'
 import { configVariables } from '../utils/config'
 
+const authority = configVariables.OAUTH2_AUTHORITY
+
+function getAuthorityHost(url: string): string {
+  try {
+    return new URL(url).host
+  } catch {
+    return url
+  }
+}
+
 export const msalConfig: Configuration = {
   auth: {
     clientId: configVariables.OAUTH2_CLIENT_ID,
-    authority: configVariables.OAUTH2_AUTHORITY,
+    authority,
+    knownAuthorities: [getAuthorityHost(authority)],
     protocolMode: ProtocolMode.OIDC,
+
     OIDCOptions: {
       serverResponseType: ServerResponseType.QUERY,
     },
