21c825fd73776da252fc8465b2d1b88c383ca778
Richard Hagen <richard.hagen@gmail.com>
2026-07-28 10:04:24 +0200

    simplify authoritty known hosts

--- src/init/msal-config.ts
+++ src/init/msal-config.ts
@@ -1,21 +1,11 @@
 import { type Configuration, ProtocolMode, ServerResponseType } from '@azure/msal-browser'
 import { configVariables } from '../utils/config'
 
-const authority = configVariables.OAUTH2_AUTHORITY
-
-function getAuthorityHost(url: string): string {
-  try {
-    return new URL(url).host
-  } catch {
-    return url
-  }
-}
-
 export const msalConfig: Configuration = {
   auth: {
     clientId: configVariables.OAUTH2_CLIENT_ID,
-    authority,
-    knownAuthorities: [getAuthorityHost(authority)],
+    authority: configVariables.OAUTH2_AUTHORITY,
+    knownAuthorities: [new URL(configVariables.OAUTH2_AUTHORITY).host],
     protocolMode: ProtocolMode.OIDC,
 
     OIDCOptions: {
